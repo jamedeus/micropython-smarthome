a180b66e9e3fbf88e1af3737e25e8cd5c8488eea
Josh Medeiros <josh@jamedeus.com>
2021-10-11 21:16:32 -0700

    Add basic tester script for PIR sensor

--- PIR-tester/boot.py
+++ PIR-tester/boot.py
@@ -2,9 +2,15 @@
 #import esp
 #esp.osdebug(None)
 
+# Connect PIR motion sensor:
+# VCC: 3v3
+# GND: GND
+# Data: D2 (GPIO 2)
+
 import webrepl
 import network
 import time
+from machine import Pin
 
 # Connect to wifi
 wlan = network.WLAN(network.STA_IF)
@@ -13,3 +19,15 @@ wlan.connect('jamnet', 'cjZY8PTa4ZQ6S83A')
 
 # Start webrepl to allow connecting and uploading scripts from browser
 webrepl.start()
+
+# PIR data pin connected to D2
+pir = Pin(2, Pin.IN)
+
+while True:
+    if pir.value():
+        print("Motion detected")
+        print(pir.value())
+    else:
+        print("No motion")
+        print(pir.value())
+    time.sleep(1)
