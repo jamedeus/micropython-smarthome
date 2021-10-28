608f10d99c514e8d626bc0bf8a50a36fe0d39ae7
Josh Medeiros <josh@jamedeus.com>
2021-10-27 18:12:14 -0700

    Added button interupt to enter upload mode, automatically reboots after upload complete

--- pir-tester.py
+++ pir-tester.py
@@ -33,6 +33,11 @@ def motion_detected(pin):
     global motion
     motion = True
 
+# Button intterupt callback
+def buttonInterrupt(pin):
+    global wifi
+    wifi = True
+
 # Onboard LED turns on when motion detected
 led = Pin(2, Pin.OUT, value=0)
 
@@ -44,6 +49,13 @@ pir = Pin(15, Pin.IN)
 
 # Create interrupt, call handler function when motion detected
 pir.irq(trigger=Pin.IRQ_RISING, handler=motion_detected)
+
+# Interrupt button, breaks loop, waits for file upload then automatically reboots
+button = Pin(16, Pin.IN)
+button.irq(trigger=Pin.IRQ_RISING, handler=buttonInterrupt)
+
+# Set to True by interrupt button
+wifi = False
 
 # If True, onboard LED and PWM light will blink when motion detected
 # Disabled by default because the delay sometimes prevents a few log messages
@@ -70,36 +82,52 @@ def view_log():
 
 
 while True:
-    if motion:
-        # Get time tuple
-        now = time.localtime()
+    if not wifi:
+        if motion:
+            # Get time tuple
+            now = time.localtime()
 
-        # Create string, timestamp added in loop
-        log = "Motion detected at "
+            # Create string, timestamp added in loop
+            log = "Motion detected at "
 
-        # Since tuple contains int not str, single digit hour/min/sec lack leading 0s
-        # For readability, check length of each and add leading 0 if single-digit
-        for i in range(3,6):
-            if len(str(now[i])) == 1:
-                log = log + "0" + str(now[i]) + ":"
+            # Since tuple contains int not str, single digit hour/min/sec lack leading 0s
+            # For readability, check length of each and add leading 0 if single-digit
+            for i in range(3,6):
+                if len(str(now[i])) == 1:
+                    log = log + "0" + str(now[i]) + ":"
+                else:
+                    log = log + str(now[i]) + ":"
             else:
-                log = log + str(now[i]) + ":"
-        else:
-            log = log[0:-1] # Remove trailing : added by loop
+                log = log[0:-1] # Remove trailing : added by loop
 
-        # Print to console for real-time monitoring
-        print(log)
+            # Print to console for real-time monitoring
+            print(log)
 
-        if log:
-            file = open('log.txt', 'a')
-            file.write(log + "\n") # Append to log with newline after
-            file.close() # Unlike CPython, nothing is written until file closed
+            if log:
+                file = open('log.txt', 'a')
+                file.write(log + "\n") # Append to log with newline after
+                file.close() # Unlike CPython, nothing is written until file closed
 
-        if blink:
-            led.value(1)
-            pwm.duty(1023)
-            time.sleep_ms(25)
-            led.value(0)
-            pwm.duty(0)
+            if blink:
+                led.value(1)
+                pwm.duty(1023)
+                time.sleep_ms(25)
+                led.value(0)
+                pwm.duty(0)
 
-        motion = False
+            motion = False
+    else:
+        # Runs when interrupt button pressed, wait for webrepl upload then reboot automatically
+        led.value(1)
+        print("maintenance mode")
+        import machine
+        import os
+        old = os.stat("boot.py")
+        while True:
+            new = os.stat("boot.py")
+            if new == old:
+                time.sleep(1)
+            else:
+                print("Upload complete, rebooting...")
+                time.sleep(1) # Prevents webrepl_cli.py from hanging after upload (esp reboots too fast)
+                machine.reset()
