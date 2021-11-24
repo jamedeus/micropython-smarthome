e04c98bb845a909d92e6524e96b4f50c7e7eba7d
Josh Medeiros <josh@jamedeus.com>
2021-11-24 14:45:01 -0800

    Simplified loop, added comments

--- sonoff-mini-r2/endpoint-server.py
+++ sonoff-mini-r2/endpoint-server.py
@@ -13,26 +13,50 @@ wlan.connect('jamnet', 'cjZY8PTa4ZQ6S83A')
 # Do not put code before this, if it hangs will not be able to connect
 webrepl.start()
 
-print("\nCompleted startup\n")
-
 relay = Pin(12, Pin.OUT)
 
+# Get filesize/modification time (to detect upload in future)
 old = os.stat("boot.py")
 
+print("\nCompleted startup\n")
+
+# Create socket listening on port 4200
 s = socket.socket()
 s.bind((wlan.ifconfig()[0], 4200))
 s.listen(1)
 
+# Handle connections
 while True:
-    if not os.stat("boot.py") == old:
-        import machine
-        machine.reset()
+    # Accept connection, decode message
     conn, addr = s.accept()
     msg = conn.recv(1024).decode()
-    if not msg:
-        break
+
     if msg == "on":
         relay.value(1)
     elif msg == "off":
         relay.value(0)
+
+    # Close connection, restart loop and wait for next connection
     conn.close()
+
+    # Check if boot.py changeed on disk (upload received through webrepl), reboot if it did
+    if not os.stat("boot.py") == old:
+        import machine
+        machine.reset()
+
+
+
+## Client-side code ##
+#import socket
+
+#def on():
+    #s = socket.socket()
+    #s.connect(('192.168.1.227', 4200))
+    #s.send("on".encode())
+    #s.close()
+
+#def off():
+    #s = socket.socket()
+    #s.connect(('192.168.1.227', 4200))
+    #s.send("off".encode())
+    #s.close()
