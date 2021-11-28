e123f9f3834dc48259a1abb04944e1e4d819482e
Josh Medeiros <josh@jamedeus.com>
2021-11-28 15:52:05 -0800

    Minor bug fix, add print lines

--- desktop-integration.py
+++ desktop-integration.py
@@ -21,6 +21,7 @@ state = None
 # Gives user a chance to trigger motion sensor and prevent monitor turning off
 def off():
     if state == False: # Will be True if motion detected before timer expired
+        print("Timer expired, turning off")
         os.system('xset dpms force off')
 
 
@@ -41,15 +42,19 @@ while True:
         with open('/sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/drm/card1/card1-DP-4/dpms', 'r') as file:
             current = file.read()
         # Only turn on if it's currently off (turning on when already on causes artifacting)
-        if current == "Off":
+        if "Off" in current:
+            print("On command received, turning on")
             os.system('xset dpms force on')
             os.system('xrandr --output DisplayPort-1-4 --set "PRIME Synchronization" 1;xrandr --output DisplayPort-1-3 --set "PRIME Synchronization" 1')
+        else:
+            print("On command received, but monitors are already on")
         state = True
     elif msg == "off":
         # Wait 5 seconds (after lights turn off) before turning monitor off
         state = False
         t = Timer(interval = 10.0, function=off)
         t.start()
+        print("Off command received, setting 10 sec timer")
 
     # Close connection, restart loop and wait for next connection
     conn.close()
