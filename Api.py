f6d38e0f7ec811014ae96f7a77605fa3a8eb5771
Josh Medeiros <josh@jamedeus.com>
2022-03-07 20:59:59 -0800

    Moved most API functions to methods of Api class (run_client method massively more readable)

--- Api.py
+++ Api.py
@@ -18,6 +18,88 @@ class Api:
         self.backlog = backlog
         self.timeout = timeout
 
+    def disable(self, sensor):
+        for i in Config.config.sensors:
+            if i.name == sensor:
+                print(f"API: Received command to disable {sensor}, disabling...")
+                log.info(f"API: Received command to disable {sensor}, enabling...")
+                i.disable()
+                return 'OK'
+
+        # If no match found
+        return 'Error: Sensor not found'
+
+
+
+    def enable(self, sensor):
+        for i in Config.config.sensors:
+            if i.name == sensor:
+                print(f"API: Received command to enable {sensor}, disabling...")
+                log.info(f"API: Received command to enable {sensor}, enabling...")
+                i.enable()
+                return 'OK'
+
+        # If no match found
+        return 'Error: Sensor not found'
+
+
+
+    def set_rule(self, target, rule, client):
+        if target.startswith("sensor"):
+            for i in Config.config.sensors:
+                if i.name == target:
+                    print(f"API: Received command to set {target} delay to {rule} minutes, setting...")
+                    try:
+                        i.current_rule = rule
+                        return 'OK'
+                    except:
+                        return 'Error: Bad rule parameter, int required'
+
+        elif target.startswith("device"):
+            for i in Config.config.devices:
+                if i.name == target:
+                    print(f"API: Received command to set {target} brightness to {rule}, setting...")
+                    try:
+                        i.current_rule = rule
+                        return 'OK'
+                    except:
+                        return 'Error: Bad rule parameter. Relay requires "on" or "off", all others require int'
+
+        else:
+            print(f"API: Received invalid command from {client}")
+            return 'Error: 2nd param must be name of a sensor or device - use status to see options'
+
+
+
+    def ir_key(self, target, key):
+        for i in Config.config.devices:
+            if i.device == "ir_blaster":
+                if not target in i.codes:
+                    return 'Error: No codes found for target "{}"'.format(target)
+                else:
+                    if not key:
+                        return 'Error: Please specify which key to simulate'
+                    else:
+                        i.send(target, key)
+                        return 'OK'
+
+        return 'Error: No IR blaster configured'
+
+
+
+    def ir_backlight(self, state):
+        if not (state == "on" or state == "off"):
+            return 'Error: Backlight setting must be "on" or "off"'
+        else:
+            for i in Config.config.devices:
+                if i.device == "ir_blaster":
+                    i.backlight(state)
+                    return 'OK'
+
+        return 'Error: No IR blaster configured'
+
+
+
     async def run(self):
         print('\API: Awaiting client connection.\n')
         log.info("API ready")
@@ -55,93 +137,20 @@ class Api:
                     await swriter.drain()  # Echo back
                     Config.reboot()
 
-
-
                 elif data[0] == "disable" and data[1].startswith("sensor"):
-                    for i in Config.config.sensors:
-                        if i.name == data[1]:
-                            print(f"API: Received command to disable {data[1]}, disabling...")
-                            log.info(f"API: Received command to disable {data[1]}, enabling...")
-                            i.disable()
-                            reply = 'OK'
-
-                    if not reply: # Detect if loop failed to find a match
-                        reply = 'Error: Sensor not found'
-
-
+                    reply = self.disable(data[1])
 
                 elif data[0] == "enable" and data[1].startswith("sensor"):
-                    for i in Config.config.sensors:
-                        if i.name == data[1]:
-                            print(f"API: Received command to enable {data[1]}, enabling...")
-                            log.info(f"API: Received command to enable {data[1]}, enabling...")
-                            i.enable()
-                            reply = 'OK'
-
-                    if not reply: # Detect if loop failed to find a match
-                        reply = 'Error: Sensor not found'
-
-
+                    reply = self.enable(data[1])
 
                 elif data[0] == "set_rule" and data[1].startswith("sensor") or data[1].startswith("device"):
-                    target = data[1]
-
-                    if target.startswith("sensor"):
-                        for i in Config.config.sensors:
-                            if i.name == target:
-                                print(f"API: Received command to set {target} delay to {data[2]} minutes, setting...")
-                                try:
-                                    i.current_rule = data[2]
-                                    reply = 'OK'
-                                except:
-                                    reply = 'Error: Bad rule parameter, int required'
-
-                    elif target.startswith("device"):
-                        for i in Config.config.devices:
-                            if i.name == target:
-                                print(f"API: Received command to set {target} brightness to {data[2]}, setting...")
-                                try:
-                                    i.current_rule = data[2]
-                                    reply = 'OK'
-                                except:
-                                    reply = 'Error: Bad rule parameter. Relay requires "on" or "off", all others require int'
-
-                    else:
-                        print(f"API: Received invalid command from {sreader.get_extra_info('peername')[0]}")
-                        reply = 'Error: 2nd param must be name of a sensor or device - use status to see options'
-
-
+                    reply = self.set_rule(data[1], data[2], sreader.get_extra_info('peername')[0])
 
                 elif data[0] == "ir" and (data[1] == "tv" or data[1] == "ac"):
-                    for i in Config.config.devices:
-                        if i.device == "ir_blaster":
-                            if not data[1] in i.codes:
-                                reply = 'Error: No codes found for target "{}"'.format(data[1])
-                            else:
-                                if not data[2]:
-                                    reply = 'Error: Please specify which key to simulate'
-                                else:
-                                    i.send(data[1], data[2])
-                                    reply = 'OK'
-
-                    if not reply:
-                        reply = 'Error: No IR blaster configured'
-
-
+                    reply = self.ir_key(data[1], data[2])
 
                 elif data[0] == "ir" and data[1] == "backlight":
-                    if not (data[2] == "on" or data[2] == "off"):
-                        reply = 'Error: Backlight setting must be "on" or "off"'
-                    else:
-                        for i in Config.config.devices:
-                            if i.device == "ir_blaster":
-                                i.backlight(data[2])
-                                reply = 'OK'
-
-                        if not reply:
-                            reply = 'Error: No IR blaster configured'
-
-
+                    reply = self.ir_backlight(data[2])
 
                 else:
                     print(f"API: Received invalid command from {sreader.get_extra_info('peername')[0]}")
