110349e791fe105242710bf87d9526c5059f996b
Josh Medeiros <josh@jamedeus.com>
2022-03-27 12:50:01 -0700

    Full Relay rewrite: Now uses Tasmota HTTP API endpoints, abandoned old plan (flash micropython to Sonoff relays).

--- devices/Relay.py
+++ devices/Relay.py
@@ -1,8 +1,5 @@
-import uasyncio as asyncio
-import socket
 import logging
-import time
-import gc
+import urequests
 from Device import Device
 
 # Set name for module's log lines
@@ -16,7 +13,6 @@ class Relay(Device):
         super().__init__(name, device_type, enabled, current_rule, scheduled_rule)
 
         self.ip = ip
-        self.integration_running = False
 
         log.info(f"Instantiated Relay named {self.name}: ip = {self.ip}")
 
@@ -33,25 +29,11 @@ class Relay(Device):
 
 
 
-    def enable(self):
-        super().enable()
-
-        # If sensor relies on desktop to turn lights off and behavior was overriden when desktop was disabled: revert.
-        if self.integration_running:
-            for sensor in self.triggered_by:
-                if sensor.scheduled_rule == "None":
-                    sensor.current_rule = sensor.scheduled_rule  # Revert to scheduled rule once desktop is enabled again
-
-
-
-    def disable(self):
-        super().disable()
-
-        # If sensor relies on desktop to turn lights off, override with 15 minute timeout so lights don't get stuck on
-        if self.integration_running:
-            for sensor in self.triggered_by:
-                if sensor.current_rule == "None":
-                    sensor.current_rule = "15"
+    def check_state(self):
+        try:
+            return urequests.get('http://' + str(self.ip) + '/cm?cmnd=Power').json()["POWER"]
+        except OSError:
+            return "Network Error"
 
 
 
@@ -65,81 +47,24 @@ class Relay(Device):
         if self.current_rule == "off" and state == 1:
             return True # Tell sensor that send succeeded so it doesn't retry forever
         else:
-            try:
-                s = socket.socket()
-                s.settimeout(10)
-                print(f"{self.name}: Relay state = {state}")
-                s.connect((self.ip, 4200))
-                if state:
-                    s.send("on".encode())
-                else:
-                    s.send("off".encode())
-                s.close()
-                log.debug("Relay.send finished")
 
-                return True # Tell calling function that request succeeded
-            except OSError:
-                # Desktop is either off or at login screen - lie that request succeeded so calling function can proceed (temp fix)
-                return True
-                ## Desktop is either off or at login screen - disable device until it comes back online
-                #self.disable()
-            except:
-                return False # Tell calling function that request failed, will retry until it succeeds
-            # TODO - receive response (msg OK/Invalid), log errors
-
-
-
-    async def desktop_integration(self):
-        print('\nDesktop integration running.\n')
-        log.info("Desktop integration running")
-        self.server = await asyncio.start_server(self.desktop_integration_client, host='0.0.0.0', port=4200, backlog=5)
-
-        # TODO add handshake here, try to connect to self.ip until successful. Desktop gets node IP from handshake, remove hardcoded IP in desktop-integration.py
-
-        while True:
-            await asyncio.sleep(100)
-
-
-
-    async def desktop_integration_client(self, sreader, swriter):
-        try:
-            while True:
+            if state:
                 try:
-                    res = await asyncio.wait_for(sreader.readline(), timeout=10)
-                except asyncio.TimeoutError:
-                    res = b''
-                if res == b'':
-                    raise OSError
+                    response = urequests.get('http://' + str(self.ip) + '/cm?cmnd=Power%20On')
+                    print(f"{self.name}: Turned on")
+                except OSError:
+                    # Wifi interruption, send failed
+                    return False
 
-                data = res.decode()
+            elif not state:
+                try:
+                    response = urequests.get('http://' + str(self.ip) + '/cm?cmnd=Power%20Off')
+                    print(f"{self.name}: Turned on")
+                except OSError:
+                    # Wifi interruption, send failed
+                    return False
 
-                if data == "on": # Unsure if this will be used - currently desktop only turns lights off (when monitors sleep)
-                    print("Desktop turned lights ON")
-                    log.info("Desktop turned lights ON")
-                    # Set sensor instance attributes so it knows that desktop changed state
-                    for sensor in self.triggered_by:
-                        sensor.state = True
-                        sensor.motion = True
-                elif data == "off": # Allow main loop to continue when desktop turns lights off
-                    print("Desktop turned lights OFF")
-                    log.info("Desktop turned lights OFF")
-                    # Set sensor instance attributes so it knows that desktop changed state
-                    for sensor in self.triggered_by:
-                        sensor.state = False
-                        sensor.motion = False
-                elif data == "enable":
-                    print("Desktop re-enabled (user logged in)")
-                    log.debug("Desktop re-enabled (user logged in)")
-                    self.enable()
-
-                elif data == "disable":
-                    print("Desktop disabled (at login screen)")
-                    log.debug("Desktop disabled (at login screen)")
-                    self.disable()
-
-                # Prevent running out of mem after repeated requests
-                gc.collect()
-
-        except OSError:
-            pass
-        await sreader.wait_closed()
+            if response.status_code == 200:
+                return True
+            else:
+                return False
