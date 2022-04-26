7c74457a3c137e9e8ae0cc88201f3836436260c3
Josh Medeiros <josh@jamedeus.com>
2022-04-25 22:31:53 -0700

    current_rule is now a dict containing seperate 'on' and 'off' rules (both lists). Calling send(1) uses 'on', send(0) uses 'off'. The keyword 'ignore' can be used to disable either, preserving original functionality.

--- devices/ApiTarget.py
+++ devices/ApiTarget.py
@@ -21,22 +21,42 @@ class ApiTarget(Device):
     # TODO test enable/disable, see if methods need to be subclassed (they set device's state, could block loop from sending/send at incorrect time)
 
 
-    # Takes list containing entire API request
+    # Takes dict containing 2 entries named "on" and "off"
+    # Both entries are lists containing a full API request
+    # "on" sent when self.send(1) called, "off" when self.send(0) called
     def rule_validator(self, rule):
-        if not isinstance(rule, list):
+        if not isinstance(rule, dict):
             return False
 
-        if rule[0] in ['reboot', 'clear_log'] and len(rule) == 1:
-            return rule
+        # Reject if more than 2 sub-rules
+        if not len(rule) == 2:
+            return False
 
-        elif rule[0] in ['enable', 'disable', 'condition_met', 'trigger_sensor'] and (rule[1].startswith("device") or rule[1].startswith("sensor")) and len(rule) == 2:
-            return rule
+        for i in rule:
+            # Index must be "on" or "off"
+            if not i == "on" or i == "off":
+                return False
 
-        elif rule[0] in ['enable_in', 'disable_in', 'set_rule'] and (rule[1].startswith("device") or rule[1].startswith("sensor")) and len(rule) == 3:
-            return rule
+            if not isinstance(rule[i], list):
+                return False
+
+            # "ignore" is not a valid command, it allows only using on/off and ignoring the other
+            if rule[i][0] in ['reboot', 'clear_log', 'ignore'] and len(rule[i]) == 1:
+                continue
+
+            elif rule[i][0] in ['enable', 'disable', 'condition_met', 'trigger_sensor'] and (rule[i][1].startswith("device") or rule[i][1].startswith("sensor")) and len(rule[i]) == 2:
+                continue
+
+            elif rule[i][0] in ['enable_in', 'disable_in', 'set_rule'] and (rule[i][1].startswith("device") or rule[i][1].startswith("sensor")) and len(rule[i]) == 3:
+                continue
+
+            else:
+                # Did not match any valid patterns
+                return False
 
         else:
-            return False
+            # Iteration finished without a return False, rule is valid
+            return rule
 
 
 
@@ -54,18 +74,28 @@ class ApiTarget(Device):
 
 
     def send(self, state=1):
-        print("send method")
-        if not state == 1:
-            # Cannot be turned off
-            # TODO add on_rule and off_rule (either can be null), will allow using this with switch sensor_type etc.
-            return True
 
-        asyncio.create_task(self.request(self.current_rule))
+        # "On" rule
+        if state:
+            if self.current_rule["on"][0] == "ignore":
+                # If rule is ignore, do nothing
+                return True
 
-        # Reset motion sensor to allow retriggering the remote motion sensor (restarts reset timer)
-        # Retrigger when motion = True only restarts sensor's own resetTimer, but does not send another API command
-        for sensor in self.triggered_by:
-            if sensor.sensor_type == "pir":
-                sensor.motion = False
+            asyncio.create_task(self.request(self.current_rule["on"]))
 
+            # Reset motion sensor to allow retriggering the remote motion sensor (restarts reset timer)
+            # Retrigger when motion = True only restarts sensor's own resetTimer, but does not send another API command
+            for sensor in self.triggered_by:
+                if sensor.sensor_type == "pir":
+                    sensor.motion = False
+
+        # "Off" rule
+        else:
+            if self.current_rule["off"][0] == "ignore":
+                # If rule is ignore, do nothing
+                return True
+
+            asyncio.create_task(self.request(self.current_rule["off"]))
+
+        # Tells main loop send succeeded
         return True
