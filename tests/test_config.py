8e8660494993099e0756ede19b405a72dc3e136d
Josh Medeiros <josh@jamedeus.com>
2022-05-20 16:52:55 -0700

    Split into seperate tests, output now much more useful (tells which test failed).

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,19 +11,23 @@ class TestConfig(unittest.TestCase):
     def test_initialization(self):
         loaded_json = {'device1': {'max': 1023, 'min': '0', 'schedule': {'sunrise': 0, 'sunset': 32}, 'type': 'pwm', 'pin': 4, 'default_rule': 32}, 'wifi': {'ssid': 'jamnet', 'password': 'cjZY8PTa4ZQ6S83A'}, 'metadata': {'id': 'Upstairs bathroom', 'location': 'Under counter', 'floor': '2'}, 'sensor1': {'schedule': {'10:00': '5', '22:00': '5'}, 'pin': 15, 'targets': ['device1'], 'type': 'pir', 'default_rule': 5}}
 
-        config = Config(loaded_json)
+        self.config = Config(loaded_json)
 
+    def test_wifi_connected(self):
         # Check if network connected successfully
         self.assertTrue(network.WLAN().isconnected())
 
+    def test_indicator_led(self):
         # Make sure LED went off
         led = Pin(2, Pin.OUT)
         print(f"State = {led.value()}")
         self.assertEqual(led.value(), 0)
 
+    def test_api_calls(self):
         # Confirm API call succeeded
-        self.assertIsNotNone(config.sunrise)
+        self.assertIsNotNone(self.config.sunrise)
 
+    def test_reboot_timer(self):
         # Confirm reboot_timer stopped by reading remaining time twice with delay in between
         reboot_timer = Timer(2)
         first = reboot_timer.value()
@@ -31,32 +35,37 @@ class TestConfig(unittest.TestCase):
         second = reboot_timer.value()
         self.assertEqual(first, second)
 
+    def test_device_instantiation(self):
         # Confirm correct devices were instantiated
-        self.assertEqual(len(config.devices), 1)
-        self.assertEqual(config.devices[0].device_type, 'pwm')
-        self.assertTrue(config.devices[0].enabled)
-        self.assertEqual(config.devices[0].triggered_by[0], config.sensors[0])
+        self.assertEqual(len(self.config.devices), 1)
+        self.assertEqual(self.config.devices[0].device_type, 'pwm')
+        self.assertTrue(self.config.devices[0].enabled)
+        self.assertEqual(self.config.devices[0].triggered_by[0], self.config.sensors[0])
 
+    def test_sensor_instantiation(self):
         # Confirm correct sensors were instantiated
-        self.assertEqual(len(config.sensors), 1)
-        self.assertEqual(config.sensors[0].sensor_type, 'pir')
-        self.assertTrue(config.sensors[0].enabled)
-        self.assertEqual(config.sensors[0].targets[0], config.devices[0])
+        self.assertEqual(len(self.config.sensors), 1)
+        self.assertEqual(self.config.sensors[0].sensor_type, 'pir')
+        self.assertTrue(self.config.sensors[0].enabled)
+        self.assertEqual(self.config.sensors[0].targets[0], self.config.devices[0])
 
+    def test_group_instantiation(self):
         # Confirm group created correctly
-        self.assertEqual(len(config.groups), 1)
-        self.assertEqual(config.groups["group1"]["targets"][0], config.devices[0])
-        self.assertEqual(config.groups["group1"]["triggers"][0], config.sensors[0])
+        self.assertEqual(len(self.config.groups), 1)
+        self.assertEqual(self.config.groups["group1"]["targets"][0], self.config.devices[0])
+        self.assertEqual(self.config.groups["group1"]["triggers"][0], self.config.sensors[0])
 
+    def test_reload_timer(self):
         # Confirm reload_config timer is running
-        config_timer = Timer(1)
-        first = config_timer.value()
+        self.config_timer = Timer(1)
+        first = self.config_timer.value()
         time.sleep_ms(1)
-        second = config_timer.value()
+        second = self.config_timer.value()
         self.assertNotEqual(first, second)
 
-        # Test find method
-        self.assertEqual(config.find("device1"), config.devices[0])
+    def test_find_method(self):
+        self.assertEqual(self.config.find("device1"), self.config.devices[0])
 
+    def test_get_status_method(self):
         # Test get_status method
-        self.assertEqual(type(config.get_status()), dict)
+        self.assertEqual(type(self.config.get_status()), dict)
