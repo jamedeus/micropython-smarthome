3b0161fc385610851a6fdc23b1298382382eafbb
Josh Medeiros <josh@jamedeus.com>
2022-05-21 15:07:31 -0700

    Add SoftwareTimer to test environment, added to event loop before tests to enable testing timer-related functions.

--- tests/unit_test_boot.py
+++ tests/unit_test_boot.py
@@ -2,43 +2,74 @@ import unittest
 import os
 import sys
 import gc
+import uasyncio as asyncio
 
 # Add tests to path
 sys.path.insert(len(sys.path), '/tests')
 
-# Create empty test suite
-suite = unittest.TestSuite()
 
-# Import all modules under /tests
-for test in os.listdir('tests'):
-    module = __import__(test.split(".")[0])
 
-    # Find all classes in module, add to test suite
-    for i in dir(module):
-        c = getattr(module, i)
-        try:
-            if issubclass(c, unittest.TestCase):
-                suite.addTest(c)
+def find_tests():
+    # Create empty test suite
+    suite = unittest.TestSuite()
 
-        except TypeError:
-            pass
+    # Import all modules under /tests
+    for test in os.listdir('tests'):
+        module = __import__(test.split(".")[0])
 
-print("\n\n---BEGIN TESTING---\n\n")
+        # Find all classes in module, add to test suite
+        for i in dir(module):
+            c = getattr(module, i)
+            try:
+                if issubclass(c, unittest.TestCase):
+                    suite.addTest(c)
 
-gc.collect()
-runner = unittest.TestRunner()
-result = runner.run(suite)
-gc.collect()
+            except TypeError:
+                pass
 
-print("\n\n---TESTING COMPLETE---\n\n")
+    return suite
 
 
 
-# After testing: listen for upload, reboot when new code received
-import network, webrepl, machine
-import uasyncio as asyncio
+async def run_tests(suite):
+    print("\n\n---BEGIN TESTING---\n\n")
+
+    gc.collect()
+    runner = unittest.TestRunner()
+    result = runner.run(suite)
+    gc.collect()
+
+    print("\n\n---TESTING COMPLETE---\n\n")
+
+    asyncio.create_task(post_test())
+
+
+
+async def post_test():
+    # After testing: listen for upload, reboot when new code received
+    import network, webrepl
+
+    # Connect to wifi
+    wlan = network.WLAN()
+    wlan.active(True)
+    if not wlan.isconnected():
+        import json
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+        wlan.connect(config["wifi"]["ssid"], config["wifi"]["password"])
+
+    # Wait until connected before starting webrepl
+    while not wlan.isconnected():
+        continue
+    else:
+        webrepl.start()
+
+    asyncio.create_task(disk_monitor())
+
+
 
 async def disk_monitor():
+    from machine import reset
     # Get filesize/modification time (to detect upload in future)
     old = os.stat("boot.py")
 
@@ -46,29 +77,35 @@ async def disk_monitor():
         # Reboot if file changed on disk
         if not os.stat("boot.py") == old:
             await asyncio.sleep(1) # Prevents webrepl_cli.py from hanging after upload (esp reboots too fast)
-            machine.reset()
+            reset()
         else:
             await asyncio.sleep(1) # Only check once per second
 
-# Connect to wifi
-wlan = network.WLAN()
-wlan.active(True)
-if not wlan.isconnected():
-    import json
-    with open('config.json', 'r') as file:
-        config = json.load(file)
-    wlan.connect(config["wifi"]["ssid"], config["wifi"]["password"])
 
-# Wait until connected before starting webrepl
-while not wlan.isconnected():
-    continue
-else:
-    webrepl.start()
+
+# Get test suite containing all modules from /tests/ directory
+suite = find_tests()
+
+gc.collect()
+
+# Import + initialize API
+#from Api import app
+
+# Import SoftwareTimer instance, add to async loop below
+from SoftwareTimer import timer
 
 # Create main loop, add tasks
 loop = asyncio.get_event_loop()
-# Disk_monitor reboots when new code upload received
-loop.create_task(disk_monitor())
+
+# SoftwareTimer loop checks if timers have expired, applies actions
+loop.create_task(timer.loop())
+# Start API server, await requests
+#loop.create_task(app.run())
+
+# Add test runner last, ensure SoftwareTimer and API are ready for testing
+loop.create_task(run_tests(suite))
+
+gc.collect()
 
 # Run
 loop.run_forever()
