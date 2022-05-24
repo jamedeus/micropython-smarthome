7218a7266e5ec1edea844408ec7c71cc6cfa3ee7
Josh Medeiros <josh@jamedeus.com>
2022-05-23 23:21:50 -0700

    Optimized to reduce memory fragmentation: now runs each test individually rather than single large test suite. Records results in dict and prints summary after testing completed, more readable than library's output. Added Api module to event loop (after optimizations no longer causes tests to fail due to mem fragmentation).

--- tests/unit_test_boot.py
+++ tests/unit_test_boot.py
@@ -9,37 +9,57 @@ sys.path.insert(len(sys.path), '/tests')
 
 
 
-def find_tests():
-    # Create empty test suite
-    suite = unittest.TestSuite()
+async def run_tests():
+    print("\n\n---BEGIN TESTING---\n\n")
+
+    runner = unittest.TestRunner()
+
+    # Record total tests, failed, errored, and skipped for each test case
+    detailed_results = {}
 
     # Import all modules under /tests
     for test in os.listdir('tests'):
         module = __import__(test.split(".")[0])
 
-        # Find all classes in module, add to test suite
+        # Find and run test case classes in module
         for i in dir(module):
             c = getattr(module, i)
             try:
                 if issubclass(c, unittest.TestCase):
+                    suite = unittest.TestSuite()
                     suite.addTest(c)
+                    gc.collect()
+                    result = runner.run(suite)
 
+                    # Record results
+                    detailed_results[test] = {}
+                    detailed_results[test]["tests_run"] = result.testsRun
+                    detailed_results[test]["failed"] = result.failuresNum
+                    detailed_results[test]["errors"] = result.errorsNum
+                    detailed_results[test]["skipped"] = result.skippedNum
+
+                    # Reduce mem fragmentation when running large number of tests
+                    del suite
+                    gc.collect()
             except TypeError:
                 pass
 
-    return suite
-
-
-
-async def run_tests(suite):
-    print("\n\n---BEGIN TESTING---\n\n")
-
-    gc.collect()
-    runner = unittest.TestRunner()
-    result = runner.run(suite)
-    gc.collect()
-
     print("\n\n---TESTING COMPLETE---\n\n")
+
+    total_tests = 0
+    total_failed = 0
+
+    # Print results summary
+    for i in detailed_results:
+        print(i.split(".")[0].split("_")[1])
+        print(f" - Tests:         {detailed_results[i]["tests_run"]}")
+        print(f"   - Failed:      {detailed_results[i]["failed"]}")
+        print(f"   - Errored:     {detailed_results[i]["errors"]}")
+        print(f"   - Skipped:     {detailed_results[i]["skipped"]}\n")
+        total_tests += detailed_results[i]["tests_run"]
+        total_failed += detailed_results[i]["failed"]
+
+    print(f"Total:  {total_tests}\nFailed: {total_failed}\n\n")
 
     asyncio.create_task(post_test())
 
@@ -83,13 +103,8 @@ async def disk_monitor():
 
 
 
-# Get test suite containing all modules from /tests/ directory
-suite = find_tests()
-
-gc.collect()
-
 # Import + initialize API
-#from Api import app
+from Api import app
 
 # Import SoftwareTimer instance, add to async loop below
 from SoftwareTimer import timer
@@ -100,10 +115,10 @@ loop = asyncio.get_event_loop()
 # SoftwareTimer loop checks if timers have expired, applies actions
 loop.create_task(timer.loop())
 # Start API server, await requests
-#loop.create_task(app.run())
+loop.create_task(app.run())
 
 # Add test runner last, ensure SoftwareTimer and API are ready for testing
-loop.create_task(run_tests(suite))
+loop.create_task(run_tests())
 
 gc.collect()
 
