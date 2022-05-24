3345bcf76e87a2b315797e460ae51684e3244713
Josh Medeiros <josh@jamedeus.com>
2022-05-24 16:10:50 -0700

    Added unit tests for Thermostat class.

---
+++ tests/test_thermostat.py
@@ -0,0 +1,62 @@
+import unittest
+from Thermostat import Thermostat
+
+
+
+class TestThermostat(unittest.TestCase):
+    def test_instantiation(self):
+        self.instance = Thermostat("sensor1", "si7021", True, 74, 74, [])
+        self.assertIsInstance(self.instance, Thermostat)
+        self.assertTrue(self.instance.enabled)
+
+    def test_rule_validation_valid(self):
+        self.assertEqual(self.instance.rule_validator(65), 65)
+        self.assertEqual(self.instance.rule_validator(80), 80)
+        self.assertEqual(self.instance.rule_validator("72"), 72)
+        self.assertEqual(self.instance.rule_validator("Disabled"), "Disabled")
+
+    def test_rule_validation_invalid(self):
+        self.assertFalse(self.instance.rule_validator(64))
+        self.assertFalse(self.instance.rule_validator(81))
+        self.assertFalse(self.instance.rule_validator([72]))
+        self.assertFalse(self.instance.rule_validator({72:72}))
+        self.assertFalse(self.instance.rule_validator(True))
+        self.assertFalse(self.instance.rule_validator(None))
+        self.assertFalse(self.instance.rule_validator("string"))
+
+    def test_rule_change(self):
+        self.assertTrue(self.instance.set_rule(75))
+        self.assertEqual(self.instance.current_rule, 75)
+
+    def test_disable(self):
+        self.instance.disable()
+        self.assertFalse(self.instance.enabled)
+
+    def test_enable(self):
+        self.instance.enable()
+        self.assertTrue(self.instance.enabled)
+
+    def test_disable_by_rule_change(self):
+        self.instance.set_rule("Disabled")
+        self.assertFalse(self.instance.enabled)
+
+    def test_enable_by_rule_change(self):
+        self.instance.set_rule(70)
+        self.assertTrue(self.instance.enabled)
+
+    def test_sensor(self):
+        self.assertIsInstance(self.instance.fahrenheit(), float)
+        self.assertIsInstance(self.instance.temp_sensor.temperature, float)
+        self.assertIsInstance(self.instance.temp_sensor.relative_humidity, float)
+
+    def test_condition_met(self):
+        current = self.instance.fahrenheit()
+
+        self.instance.set_rule(current)
+        self.assertEqual(self.instance.condition_met(), None)
+
+        self.instance.set_rule(current-2)
+        self.assertFalse(self.instance.condition_met())
+
+        self.instance.set_rule(current+2)
+        self.assertTrue(self.instance.condition_met())
