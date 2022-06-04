e21cc2bf8d2b19791dc2c9a18eed3c8b92426a8e
Josh Medeiros <josh@jamedeus.com>
2022-06-03 23:51:51 -0700

    Added script that automatically runs all client-side tests (api_client.py, provision.py). Optional 'upload' argument provisions test node with correct config file, then runs all tests.

---
+++ runtests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $1 == "upload" ]]; then
+    python3 provision.py -c tests/config.json -ip 192.168.1.223
+    sleep 30
+fi
+
+python3 -m unittest discover -s tests -p client_test*.py
