f7c90062800c15cdffbc4a532c9d3bc5e993ab3d
Josh Medeiros <josh@jamedeus.com>
2022-06-11 16:59:33 -0700

    Created project + node_configuration app. Added 'Node' model that stores the same information as nodes.json. Enabled admin page access.

---
+++ frontend/node_configuration/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class Node(models.Model):
+
+    def __str__(self):
+        return self.friendly_name
+
+    friendly_name = models.CharField(max_length=50)
+
+    ip = models.GenericIPAddressField(protocol='IPv4')
+
+    config_file = models.FilePathField(path='/home/jamedeus/git/micropython-smarthome/config')
