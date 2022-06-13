ac409a0b0d4e04ffb318336914a4b490c7c1d0d3
Josh Medeiros <josh@jamedeus.com>
2022-06-12 22:07:35 -0700

    Now creates json object containing form data on submit. All device/sensor config templates now require integer argument, used to dynamically number all parameter names (device1-type, device1-defaultRule, etc).

--- frontend/node_configuration/urls.py
+++ frontend/node_configuration/urls.py
@@ -9,20 +9,20 @@ urlpatterns = [
 
     path('addSensor/<int:count>', views.addSensor, name='addSensor'),
 
-    path('sensorOptionsPir', views.sensorOptionsPir, name='sensorOptionsPir'),
-    path('sensorOptionsSwitch', views.sensorOptionsSwitch, name='sensorOptionsSwitch'),
-    path('sensorOptionsDummy', views.sensorOptionsDummy, name='sensorOptionsDummy'),
-    path('sensorOptionsDesktop', views.sensorOptionsDesktop, name='sensorOptionsDesktop'),
-    path('sensorOptionsSi7021', views.sensorOptionsSi7021, name='sensorOptionsSi7021'),
+    path('sensorOptionsPir/<int:count>', views.sensorOptionsPir, name='sensorOptionsPir'),
+    path('sensorOptionsSwitch/<int:count>', views.sensorOptionsSwitch, name='sensorOptionsSwitch'),
+    path('sensorOptionsDummy/<int:count>', views.sensorOptionsDummy, name='sensorOptionsDummy'),
+    path('sensorOptionsDesktop/<int:count>', views.sensorOptionsDesktop, name='sensorOptionsDesktop'),
+    path('sensorOptionsSi7021/<int:count>', views.sensorOptionsSi7021, name='sensorOptionsSi7021'),
 
     path('addDevice/<int:count>', views.addDevice, name='addDevice'),
 
-    path('deviceOptionsDimmer', views.deviceOptionsDimmer, name='deviceOptionsDimmer'),
-    path('deviceOptionsBulb', views.deviceOptionsBulb, name='deviceOptionsBulb'),
-    path('deviceOptionsRelay', views.deviceOptionsRelay, name='deviceOptionsRelay'),
-    path('deviceOptionsDumbRelay', views.deviceOptionsDumbRelay, name='deviceOptionsDumbRelay'),
-    path('deviceOptionsDesktop', views.deviceOptionsDesktop, name='deviceOptionsDesktop'),
-    path('deviceOptionsPwm', views.deviceOptionsPwm, name='deviceOptionsPwm'),
-    path('deviceOptionsMosfet', views.deviceOptionsMosfet, name='deviceOptionsMosfet'),
-    path('deviceOptionsApi', views.deviceOptionsApi, name='deviceOptionsApi'),
+    path('deviceOptionsDimmer/<int:count>', views.deviceOptionsDimmer, name='deviceOptionsDimmer'),
+    path('deviceOptionsBulb/<int:count>', views.deviceOptionsBulb, name='deviceOptionsBulb'),
+    path('deviceOptionsRelay/<int:count>', views.deviceOptionsRelay, name='deviceOptionsRelay'),
+    path('deviceOptionsDumbRelay/<int:count>', views.deviceOptionsDumbRelay, name='deviceOptionsDumbRelay'),
+    path('deviceOptionsDesktop/<int:count>', views.deviceOptionsDesktop, name='deviceOptionsDesktop'),
+    path('deviceOptionsPwm/<int:count>', views.deviceOptionsPwm, name='deviceOptionsPwm'),
+    path('deviceOptionsMosfet/<int:count>', views.deviceOptionsMosfet, name='deviceOptionsMosfet'),
+    path('deviceOptionsApi/<int:count>', views.deviceOptionsApi, name='deviceOptionsApi'),
 ]
--- frontend/node_configuration/views.py
+++ frontend/node_configuration/views.py
@@ -16,30 +16,30 @@ def addSensor(request, count):
 
     return HttpResponse(template.render({'context': count}, request))
 
-def sensorOptionsPir(request):
+def sensorOptionsPir(request, count):
     template = loader.get_template('node_configuration/pir-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def sensorOptionsSwitch(request):
+def sensorOptionsSwitch(request, count):
     template = loader.get_template('node_configuration/switch-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def sensorOptionsDummy(request):
+def sensorOptionsDummy(request, count):
     template = loader.get_template('node_configuration/dummy-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def sensorOptionsDesktop(request):
+def sensorOptionsDesktop(request, count):
     template = loader.get_template('node_configuration/desktop-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def sensorOptionsSi7021(request):
+def sensorOptionsSi7021(request, count):
     template = loader.get_template('node_configuration/si7021-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
 
 
@@ -48,42 +48,42 @@ def addDevice(request, count):
 
     return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsDimmer(request):
+def deviceOptionsDimmer(request, count):
     template = loader.get_template('node_configuration/dimmer-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsBulb(request):
+def deviceOptionsBulb(request, count):
     template = loader.get_template('node_configuration/bulb-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsRelay(request):
+def deviceOptionsRelay(request, count):
     template = loader.get_template('node_configuration/relay-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsDumbRelay(request):
+def deviceOptionsDumbRelay(request, count):
     template = loader.get_template('node_configuration/dumb-relay-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsDesktop(request):
+def deviceOptionsDesktop(request, count):
     template = loader.get_template('node_configuration/desktop-target-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsPwm(request):
+def deviceOptionsPwm(request, count):
     template = loader.get_template('node_configuration/pwm-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsMosfet(request):
+def deviceOptionsMosfet(request, count):
     template = loader.get_template('node_configuration/mosfet-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
 
-def deviceOptionsApi(request):
+def deviceOptionsApi(request, count):
     template = loader.get_template('node_configuration/api-target-config.html')
 
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'context': count}, request))
