jamedeus/micropython-smarthome

commit 3b2d19066e5c8b22eca4948ea59ff370a2b81275

Bug fixes: Prevent SoftwareTimer from crashing if existing timer canceled while iterating queue. Fix issue where Tplink fade starting right when another fade ends caused 2 callback timers for each step, crashing timer when fade ended (last timer calls fade after first timer defaults parameters to 'None', resulting in comparison between None and int).

SoftwareTimer.py
@@ -56,7 +56,7 @@ class SoftwareTimer():
                     break
 
         # Callers are only allowed 1 timer each (scheduler is exempt) - delete any existing timers with same name before adding
-        if not (name == "scheduler" or name == "fade"):
+        if not (name == "scheduler" or "fade" in name):
             for i in self.schedule:
                 if name in self.schedule[i]:
                     del self.schedule[i]
@@ -106,10 +106,13 @@ class SoftwareTimer():
         while True:
             if not self.pause:
                 for i in self.queue:
-                    # Run actions for all expired rules, add to list to be removed from queue
-                    if self.epoch_now() >= i:
-                        self.schedule[i][1]() # Run action
-                        self.delete.append(i)
+                    try:
+                        # Run actions for all expired rules, add to list to be removed from queue
+                        if self.epoch_now() >= i:
+                            self.schedule[i][1]() # Run action
+                            self.delete.append(i)
+                    except KeyError:
+                        pass # Prevent crash if rule was removed by self.cancel while loop running
 
                     else:
                         # First unexpired rule found

devices/Tplink.py
@@ -74,7 +74,11 @@ class Tplink(Device):
                     self.fade_started = SoftwareTimer.timer.epoch_now()
                     self.fade_start_bright = brightness
 
-                    SoftwareTimer.timer.create(self.fade_period, self.fade, "fade")
+                    # Cancel existing fade timers (prior fade may be in progress)
+                    SoftwareTimer.timer.cancel(self.name + "_fade")
+
+                    # Create fade timer
+                    SoftwareTimer.timer.create(self.fade_period, self.fade, self.name + "_fade")
 
                     # Return starting point (will be set as current rule by device.set_rule)
                     return brightness
@@ -163,6 +167,7 @@ class Tplink(Device):
         if not self.enabled:
             return True
 
+        # Fade to next step (unless fade already complete)
         if not self.fade_target == int(self.current_rule):
 
             # Use starting time, current time, and time for each step (fade_period) to determine how many steps should have been taken
@@ -180,11 +185,8 @@ class Tplink(Device):
 
             self.set_rule(new_rule)
 
-            # Sleep until next step
-            next_step = int(self.fade_period - ((SoftwareTimer.timer.epoch_now() - self.fade_started) % self.fade_period))
-            SoftwareTimer.timer.create(next_step, self.fade, "fade")
-
-        else:
+        # Check if fade complete after step
+        if self.fade_target == int(self.current_rule):
             # Animation complete
             self.scheduled_rule = self.current_rule
             self.fade_target = None
@@ -194,3 +196,8 @@ class Tplink(Device):
 
             if self.current_rule == 0:
                 self.state = False
+
+        else:
+            # Sleep until next step
+            next_step = int(self.fade_period - ((SoftwareTimer.timer.epoch_now() - self.fade_started) % self.fade_period))
+            SoftwareTimer.timer.create(next_step, self.fade, self.name + "_fade")

commit 3b203f2b70577beeb76827008486e6981de116b9

Fixed bug causing freeze after last rule in queue run (next_rule carried over to next loop resulting in negative timestamp, resulting in infinite loop).
Fixed minor bug introduced in 3b2d1906 preventing multiple expired rules running in the same loop iteration.

SoftwareTimer.py
@@ -108,13 +108,12 @@ class SoftwareTimer():
                         if self.epoch_now() >= i:
                             self.schedule[i][1]() # Run action
                             self.delete.append(i)
+                        else:
+                            # First unexpired rule found
+                            next_rule = i
+                            break
                     except KeyError:
                         pass # Prevent crash if rule was removed by self.cancel while loop running
-
-                    else:
-                        # First unexpired rule found
-                        next_rule = i
-                        break
 
                 # Delete rules that were just run
                 for i in self.delete:
@@ -129,6 +128,9 @@ class SoftwareTimer():
                 # Get time until next rule due
                 try:
                     period = int(next_rule - self.epoch_now())
+
+                    # Prevent carrying over to next loop after running last queue item, resulting in negative period
+                    del next_rule
                 except NameError:
                     # No tasks in queue - sleep for 1 hour (will be interrupted if task is added)
                     period = 3600000