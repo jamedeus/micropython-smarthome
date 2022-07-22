8ea96a89f80a85593b6ecf913b1b2559d46b8f3c
Josh Medeiros <josh@jamedeus.com>
2022-07-21 18:17:07 -0700

    Frontend now supports nicknames for devices and sensors, replaces unintuitive sequential names (device1, device2, ...).
    Api Card view now displays nickname as title of each card.
    
    New config and edit config pages now have nickname field for each device/sensor.
    
    Config pages: Merged templates for each sensor/device type (pir-config.html, pwm-config.html, etc) into main file as template literals, no longer makes request each time user changes device/sensor type or adds new device/sensor. Removed from URLs and views.

--- frontend/node_configuration/urls.py
+++ frontend/node_configuration/urls.py
@@ -18,27 +18,6 @@ urlpatterns = [
     path('configure_page2', views.configure_page2, name='configure_page2'),
     path('configure_page3', views.configure_page3, name='configure_page3'),
 
-    path('addSensor/<int:count>', views.addSensor, name='addSensor'),
-    path('edit_config/addSensor/<int:count>', views.addSensor, name='addSensor'),
-
-    path('sensorOptionsPir/<int:count>', views.sensorOptionsPir, name='sensorOptionsPir'),
-    path('sensorOptionsSwitch/<int:count>', views.sensorOptionsSwitch, name='sensorOptionsSwitch'),
-    path('sensorOptionsDummy/<int:count>', views.sensorOptionsDummy, name='sensorOptionsDummy'),
-    path('sensorOptionsDesktop/<int:count>', views.sensorOptionsDesktop, name='sensorOptionsDesktop'),
-    path('sensorOptionsSi7021/<int:count>', views.sensorOptionsSi7021, name='sensorOptionsSi7021'),
-
-    path('addDevice/<int:count>', views.addDevice, name='addDevice'),
-    path('edit_config/addDevice/<int:count>', views.addDevice, name='addDevice'),
-
-    path('deviceOptionsDimmer/<int:count>', views.deviceOptionsDimmer, name='deviceOptionsDimmer'),
-    path('deviceOptionsBulb/<int:count>', views.deviceOptionsBulb, name='deviceOptionsBulb'),
-    path('deviceOptionsRelay/<int:count>', views.deviceOptionsRelay, name='deviceOptionsRelay'),
-    path('deviceOptionsDumbRelay/<int:count>', views.deviceOptionsDumbRelay, name='deviceOptionsDumbRelay'),
-    path('deviceOptionsDesktop/<int:count>', views.deviceOptionsDesktop, name='deviceOptionsDesktop'),
-    path('deviceOptionsPwm/<int:count>', views.deviceOptionsPwm, name='deviceOptionsPwm'),
-    path('deviceOptionsMosfet/<int:count>', views.deviceOptionsMosfet, name='deviceOptionsMosfet'),
-    path('deviceOptionsApi/<int:count>', views.deviceOptionsApi, name='deviceOptionsApi'),
-
     # Argument is optional
     path('generateConfigFile', views.generateConfigFile, name='generateConfigFile'),
     path('generateConfigFile/<str:edit_existing>', views.generateConfigFile, name='generateConfigFile'),
