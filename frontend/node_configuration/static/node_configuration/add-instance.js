jamedeus/micropython-smarthome

commit 98a85be487a0e3a881ee125ce6bf518baab1e0b5

ApiTarget Modal refactor
Consistent ID syntax for buttons and hidden fields, no longer need conditional to set target.

New reload_rule function populates dropdowns to match existing rule from config format. FormData format no longer necessary, same syntax used everywhere.

Views now generate context with literal ApiTarget rules (json-encoded), edit_config returns ApiTarget rules in final config format. No longer needs conversion on backend, removed convert and reverse-convert functions.

Frontend schedule rules no longer need seperate function to add ApiTarget rule (value in field no longer needs to be converted).

frontend/node_configuration/static/node_configuration/add-instance.js
@@ -168,7 +168,7 @@ async function load_device_section(select) {
                     </div>
 
                     <div class="mb-2 text-center">
-                        <button id="device${index}-set-rule" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" disabled>Set rule</button>
+                        <button id="device${index}-default_rule-button" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" disabled>Set rule</button>
                     </div>
 
                     <div class="mb-2 text-center">

frontend/node_configuration/static/node_configuration/apiTargetOptions.js
@@ -13,12 +13,14 @@ function submit_api_rule(el) {
     // Remove friendly name, leave only instance id (device1, sensor1, etc)
     value["instance-on"] = value["instance-on"].split("-")[0]
     value["instance-off"] = value["instance-off"].split("-")[0]
-    document.getElementById(el.dataset.target).value = JSON.stringify(value);
+    // Convert form object to correct format, set value of hidden rule field
+    document.getElementById(el.dataset.target).value = convert_api_target_rule(value);
     var event = new Event('change');
     document.getElementById(el.dataset.target).dispatchEvent(event);
 };
 
 
+// Called when user changes target node in dropdown
 function api_target_selected(el) {
     if (el.value) {
         document.getElementById(el.id.split("-")[0] + "-set-rule").disabled = false;
@@ -149,12 +151,6 @@ function populate_sub_command_off(target) {
     // empty sub-command dropdown
     sub_command_select_off.length = 1;
 
-    console.log()
-    console.log(target)
-    console.log(instance_select_off.value)
-    console.log(selected)
-    console.log()
-
     var z = ApiTargetOptions[target][instance_select_off.value][selected];
 
     //display correct values
@@ -166,26 +162,16 @@ function populate_sub_command_off(target) {
 function open_rule_modal(el) {
     $('#api-rule-modal').modal('show')
 
-    if (el.id.endsWith("set_rule")) {
-        // Frontend (change rule option in device dropdown)
-        document.getElementById('submit-api-rule').dataset.target = el.id.split("-")[0] + "-rule";
-        var target = el.id.split("-")[0];
+    // Get target device ID, use to get options from ApiTargetOptions object
+    var target = el.id.split("-")[0];
 
-    } else if (el.id.endsWith("button")) {
-        // Frontend schedule rules section
-        document.getElementById('submit-api-rule').dataset.target = el.id.replace("button", "value");
-        var target = el.id.split("-")[1];
-
-    } else if (el.id.startsWith("device")) {
-        // Provisioning default rule (configure page 1)
-        document.getElementById('submit-api-rule').dataset.target = el.id.split("-")[0] + "-default_rule";
-        var target = document.getElementById(el.id.split("-")[0] + "-ip").value;
-
-    } else {
-        // Provisioning schedule rules (configure page 3)
-        document.getElementById('submit-api-rule').dataset.target = el.id.replace("button", "value");
-        var target = document.getElementById(el.id.split("-")[1] + "-ip").value;
+    // Options object has different syntax on provision page, use selected dropdown item as object key instead
+    if (!ApiTargetOptions[target]) {
+        target = document.getElementById(`${target}-ip`).selectedOptions[0].innerText;
     };
+
+    // Store ID of hidden rule field in submit button data attribute. User selection is copied to this field on submit
+    document.getElementById('submit-api-rule').dataset.target = el.id.replace("-button", "");
 
     // Clear all options from last time menu was opened
     instance_select_on.length = 1;
@@ -225,67 +211,87 @@ function open_rule_modal(el) {
     command_select_on.onchange = function() {populate_sub_command_on(target)};
     command_select_off.onchange = function() {populate_sub_command_off(target)};
 
-    // Load existing rule (if present) into dropdowns
+    // Re-populate dropdowns from existing rule (if present)
     try {
-        if (el.id.startsWith("device")) {
-            var restore = JSON.parse(document.getElementById(el.id.split("-")[0] + "-default_rule").value);
-        } else {
-            var restore = JSON.parse(document.getElementById(el.id.replace("button", "value")).value);
-        }
-        console.log(restore)
+        var restore = JSON.parse(document.getElementById(el.id.replace("-button", "")).value);
+        reload_rule(target, restore);
     } catch(err) {
-        // Skip if no rule set
-        console.log("No existing rule")
-        return
+        console.log("No existing rule");
+    };
+};
+
+// Reads existing rule, selects all corresponding dropdown options
+function reload_rule(target, rule) {
+    if (rule['on'][0] == 'ir_key') {
+        // IR commands have different syntax (no target instance, extra command)
+        // Replace command (ir_key) with instance (ir_blaster) and change order so dropdowns populate correctly
+        rule['on'].shift();
+        rule['on'].splice(1, 0, "ir_blaster");
     };
 
-    // Re-populate dropdowns from existing rule
+    // Populate instance (or IrBlaster)
     Array.from(instance_select_on.options).forEach( function(option) {
-        if (option.value.split("-")[0] == restore["instance-on"]) {
+        if (option.value.split("-")[0] == rule['on'][1]) {
             option.selected = true;
             populate_command_on(target);
         }
     });
 
-    Array.from(instance_select_off.options).forEach( function(option) {
-        if (option.value.split("-")[0] == restore["instance-off"]) {
-            option.selected = true;
-            populate_command_off(target);
-        }
-    });
-
+    // Populate command (or IrBlaster virtual remote)
     Array.from(command_select_on.options).forEach( function(option) {
-        if (option.value == restore["command-on"]) {
+        if (option.value == rule['on'][0]) {
             option.selected = true;
             populate_sub_command_on(target);
         }
     });
 
+    if (!sub_command_select_on.disabled) {
+        // Populate IrBlaster key
+        Array.from(sub_command_select_on.options).forEach( function(option) {
+            if (option.value == rule['on'][2]) {
+                option.selected = true;
+            }
+        });
+
+    } else if (!command_arg_on.disabled) {
+        // Populate command argument field
+        command_arg_on.value = rule['on'][2];
+    };
+
+    if (rule['off'][0] == 'ir_key') {
+        // IR commands have different syntax (no target instance, extra command)
+        // Replace command (ir_key) with instance (ir_blaster) and change order so dropdowns populate correctly
+        rule['off'].shift();
+        rule['off'].splice(1, 0, "ir_blaster");
+    };
+
+    // Populate instance (or IrBlaster)
+    Array.from(instance_select_off.options).forEach( function(option) {
+        if (option.value.split("-")[0] == rule['off'][1]) {
+            option.selected = true;
+            populate_command_off(target);
+        }
+    });
+
+    // Populate command (or IrBlaster virtual remote)
     Array.from(command_select_off.options).forEach( function(option) {
-        if (option.value == restore["command-off"]) {
+        if (option.value == rule['off'][0]) {
             option.selected = true;
             populate_sub_command_off(target);
         }
     });
 
-    Array.from(sub_command_select_on.options).forEach( function(option) {
-        if (option.value == restore["sub-command-on"]) {
-            option.selected = true;
-        }
-    });
+    if (!sub_command_select_off.disabled) {
+        // Populate IrBlaster key
+        Array.from(sub_command_select_off.options).forEach( function(option) {
+            if (option.value == rule['off'][2]) {
+                option.selected = true;
+            }
+        });
 
-    Array.from(sub_command_select_off.options).forEach( function(option) {
-        if (option.value == restore["sub-command-off"]) {
-            option.selected = true;
-        }
-    });
-
-    if (restore["command-arg-on"]) {
-        command_arg_on.value = restore["command-arg-on"];
-    };
-
-    if (restore["command-arg-off"]) {
-        command_arg_off.value = restore["command-arg-off"];
+    } else if (!command_arg_off.disabled) {
+        // Populate command argument field
+        command_arg_off.value = rule['off'][2];
     };
 };
 
@@ -328,12 +334,7 @@ async function change_api_target_rule(el) {
 
     const target = el.id.split("-")[0];
 
-    var rule = JSON.parse(document.getElementById(`${target}-rule`).value);
-    console.log("Old:");
-    console.log(rule);
-    rule = convert_api_target_rule(rule);
-    console.log("New:");
-    console.log(rule);
+    var rule = document.getElementById(`${target}-current_rule`).value;
 
     var result = await send_command({'command': 'set_rule', 'instance': target, 'rule': rule});
     result = await result.json();
@@ -341,25 +342,4 @@ async function change_api_target_rule(el) {
     if (JSON.stringify(result).startsWith('{"ERROR')) {
         console.log(JSON.stringify(result));
     };
-};
-
-function add_rule_api(el) {
-    // Get target device/sensor
-    const target = el.id.split("-")[0];
-
-    // Get rule number
-    const num = el.id.split("add")[1];
-
-    // Get old rule
-    var old_rule = JSON.parse(document.getElementById(`schedule-${target}-rule${num}-value`).value);
-
-    // Convert, overwrite field value
-    var new_rule = convert_api_target_rule(old_rule);
-    document.getElementById(`schedule-${target}-rule${num}-value`).value = new_rule;
-
-    // Call function to add rule (reads from field set above)
-    add_rule(el);
-
-    // Revert field value (used to re-populate dropdowns if user edits rule again)
-    document.getElementById(`schedule-${target}-rule${num}-value`).value = old_rule;
 };

frontend/node_configuration/templates/node_configuration/edit-config.html
@@ -407,14 +407,14 @@
                                                     <label for="device{{ forloop.counter }}-ip"><b>Target:</b></label>
                                                     <select id="device{{ forloop.counter }}-ip" class="form-select mb-3" onchange="api_target_selected(this)">
                                                         <option value="" selected="selected"></option>
-                                                        {% for node in context.api_target_options %}
-                                                            <option value="{{node}}" {% if params.ip in node %}selected{% endif %}>{{node}}</option>
+                                                        {% for node, ip in context.api_target_options.addresses.items %}
+                                                            <option value="{{ip}}" {% if params.ip == ip %}selected{% endif %}>{{node}}</option>
                                                         {% endfor %}
                                                     </select>
                                                 </div>
 
                                                 <div class="mb-2 text-center">
-                                                    <button id="device{{ forloop.counter }}-set-rule" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                                                    <button id="device{{ forloop.counter }}-default_rule-button" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" type="button">Set rule</button>
                                                 </div>
 
                                                 <div class="mb-2 text-center">
@@ -553,14 +553,14 @@
                                     {% if params.schedule.items|length > 0 %}
                                         {% for time, rule in params.schedule.items %}
                                             <tr id="{{instance}}-row-{{forloop.counter}}">
-                                                <td><input type="text" class="form-control timestamp {{instance}}" id="schedule-{{instance}}-rule{{forloop.counter}}-time" placeholder="HH:MM" value="{{ time }}"></td>
+                                                <td><input type="text" class="form-control timestamp {{instance}}" id="{{instance}}-rule{{forloop.counter}}-time" placeholder="HH:MM" value="{{time}}"></td>
 
                                                 {% if not params.type == "api-target" %}
-                                                    <td><input type="text" class="form-control rule {{instance}}" id="schedule-{{instance}}-rule{{forloop.counter}}-value" placeholder="" value="{{ rule }}"></td>
+                                                    <td><input type="text" class="form-control rule {{instance}}" id="{{instance}}-rule{{forloop.counter}}" placeholder="" value="{{rule}}"></td>
                                                 {% else %}
                                                     <td style="width: 50%">
-                                                        <button id="schedule-{{instance}}-rule{{forloop.counter}}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
-                                                        <input type="text" class="form-control rule {{instance}}" id="schedule-{{instance}}-rule{{forloop.counter}}-value" value="{{ rule }}" style="display:none;">
+                                                        <button id="{{instance}}-rule{{forloop.counter}}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                                                        <input type="text" class="form-control rule {{instance}}" id="{{instance}}-rule{{forloop.counter}}" value="{{rule}}" style="display:none;">
                                                     </td>
                                                 {% endif %}
                                                 <td class="min"><button type="button" class="remove btn btn-danger" id="{{instance}}-remove{{forloop.counter}}"  onclick="remove(this)">X</button></td>
@@ -568,15 +568,15 @@
                                         {% endfor %}
                                     {% else %}
                                         <tr id="{{instance}}-row-1">
-                                            <td><input type="text" class="form-control timestamp {{instance}}" id="schedule-{{instance}}-rule1-time" placeholder="HH:MM"></td>
+                                            <td><input type="text" class="form-control timestamp {{instance}}" id="{{instance}}-rule1-time" placeholder="HH:MM"></td>
 
                                             {% if not params.type == "api-target" %}
-                                                <td><input type="text" class="form-control rule {{instance}}" id="schedule-{{instance}}-rule1-value" placeholder="HH:MM"></td>
+                                                <td><input type="text" class="form-control rule {{instance}}" id="{{instance}}-rule1" placeholder="HH:MM"></td>
                                             {% else %}
                                                 <td style="width: 50%">
                                                     {# Show button that opens api rule modal containing dropdowns used to set rule. Selection from modal added to hidden text box, read by form #}
-                                                    <button id="schedule-{{instance}}-rule1-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
-                                                    <input type="text" class="form-control rule {{instance}}" id="schedule-{{instance}}-rule1-value" style="display:none;">
+                                                    <button id="{{instance}}-rule1-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                                                    <input type="text" class="form-control rule {{instance}}" id="{{instance}}-rule1" style="display:none;">
                                                 </td>
                                             {% endif %}
 

frontend/api/static/api/schedule_rules.js
@@ -41,8 +41,8 @@ const save_rules_toast = new bootstrap.Toast(document.getElementById("save_rules
 function schedule_rule_field_handler(e) {
     const id = e.target.id.split("-")[1];
     const row = e.target.id.split("-")[2].replace("rule", "");
-    const time_field = document.getElementById(`schedule-${id}-rule${row}-time`);
-    const rule_field = document.getElementById(`schedule-${id}-rule${row}-value`);
+    const time_field = document.getElementById(`${id}-rule${row}-time`);
+    const rule_field = document.getElementById(`${id}-rule${row}`);
 
     // Function runs on each keystroke, only need to change button once on first change
     // Subsequent changes will throw error when getting button element (ID changed first time)
@@ -88,15 +88,15 @@ async function add_rule_row(el) {
     // Add row number + target id to empty row template (different template if api-target)
     if (target.startsWith("device") && target_node_status['devices'][target]['type'] == 'api-target') {
         var template = `<tr id="${target}-row-${row}">
-                            <td><input type="time" class="form-control" id="schedule-${target}-rule${row}-time" placeholder="HH:MM" name="schedule-${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
-                            <td><button id="schedule-${target}-rule${row}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
-                            <input type="text" class="form-control rule ${target}" id="schedule-${target}-rule${row}-value" value="" style="display:none;"></td>
-                            <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule_api(this)"><i class="bi-plus-lg"></i></button></td>
+                            <td><input type="time" class="form-control" id="${target}-rule${row}-time" placeholder="HH:MM" name="${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                            <td><button id="${target}-rule${row}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                            <input type="text" class="form-control rule ${target}" id="${target}-rule${row}" value="" style="display:none;"></td>
+                            <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                         </tr>`
     } else {
         var template = `<tr id="${target}-row-${row}">
-                            <td><input type="time" class="form-control" id="schedule-${target}-rule${row}-time" placeholder="HH:MM" name="schedule-${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
-                            <td><input type="text" class="form-control" id="schedule-${target}-rule${row}-value" placeholder="" name="schedule-${target}-rule${row}-value" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
+                            <td><input type="time" class="form-control" id="${target}-rule${row}-time" placeholder="HH:MM" name="${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                            <td><input type="text" class="form-control" id="${target}-rule${row}" placeholder="" name="${target}-rule${row}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
                             <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                         </tr>`
     };
@@ -105,17 +105,17 @@ async function add_rule_row(el) {
     document.getElementById(target + "-rules").insertAdjacentHTML('beforeend', template);
 
     // Add tooltips (used for help messages when invalid rule entered)
-    schedule_rule_tooltips[`schedule-${target}-rule${row}-time`] = new bootstrap.Tooltip(document.getElementById(`schedule-${target}-rule${row}-time`));
-    schedule_rule_tooltips[`schedule-${target}-rule${row}-value`] = new bootstrap.Tooltip(document.getElementById(`schedule-${target}-rule${row}-value`));
+    schedule_rule_tooltips[`${target}-rule${row}-time`] = new bootstrap.Tooltip(document.getElementById(`${target}-rule${row}-time`));
+    schedule_rule_tooltips[`${target}-rule${row}`] = new bootstrap.Tooltip(document.getElementById(`${target}-rule${row}`));
 
     // Add listener to dismiss tooltips on hover/select
-    document.getElementById(`schedule-${target}-rule${row}-time`).addEventListener("mouseover", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-time`).addEventListener("focus", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-value`).addEventListener("mouseover", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-value`).addEventListener("focus", hide_tooltip);
+    document.getElementById(`${target}-rule${row}-time`).addEventListener("mouseover", hide_tooltip);
+    document.getElementById(`${target}-rule${row}-time`).addEventListener("focus", hide_tooltip);
+    document.getElementById(`${target}-rule${row}`).addEventListener("mouseover", hide_tooltip);
+    document.getElementById(`${target}-rule${row}`).addEventListener("focus", hide_tooltip);
 
     // Focus time field
-    document.getElementById(`schedule-${target}-rule${row}-time`).focus();
+    document.getElementById(`${target}-rule${row}-time`).focus();
 
     // Hide add rule button (will be un-hidden when user finishes adding this rule)
     document.getElementById(target + "-add-rule").style.display = "none"
@@ -150,10 +150,10 @@ async function add_rule(el) {
     disable_row(row, true);
 
     // Get timestamp input
-    const timestamp = document.getElementById(`schedule-${target}-rule${num}-time`);
+    const timestamp = document.getElementById(`${target}-rule${num}-time`);
 
     // Get rule input
-    const rule = document.getElementById(`schedule-${target}-rule${num}-value`);
+    const rule = document.getElementById(`${target}-rule${num}`);
 
     // Do not add incomplete rule
     if (timestamp.value.length == 0 || rule.value.length == 0) {
@@ -256,7 +256,7 @@ async function delete_rule(selected, remove=true) {
     // Get timestamp
     // Uses dataset.original instead of .value to allow editing existing rules (old needs to be deleted before adding new, see add_rule() below)
     // While delete button is visible to user, dataset.original and .value are always identical (changes to add button when different)
-    const timestamp = document.getElementById(`schedule-${target}-rule${rule}-time`).dataset.original;
+    const timestamp = document.getElementById(`${target}-rule${rule}-time`).dataset.original;
 
     // Send command
     var result = await send_command({'command': 'remove_rule', 'instance': target, 'rule': timestamp});
@@ -274,8 +274,8 @@ async function delete_rule(selected, remove=true) {
         if (document.getElementById(target + "-rules").rows.length == 1) {
             // If no rules remain, add blank row
             var template = `<tr id="${target}-row-1">
-                                <td><input type="time" class="form-control" id="schedule-${target}-rule1-time" placeholder="HH:MM" name="schedule-${target}-rule1-time"></td>
-                                <td><input type="text" class="form-control" id="schedule-${target}-rule1-value" placeholder="" name="schedule-${target}-rule1-value"></td>
+                                <td><input type="time" class="form-control" id="${target}-rule1-time" placeholder="HH:MM" name="${target}-rule1-time"></td>
+                                <td><input type="text" class="form-control" id="${target}-rule1" placeholder="" name="${target}-rule1"></td>
                                 <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add1" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                             </tr>`
 

commit b8dd8038f818914b4682cd3524338b3440ea8da3

Bug fixes: ApiTarget target node dropdown handler now works correctly again, target node options now display correctly (both broken in 98a85be4). Config generator no longer ignores ApiTarget default_rule param (broken in a61cf113).

frontend/node_configuration/static/node_configuration/add-instance.js
@@ -161,7 +161,8 @@ async function load_device_section(select) {
                             <option value="" selected="selected" selected></option>`
 
         for (var x in ApiTargetOptions) {
-            template +=    `<option value="${x}">${x.split("-")[1]}</option>`
+            if (x == "addresses") { continue };
+            template +=    `<option value="${ApiTargetOptions["addresses"][x]}">${x}</option>`
         };
 
         template +=     `</select>
@@ -172,6 +173,7 @@ async function load_device_section(select) {
                     </div>
 
                     <div class="mb-2 text-center">
+                        <label for="device${index}-default_rule" style="display:none;"><b>Default Rule:</b></label>
                         <input type="default_rule" class="form-control" id="device${index}-default_rule" placeholder="" style="display:none;" required>
                     </div>`
 

frontend/node_configuration/static/node_configuration/apiTargetOptions.js
@@ -23,11 +23,11 @@ function submit_api_rule(el) {
 // Called when user changes target node in dropdown
 function api_target_selected(el) {
     if (el.value) {
-        document.getElementById(el.id.split("-")[0] + "-set-rule").disabled = false;
+        document.getElementById(el.id.split("-")[0] + "-default_rule-button").disabled = false;
         // Clear old rule so menu doesn't populate when opened
         document.getElementById(el.id.split("-")[0] + "-default_rule").value = "";
     } else {
-        document.getElementById(el.id.split("-")[0] + "-set-rule").disabled = true;
+        document.getElementById(el.id.split("-")[0] + "-default_rule-button").disabled = true;
     };
 }
 

frontend/node_configuration/templates/node_configuration/edit-config.html
@@ -418,6 +418,7 @@
                                                 </div>
 
                                                 <div class="mb-2 text-center">
+                                                    <label for="device{{ forloop.counter }}-default_rule" style="display:none;"><b>Default Rule:</b></label>
                                                     <input type="default_rule" class="form-control" id="device{{ forloop.counter }}-default_rule" placeholder="" style="display:none;" value="{{params.default_rule}}" required>
                                                 </div>
 