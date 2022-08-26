jamedeus/micropython-smarthome

commit 98a85be487a0e3a881ee125ce6bf518baab1e0b5

ApiTarget Modal refactor
Consistent ID syntax for buttons and hidden fields, no longer need conditional to set target.

New reload_rule function populates dropdowns to match existing rule from config format. FormData format no longer necessary, same syntax used everywhere.

Views now generate context with literal ApiTarget rules (json-encoded), edit_config returns ApiTarget rules in final config format. No longer needs conversion on backend, removed convert and reverse-convert functions.

Frontend schedule rules no longer need seperate function to add ApiTarget rule (value in field no longer needs to be converted).

frontend/api/static/api/schedule_rules.js
@@ -41,8 +41,8 @@ const save_rules_toast = new bootstrap.Toast(document.getElementById("save_rules
 function schedule_rule_field_handler(e) {
     const id = e.target.id.split("-")[1];
     const row = e.target.id.split("-")[2].replace("rule", "");
-    const time_field = document.getElementById(`schedule-${id}-rule${row}-time`);
-    const rule_field = document.getElementById(`schedule-${id}-rule${row}-value`);
+    const time_field = document.getElementById(`${id}-rule${row}-time`);
+    const rule_field = document.getElementById(`${id}-rule${row}`);
 
     // Function runs on each keystroke, only need to change button once on first change
     // Subsequent changes will throw error when getting button element (ID changed first time)
@@ -88,15 +88,15 @@ async function add_rule_row(el) {
     // Add row number + target id to empty row template (different template if api-target)
     if (target.startsWith("device") && target_node_status['devices'][target]['type'] == 'api-target') {
         var template = `<tr id="${target}-row-${row}">
-                            <td><input type="time" class="form-control" id="schedule-${target}-rule${row}-time" placeholder="HH:MM" name="schedule-${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
-                            <td><button id="schedule-${target}-rule${row}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
-                            <input type="text" class="form-control rule ${target}" id="schedule-${target}-rule${row}-value" value="" style="display:none;"></td>
-                            <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule_api(this)"><i class="bi-plus-lg"></i></button></td>
+                            <td><input type="time" class="form-control" id="${target}-rule${row}-time" placeholder="HH:MM" name="${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                            <td><button id="${target}-rule${row}-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                            <input type="text" class="form-control rule ${target}" id="${target}-rule${row}" value="" style="display:none;"></td>
+                            <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                         </tr>`
     } else {
         var template = `<tr id="${target}-row-${row}">
-                            <td><input type="time" class="form-control" id="schedule-${target}-rule${row}-time" placeholder="HH:MM" name="schedule-${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
-                            <td><input type="text" class="form-control" id="schedule-${target}-rule${row}-value" placeholder="" name="schedule-${target}-rule${row}-value" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
+                            <td><input type="time" class="form-control" id="${target}-rule${row}-time" placeholder="HH:MM" name="${target}-rule${row}-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                            <td><input type="text" class="form-control" id="${target}-rule${row}" placeholder="" name="${target}-rule${row}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
                             <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add${row}" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                         </tr>`
     };
@@ -105,17 +105,17 @@ async function add_rule_row(el) {
     document.getElementById(target + "-rules").insertAdjacentHTML('beforeend', template);
 
     // Add tooltips (used for help messages when invalid rule entered)
-    schedule_rule_tooltips[`schedule-${target}-rule${row}-time`] = new bootstrap.Tooltip(document.getElementById(`schedule-${target}-rule${row}-time`));
-    schedule_rule_tooltips[`schedule-${target}-rule${row}-value`] = new bootstrap.Tooltip(document.getElementById(`schedule-${target}-rule${row}-value`));
+    schedule_rule_tooltips[`${target}-rule${row}-time`] = new bootstrap.Tooltip(document.getElementById(`${target}-rule${row}-time`));
+    schedule_rule_tooltips[`${target}-rule${row}`] = new bootstrap.Tooltip(document.getElementById(`${target}-rule${row}`));
 
     // Add listener to dismiss tooltips on hover/select
-    document.getElementById(`schedule-${target}-rule${row}-time`).addEventListener("mouseover", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-time`).addEventListener("focus", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-value`).addEventListener("mouseover", hide_tooltip);
-    document.getElementById(`schedule-${target}-rule${row}-value`).addEventListener("focus", hide_tooltip);
+    document.getElementById(`${target}-rule${row}-time`).addEventListener("mouseover", hide_tooltip);
+    document.getElementById(`${target}-rule${row}-time`).addEventListener("focus", hide_tooltip);
+    document.getElementById(`${target}-rule${row}`).addEventListener("mouseover", hide_tooltip);
+    document.getElementById(`${target}-rule${row}`).addEventListener("focus", hide_tooltip);
 
     // Focus time field
-    document.getElementById(`schedule-${target}-rule${row}-time`).focus();
+    document.getElementById(`${target}-rule${row}-time`).focus();
 
     // Hide add rule button (will be un-hidden when user finishes adding this rule)
     document.getElementById(target + "-add-rule").style.display = "none"
@@ -150,10 +150,10 @@ async function add_rule(el) {
     disable_row(row, true);
 
     // Get timestamp input
-    const timestamp = document.getElementById(`schedule-${target}-rule${num}-time`);
+    const timestamp = document.getElementById(`${target}-rule${num}-time`);
 
     // Get rule input
-    const rule = document.getElementById(`schedule-${target}-rule${num}-value`);
+    const rule = document.getElementById(`${target}-rule${num}`);
 
     // Do not add incomplete rule
     if (timestamp.value.length == 0 || rule.value.length == 0) {
@@ -256,7 +256,7 @@ async function delete_rule(selected, remove=true) {
     // Get timestamp
     // Uses dataset.original instead of .value to allow editing existing rules (old needs to be deleted before adding new, see add_rule() below)
     // While delete button is visible to user, dataset.original and .value are always identical (changes to add button when different)
-    const timestamp = document.getElementById(`schedule-${target}-rule${rule}-time`).dataset.original;
+    const timestamp = document.getElementById(`${target}-rule${rule}-time`).dataset.original;
 
     // Send command
     var result = await send_command({'command': 'remove_rule', 'instance': target, 'rule': timestamp});
@@ -274,8 +274,8 @@ async function delete_rule(selected, remove=true) {
         if (document.getElementById(target + "-rules").rows.length == 1) {
             // If no rules remain, add blank row
             var template = `<tr id="${target}-row-1">
-                                <td><input type="time" class="form-control" id="schedule-${target}-rule1-time" placeholder="HH:MM" name="schedule-${target}-rule1-time"></td>
-                                <td><input type="text" class="form-control" id="schedule-${target}-rule1-value" placeholder="" name="schedule-${target}-rule1-value"></td>
+                                <td><input type="time" class="form-control" id="${target}-rule1-time" placeholder="HH:MM" name="${target}-rule1-time"></td>
+                                <td><input type="text" class="form-control" id="${target}-rule1" placeholder="" name="${target}-rule1"></td>
                                 <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="${target}-add1" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                             </tr>`
 

frontend/api/templates/api/api_card.html
@@ -219,15 +219,15 @@
                                         {% if params.schedule.items|length > 0 %}
                                             {% for time, rule in params.schedule.items %}
                                                 <tr id="{{sensor}}-row-{{forloop.counter}}">
-                                                    <td><input type="time" class="form-control schedule-rule" id="schedule-{{sensor}}-rule{{forloop.counter}}-time" placeholder="HH:MM" name="schedule-{{sensor}}-rule{{forloop.counter}}-time" value="{{ time }}" data-original="{{ time }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists" autocomplete="off"></td>
-                                                    <td><input type="text" class="form-control schedule-rule" id="schedule-{{sensor}}-rule{{forloop.counter}}-value" placeholder="" name="schedule-{{sensor}}-rule{{forloop.counter}}-value" value="{{ rule }}" data-original="{{ rule }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule" autocomplete="off"></td>
+                                                    <td><input type="time" class="form-control schedule-rule" id="{{sensor}}-rule{{forloop.counter}}-time" placeholder="HH:MM" name="{{sensor}}-rule{{forloop.counter}}-time" value="{{ time }}" data-original="{{ time }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists" autocomplete="off"></td>
+                                                    <td><input type="text" class="form-control schedule-rule" id="{{sensor}}-rule{{forloop.counter}}" placeholder="" name="{{sensor}}-rule{{forloop.counter}}" value="{{ rule }}" data-original="{{ rule }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule" autocomplete="off"></td>
                                                     <td class="min"><button type="button" class="remove btn btn-sm btn-danger mt-1" id="{{sensor}}-remove{{forloop.counter}}"  onclick="delete_rule(this)"><i class="bi-trash"></i></button></td>
                                                 </tr>
                                             {% endfor %}
                                         {% else %}
                                             <tr id="{{sensor}}-row-1">
-                                                <td><input type="time" class="form-control" id="schedule-{{sensor}}-rule1-time" placeholder="HH:MM" name="schedule-{{sensor}}-rule1-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
-                                                <td><input type="text" class="form-control" id="schedule-{{sensor}}-rule1-value" placeholder="" name="schedule-{{sensor}}-rule1-value" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
+                                                <td><input type="time" class="form-control" id="{{sensor}}-rule1-time" placeholder="HH:MM" name="{{sensor}}-rule1-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                                                <td><input type="text" class="form-control" id="{{sensor}}-rule1" placeholder="" name="{{sensor}}-rule1" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
                                                 <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="{{sensor}}-add1" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>
                                             </tr>
                                         {% endif %}
@@ -278,7 +278,7 @@
                                             {% endif %}
                                             <li><a id="{{device}}-reset" class="dropdown-item reset-rule" onclick="reset(this);">Reset rule</a></li>
                                             {% if params.type == "api-target" %}
-                                                <li><a id="{{device}}-set_rule" class="dropdown-item" onclick="open_rule_modal(this);">Change rule</a></li>
+                                                <li><a id="{{device}}-current_rule-button" class="dropdown-item" onclick="open_rule_modal(this);">Change rule</a></li>
                                             {% endif %}
                                         </ul>
                                     </div>
@@ -312,7 +312,7 @@
 
                                 {% if params.type == "api-target" %}
                                     <div style="display:none;">
-                                        <input id="{{device}}-rule" type="text" autocomplete="off" onchange="change_api_target_rule(this);">
+                                        <input id="{{device}}-current_rule" type="text" autocomplete="off" value="{{params.current_rule}}" onchange="change_api_target_rule(this);">
                                     </div>
                                 {% endif %}
 
@@ -333,13 +333,13 @@
                                         {% if params.schedule.items|length > 0 %}
                                             {% for time, rule in params.schedule.items %}
                                                 <tr id="{{device}}-row-{{forloop.counter}}">
-                                                    <td><input type="time" class="form-control schedule-rule" id="schedule-{{device}}-rule{{forloop.counter}}-time" placeholder="HH:MM" name="schedule-{{device}}-rule{{forloop.counter}}-time" value="{{ time }}" data-original="{{ time }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists" autocomplete="off"></td>
+                                                    <td><input type="time" class="form-control schedule-rule" id="{{device}}-rule{{forloop.counter}}-time" placeholder="HH:MM" name="{{device}}-rule{{forloop.counter}}-time" value="{{ time }}" data-original="{{ time }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists" autocomplete="off"></td>
 
                                                     {% if not params.type == "api-target" %}
-                                                        <td><input type="text" class="form-control schedule-rule" id="schedule-{{device}}-rule{{forloop.counter}}-value" placeholder="" name="schedule-{{device}}-rule{{forloop.counter}}-value" value="{{ rule }}" data-original="{{ rule }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule" autocomplete="off"></td>
+                                                        <td><input type="text" class="form-control schedule-rule" id="{{device}}-rule{{forloop.counter}}" placeholder="" name="{{device}}-rule{{forloop.counter}}" value="{{ rule }}" data-original="{{ rule }}" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule" autocomplete="off"></td>
                                                     {% else %}
-                                                        <td><button id="schedule-{{device}}-rule{{forloop.counter}}-button" class="form-control" onclick="open_rule_modal(this);" type="button" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule">Set rule</button>
-                                                        <input type="text" class="form-control rule {{device}}" id="schedule-{{device}}-rule{{forloop.counter}}-value" value="" value="{{ rule }}" data-original="{{ rule }}" style="display:none;"></td>
+                                                        <td><button id="{{device}}-rule{{forloop.counter}}-button" class="form-control" onclick="open_rule_modal(this);" type="button" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule">Set rule</button>
+                                                        <input type="text" class="form-control rule {{device}}" id="{{device}}-rule{{forloop.counter}}" value="{{ rule }}" data-original="{{ rule }}" style="display:none;"></td>
                                                     {% endif %}
 
                                                     <td class="min"><button type="button" class="remove btn btn-sm btn-danger mt-1" id="{{device}}-remove{{forloop.counter}}"  onclick="delete_rule(this)"><i class="bi-trash"></i></button></td>
@@ -347,13 +347,13 @@
                                             {% endfor %}
                                         {% else %}
                                             <tr id="{{device}}-row-1">
-                                                <td><input type="time" class="form-control" id="schedule-{{device}}-rule1-time" placeholder="HH:MM" name="schedule-{{device}}-rule1-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
+                                                <td><input type="time" class="form-control" id="{{device}}-rule1-time" placeholder="HH:MM" name="{{device}}-rule1-time" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Rule at this time already exists"></td>
 
                                                 {% if not params.type == "api-target" %}
-                                                    <td><input type="text" class="form-control" id="schedule-{{device}}-rule1-value" placeholder="" name="schedule-{{device}}-rule1-value" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
+                                                    <td><input type="text" class="form-control" id="{{device}}-rule1" placeholder="" name="{{device}}-rule1" data-bs-toggle="tooltip" data-bs-trigger="manual" title="Invalid rule"></td>
                                                 {% else %}
-                                                    <td><button id="schedule-{{device}}-rule1-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
-                                                    <input type="text" class="form-control rule {{device}}" id="schedule-{{device}}-rule1-value" value="" style="display:none;"></td>
+                                                    <td><button id="{{device}}-rule1-button" class="form-control" onclick="open_rule_modal(this);" type="button">Set rule</button>
+                                                    <input type="text" class="form-control rule {{device}}" id="{{device}}-rule1" value="" style="display:none;"></td>
                                                 {% endif %}
 
                                                 <td class="min"><button type="button" class="remove btn btn-sm btn-success mt-1" id="{{device}}-add1" onclick="add_rule(this)"><i class="bi-plus-lg"></i></button></td>

frontend/api/views.py
@@ -67,11 +67,17 @@ def api(request, node):
 
         for i in config:
             if i.startswith("device") and config[i]['type'] == 'api-target':
-                # ApiTarget found, find section in options object with matching IP
-                for node in options:
-                    if node.split("-")[0] == config[i]['ip']:
+                # ApiTarget found, find section in options object with matching IP, add to context
+                for node in options['addresses']:
+                    if options['addresses'][node] == config[i]['ip']:
                         status["api_target_options"][i] = options[node]
                         break
+
+                # JSON-encode rule dicts
+                status['devices'][i]['current_rule'] = json.dumps(status['devices'][i]['current_rule'])
+
+                for rule in status['devices'][i]['schedule']:
+                    status['devices'][i]['schedule'][rule] = json.dumps(status['devices'][i]['schedule'][rule])
 
     template = loader.get_template('api/api_card.html')
 

frontend/node_configuration/static/node_configuration/add-instance.js
@@ -168,7 +168,7 @@ async function load_device_section(select) {
                     </div>
 
                     <div class="mb-2 text-center">
-                        <button id="device${index}-set-rule" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" disabled>Set rule</button>
+                        <button id="device${index}-default_rule-button" class="btn btn-secondary mt-3" onclick="open_rule_modal(this);" disabled>Set rule</button>
                     </div>
 
                     <div class="mb-2 text-center">

commit a216c0297234fa100eceae8ad4e914635a4c63b4

Bug fix: Now possible to edit rules again (broken in 98a85be4).

frontend/api/static/api/schedule_rules.js
@@ -39,8 +39,8 @@ const save_rules_toast = new bootstrap.Toast(document.getElementById("save_rules
 // Existing rules: Delete button changes to add button
 // Modified rules: If changed back to original value, add button reverts back to delete
 function schedule_rule_field_handler(e) {
-    const id = e.target.id.split("-")[1];
-    const row = e.target.id.split("-")[2].replace("rule", "");
+    const id = e.target.id.split("-")[0];
+    const row = e.target.id.split("-")[1].replace("rule", "");
     const time_field = document.getElementById(`${id}-rule${row}-time`);
     const rule_field = document.getElementById(`${id}-rule${row}`);
 