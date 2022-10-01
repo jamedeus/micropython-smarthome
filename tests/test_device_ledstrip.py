3bc69f990a92afac10a8e3a304835db5d4dac0c7
Josh Medeiros <josh@jamedeus.com>
2022-10-01 14:53:52 -0700

    Regression test for bug fixed in efd79c6f.

--- tests/test_device_ledstrip.py
+++ tests/test_device_ledstrip.py
@@ -6,10 +6,10 @@ from LedStrip import LedStrip
 class TestLedStrip(unittest.TestCase):
 
     def __dir__(self):
-        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_on", "test_turn_off"]
+        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_on", "test_turn_off", "test_enable_regression_test"]
 
     def test_instantiation(self):
-        self.instance = LedStrip("device1", "device1", "pwm", True, None, "off", 4, 0, 1023)
+        self.instance = LedStrip("device1", "device1", "pwm", True, None, 512, 4, 0, 1023)
         self.assertIsInstance(self.instance, LedStrip)
         self.assertFalse(self.instance.pwm.duty())
         self.assertTrue(self.instance.enabled)
@@ -67,3 +67,18 @@ class TestLedStrip(unittest.TestCase):
         self.instance.enable()
         self.assertTrue(self.instance.send(0))
         self.assertEqual(self.instance.pwm.duty(), 0)
+
+    # Original bug: Enabling and turning on when both current and scheduled rules == "disabled"
+    # resulted in comparison operator between int and string, causing crash.
+    # After fix (see efd79c6f) this is handled by overwriting current_rule with default_rule.
+    def test_enable_regression_test(self):
+        # Simulate disabling by scheduled rule change
+        self.instance.scheduled_rule = "disabled"
+        self.instance.set_rule("disabled")
+        # Simulate user enabling and turning on from frontend
+        self.instance.enable()
+        self.instance.send(1)
+        # Should not crash, should replace unusable rule with default_rule (512) and fade on
+        self.assertNotEqual(self.instance.current_rule, "disabled")
+        self.assertEqual(self.instance.current_rule, 512)
+        self.assertEqual(self.instance.pwm.duty(), 512)
