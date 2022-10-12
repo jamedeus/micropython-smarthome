b07c5ab42e278e5c89140fdc4159fa6d3b09b4ec
Josh Medeiros <josh@jamedeus.com>
2022-10-12 16:37:43 -0700

    Bug fix: provision.py --test now uploads missing Group dependency.

--- provision.py
+++ provision.py
@@ -224,6 +224,9 @@ class Provisioner():
             # Upload Config module
             self.upload("Config.py", "Config.py")
 
+            # Upload Group module
+            self.upload("Group.py", "Group.py")
+
             # Upload SoftwareTimer module
             self.upload("SoftwareTimer.py", "SoftwareTimer.py")
 
