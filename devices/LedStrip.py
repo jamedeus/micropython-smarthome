jamedeus/micropython-smarthome

commit 1945df43b52888c38f9bd806ac38590911c1436e

Bug fix: LedStrip now correctly aborts fade when rule changed while in-progress. Added regression tests.

devices/LedStrip.py
@@ -93,6 +93,10 @@ class LedStrip(Device):
             print(f"{self.name}: Rule changed to {self.current_rule}")
             log.info(f"{self.name}: Rule changed to {self.current_rule}")
 
+            # If fade in progress when rule changed, abort
+            if self.fading:
+                self.fading = False
+
             # Rule just changed to disabled
             if self.current_rule == "disabled":
                 self.send(0)

tests/test_device_ledstrip.py
@@ -6,7 +6,7 @@ from LedStrip import LedStrip
 class TestLedStrip(unittest.TestCase):
 
     def __dir__(self):
-        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_on", "test_turn_off", "test_turn_off_when_disabled", "test_enable_regression_test", "test_rule_change_to_enabled_regression", "test_regression_invalid_default_rule", "test_regression_turn_off_while_disabled"]
+        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_on", "test_turn_off", "test_turn_off_when_disabled", "test_enable_regression_test", "test_regression_rule_change_to_enabled", "test_regression_invalid_default_rule", "test_regression_turn_off_while_disabled", "test_regression_rule_change_while_fading"]
 
     def test_instantiation(self):
         self.instance = LedStrip("device1", "device1", "pwm", True, None, 512, 4, 0, 1023)
@@ -100,7 +100,7 @@ class TestLedStrip(unittest.TestCase):
     # Original bug: LedStrip class overwrites parent set_rule method and did not include conditional
     # that overwrites "enabled" with default_rule. This resulted in an unusable rule which caused
     # crash next time send method was called.
-    def test_rule_change_to_enabled_regression(self):
+    def test_regression_rule_change_to_enabled(self):
         self.instance.disable()
         self.assertFalse(self.instance.enabled)
         self.instance.set_rule('enabled')
@@ -155,3 +155,12 @@ class TestLedStrip(unittest.TestCase):
         # On command should also return True, but shouldn't cause any action
         self.assertTrue(self.instance.send(1))
         self.assertEqual(self.instance.pwm.duty(), 0)
+
+    def test_regression_rule_change_while_fading(self):
+        # Start fade, confirm that fade started
+        self.assertTrue(self.instance.set_rule('fade/50/1800'))
+        self.assertTrue(self.instance.fading)
+
+        # Change rule, fade should stop
+        self.instance.set_rule(98)
+        self.assertFalse(self.instance.fading)

tests/test_device_tplink.py
@@ -6,7 +6,7 @@ from Tplink import Tplink
 class TestTplink(unittest.TestCase):
 
     def __dir__(self):
-        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_off", "test_turn_on", "test_rule_change_to_enabled_regression", "test_regression_invalid_default_rule"]
+        return ["test_instantiation", "test_rule_validation_valid", "test_rule_validation_invalid", "test_rule_change", "test_enable_disable", "test_disable_by_rule_change", "test_enable_by_rule_change", "test_turn_off", "test_turn_on", "test_regression_rule_change_to_enabled", "test_regression_invalid_default_rule", "test_regression_rule_change_while_fading"]
 
     def test_instantiation(self):
         self.instance = Tplink("device1", "device1", "dimmer", True, None, 42, "192.168.1.233")
@@ -67,7 +67,7 @@ class TestTplink(unittest.TestCase):
     # Original bug: Tplink class overwrites parent set_rule method and did not include conditional
     # that overwrites "enabled" with default_rule. This resulted in an unusable rule which caused
     # crash next time send method was called.
-    def test_rule_change_to_enabled_regression(self):
+    def test_regression_rule_change_to_enabled(self):
         self.instance.disable()
         self.assertFalse(self.instance.enabled)
         self.instance.set_rule('enabled')
@@ -97,3 +97,12 @@ class TestTplink(unittest.TestCase):
         except AttributeError:
             # Should raise exception, test passed
             self.assertTrue(True)
+
+    def test_regression_rule_change_while_fading(self):
+        # Start fade, confirm that fade started
+        self.assertTrue(self.instance.set_rule('fade/50/1800'))
+        self.assertTrue(self.instance.fading)
+
+        # Change rule, fade should stop
+        self.instance.set_rule(98)
+        self.assertFalse(self.instance.fading)

commit a29f5383644dc3e1d575e2d7b0b89f36a3ed0c94

Bug fix: Fixed regression from 1945df43 breaking LedStrip.fade method.
Before 1945df43, LedStrip fade method called set_rule method for each step, and set_rule did not abort an ongoing fade. After 1945df43 set_rule aborts in-progress fade before setting new rule. Since fade still called set_rule, this resulted in the fade being aborted after the first step. Fixed by switching to approach used in TpLink fade method.

devices/LedStrip.py
@@ -215,7 +215,11 @@ class LedStrip(Device):
                 if new_rule < self.fading["target"]:
                     new_rule = self.fading["target"]
 
-            self.set_rule(new_rule)
+            # Set new rule without calling set_rule method (would abort fade)
+            self.current_rule = int(new_rule)
+            self.scheduled_rule = int(new_rule)
+            if self.state == True:
+                self.send(1)
 
         # Check if fade complete after step
         if self.fading["target"] == int(self.current_rule):

devices/Tplink.py
@@ -230,6 +230,7 @@ class Tplink(Device):
                 if new_rule < self.fading["target"]:
                     new_rule = self.fading["target"]
 
+            # Set new rule without calling set_rule method (would abort fade)
             self.current_rule = int(new_rule)
             self.scheduled_rule = int(new_rule)
             if self.state == True: