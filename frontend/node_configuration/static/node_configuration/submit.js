jamedeus/micropython-smarthome

commit 00ca9bcdef08618945c568fe62bc1338d132d774

Model improvements: Added OneToOne relation between Config and Node models. Full config object now stored in JSONField on Config, can be accessed from Node through reverse relation. Removed config_file field (absolute path only, limits portability), now uses CONFIG_DIR + filename to open file. Updated views for compatibility, all tested.

frontend/node_configuration/views.py
@@ -91,13 +91,12 @@ def upload(request, reupload=False):
         raise Http404("ERROR: Must post data")
 
     try:
-        with open(CONFIG_DIR + data["config"], 'r') as file:
-            config = json.load(file)
-    except FileNotFoundError:
+        config = Config.objects.get(filename = data["config"])
+    except Config.DoesNotExist:
         return JsonResponse("ERROR: Config file doesn't exist - did you delete it manually?", safe=False, status=200)
 
     if not data["config"] == "setup.json":
-        modules, libs = get_modules(config)
+        modules, libs = get_modules(config.config)
     else:
         modules = []
         libs = []
@@ -107,12 +106,11 @@ def upload(request, reupload=False):
 
     # If uploaded for the first time, update models
     if response.status_code == 200 and not reupload:
-        target = Config.objects.get(config_file = CONFIG_DIR + data["config"])
-        target.uploaded = True
-        target.save()
-
-        new = Node(friendly_name = config["metadata"]["id"], ip = data["ip"], config_file = CONFIG_DIR + data["config"], floor = config["metadata"]["floor"])
+        new = Node(friendly_name = config.config["metadata"]["id"], ip = data["ip"], floor = config.config["metadata"]["floor"])
         new.save()
+
+        config.node = new
+        config.save()
 
     return response
 
@@ -202,13 +200,10 @@ def reupload_all(request):
     nodes = Node.objects.all()
 
     for node in nodes:
-        with open(node.config_file, 'r') as file:
-            config = json.load(file)
-
-        modules, libs = get_modules(config)
+        modules, libs = get_modules(node.config.config)
 
         print(f"\nReuploading {node.friendly_name}...")
-        provision(node.config_file.split("/")[-1], node.ip, modules, libs)
+        provision(node.config.filename, node.ip, modules, libs)
 
     return JsonResponse("Finished reuploading", safe=False, status=200)
 
@@ -220,9 +215,9 @@ def delete_config(request):
     else:
         raise Http404("ERROR: Must post data")
 
-    target = Config.objects.get(config_file = CONFIG_DIR + data)
+    target = Config.objects.get(filename = data)
     target.delete()
-    os.system(f'rm {target.config_file}')
+    os.system(f'rm {CONFIG_DIR}/{target.filename}')
 
     return JsonResponse("Deleted {}".format(data), safe=False, status=200)
 
@@ -234,13 +229,11 @@ def delete_node(request):
     else:
         raise Http404("ERROR: Must post data")
 
-    # Get both model entries
+    # Get model entry
     node = Node.objects.get(friendly_name = data)
-    config = Config.objects.get(config_file = node.config_file)
 
     # Delete from disk, delete from models
-    os.system(f'rm {node.config_file}')
-    config.delete()
+    os.system(f'rm {CONFIG_DIR}/{node.config.filename}')
     node.delete()
 
     return JsonResponse("Deleted {}".format(data), safe=False, status=200)
@@ -253,10 +246,10 @@ def node_configuration(request):
         "uploaded" : []
     }
 
-    not_uploaded = Config.objects.filter(uploaded = False)
+    not_uploaded = Config.objects.filter(node = None)
 
     for i in not_uploaded:
-        context["not_uploaded"].append(str(i).split("/")[-1])
+        context["not_uploaded"].append(str(i))
 
     uploaded = Node.objects.all()
     for i in uploaded:
@@ -287,13 +280,12 @@ def configure(request):
 def edit_config(request, name):
     target = Node.objects.get(friendly_name = name)
 
-    with open(target.config_file, 'r') as file:
-        config = json.load(file)
+    config = target.config.config
 
     config["NAME"] = target.friendly_name
     config["TITLE"] = f"Editing {target.friendly_name}"
     config["IP"] = target.ip
-    config["FILENAME"] = target.config_file.split("/")[-1]
+    config["FILENAME"] = target.config.filename
 
     sensors = {}
     devices = {}
@@ -354,7 +346,7 @@ def generateConfigFile(request, edit_existing=False):
 
     try:
         # Check if file with identical parameters exists in database
-        duplicate = Config.objects.get(config_file = CONFIG_DIR + data["friendlyName"] + ".json")
+        duplicate = Config.objects.get(filename = data["friendlyName"])
 
         # Ignore duplicate error if editing an existing config
         if not edit_existing:
@@ -414,15 +406,22 @@ def generateConfigFile(request, edit_existing=False):
     print(json.dumps(config, indent=4))
 
     # Get filename (all lowercase, replace spaces with hyphens)
-    filename = config["metadata"]["id"].lower().replace(" ", "-")
+    filename = config["metadata"]["id"].lower().replace(" ", "-") + ".json"
 
-    with open(CONFIG_DIR + filename + ".json", 'w') as file:
-        json.dump(config, file)
+    print(f"\n\n{filename}\n\n")
 
-    # If creating a new config, add to models
+    # If creating new config, add to models + write to disk
     if not edit_existing:
-        new = Config(config_file = CONFIG_DIR + filename + ".json", uploaded = False)
+        new = Config(config = config, filename = filename)
         new.save()
+        new.write_to_disk()
+
+    # If modifying old config, update JSON object and write to disk
+    else:
+        old = Config.objects.get(filename=filename)
+        old.config = config
+        old.save()
+        old.write_to_disk()
 
     return JsonResponse("Config created.", safe=False, status=200)
 
@@ -442,8 +441,7 @@ def get_api_target_menu_options(editing_node=False):
     for node in Node.objects.all():
         entries = {}
 
-        with open(node.config_file, 'r') as file:
-            config = json.load(file)
+        config = node.config.config
 
         for i in config:
             if i.startswith("device"):

frontend/node_configuration/migrations/0005_remove_config_uploaded_config_config_config_filename_and_more.py
@@ -1,0 +1,43 @@
+# Generated by Django 4.0.4 on 2023-01-20 11:28
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('node_configuration', '0004_node_floor'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='config',
+            name='uploaded',
+        ),
+        migrations.AddField(
+            model_name='config',
+            name='config',
+            field=models.JSONField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='config',
+            name='filename',
+            field=models.CharField(blank=True, max_length=50, null=True),
+        ),
+        migrations.AddField(
+            model_name='config',
+            name='node',
+            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='config', to='node_configuration.node'),
+        ),
+        migrations.AlterField(
+            model_name='config',
+            name='config_file',
+            field=models.FilePathField(path=None),
+        ),
+        migrations.AlterField(
+            model_name='node',
+            name='config_file',
+            field=models.FilePathField(path=None),
+        ),
+    ]

frontend/node_configuration/migrations/0006_remove_config_config_file_remove_node_config_file_and_more.py
@@ -1,0 +1,31 @@
+# Generated by Django 4.0.4 on 2023-01-20 11:53
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('node_configuration', '0005_remove_config_uploaded_config_config_config_filename_and_more'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='config',
+            name='config_file',
+        ),
+        migrations.RemoveField(
+            model_name='node',
+            name='config_file',
+        ),
+        migrations.AlterField(
+            model_name='config',
+            name='config',
+            field=models.JSONField(),
+        ),
+        migrations.AlterField(
+            model_name='config',
+            name='filename',
+            field=models.CharField(max_length=50),
+        ),
+    ]

frontend/node_configuration/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import os
+import json
 
 CONFIG_DIR = os.environ.get('CONFIG_DIR')
 
@@ -16,18 +17,34 @@ class Node(models.Model):
 
     floor = models.IntegerField(default=1)
 
-    config_file = models.FilePathField(path=CONFIG_DIR)
-
 
 
 class Config(models.Model):
 
     def __str__(self):
-        return self.config_file
+        return self.filename
 
-    config_file = models.FilePathField(path=CONFIG_DIR)
+    # The actual config object
+    config = models.JSONField(null=False, blank=False)
 
-    uploaded = models.BooleanField(default=False)
+    filename = models.CharField(max_length=50, null=False, blank=False)
+
+    node = models.OneToOneField(
+        Node,
+        on_delete=models.CASCADE,
+        related_name='config',
+        null=True,
+        blank=True
+    )
+
+    def read_from_disk(self):
+        with open(CONFIG_DIR + self.filename, 'r') as file:
+            self.config = json.load(file)
+            self.save()
+
+    def write_to_disk(self):
+        with open(CONFIG_DIR + self.filename, 'w') as file:
+            json.dump(self.config, file)
 
 
 

commit 73e578f4ef9ca656b20630d080014a826dc05631

Bug fix: Frontend no longer breaks while trying to upload first-time setup, bug introduced in 00ca9bcd.

frontend/node_configuration/static/node_configuration/submit.js
@@ -120,7 +120,7 @@ async function reupload() {
             // Show loading again, upload setup file
             $("#error-modal").modal("hide");
             show_modal("upload-modal");
-            var result = await send_post_request(base_url + "upload/True", {config: "setup.json", ip: target_ip});
+            var result = await send_post_request(base_url + "setup", {ip: target_ip});
 
             if (result.ok) {
                 // After uploading config, tell user to reboot node then click OK

frontend/node_configuration/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('upload', views.upload, name='upload'),
     path('upload/<str:reupload>', views.upload, name='upload'),
     path('reupload_all', views.reupload_all, name='reupload_all'),
+    path('setup', views.setup, name='setup'),
 
     path('delete_config', views.delete_config, name='delete_config'),
     path('delete_node', views.delete_node, name='delete_node'),

frontend/node_configuration/views.py
@@ -88,6 +88,19 @@ def get_modules(conf):
 
 
 
+def setup(request):
+    if request.method == "POST":
+        data = json.loads(request.body.decode("utf-8"))
+        print(json.dumps(data, indent=4))
+    else:
+        raise Http404("ERROR: Must post data")
+
+    # Upload
+    response = provision("setup.json", data["ip"], [], [])
+    return response
+
+
+
 def upload(request, reupload=False):
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))