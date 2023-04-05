8e4b14e92b16121e822642fb487dd119c3ce1c06
Josh Medeiros <josh@jamedeus.com>
2023-04-05 00:53:59 -0700

    Add script to automate desktop-integration installation.

---
+++ desktop-integration/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install dependencies
+pip3 install -r requirements.txt > /dev/null 2>&1 || { printf "Failed to install dependencies.\n"; exit 1; }
+printf "Dependencies installed.\n"
+
+# Copy executable to local bin
+cp desktop-integration.py /home/$USER/.local/bin/desktop-integration.py || { printf "Failed to install application.\n"; exit 1; }
+printf "Application installed.\n"
+
+# Create service file for current user
+cat > micropython-smarthome-integration.service << EOL
+[Unit]
+Description=Micropython smarthome desktop integration served by gunicorn
+After=network.target
+StartLimitIntervalSec=30
+StartLimitBurst=12
+
+[Service]
+Restart=on-failure
+User=$USER
+Environment="DISPLAY=$DISPLAY"
+WorkingDirectory=/home/$USER/.local/bin
+ExecStart=/home/$USER/.local/bin/gunicorn --workers 2 --timeout 60 --bind 0.0.0.0:5000 desktop-integration:app
+
+[Install]
+WantedBy=network.target
+EOL
+
+# Install + detect new service
+sudo mv micropython-smarthome-integration.service /usr/lib/systemd/system/ || { printf "Failed to install service file.\n"; exit 1; }
+sudo systemctl daemon-reload || { printf "Failed to detect service file.\n"; exit 1; }
+printf "Service installed.\n"
+
+# Enable + start
+sudo systemctl enable --now micropython-smarthome-integration.service || { printf "Failed to start service.\n"; exit 1; }
+printf "\nInstallation complete, service started.\n"
