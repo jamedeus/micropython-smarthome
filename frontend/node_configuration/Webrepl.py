54cb4b03ccc936072572671520320d58b01e9e4c
Josh Medeiros <josh@jamedeus.com>
2023-05-06 23:21:35 -0700

    Add comments, improve variable names, greately improve readability

--- frontend/node_configuration/Webrepl.py
+++ frontend/node_configuration/Webrepl.py
@@ -3,12 +3,7 @@ import sys
 import os
 import struct
 import socket
-import binascii
-import hashlib
 import io
-
-# TODO Attribute? Arg?
-SANDBOX = ""
 
 handshake_message = b"""\
 GET / HTTP/1.1\r
@@ -28,45 +23,68 @@ class websocket:
         self.buf = b""
 
     def write(self, data):
-        l = len(data)
-        if l < 126:
-            hdr = struct.pack(">BB", 0x82, l)
+        # Get correct header for payload size
+        size = len(data)
+        if size < 126:
+            hdr = struct.pack(">BB", 0x82, size)
         else:
-            hdr = struct.pack(">BBH", 0x82, 126, l)
+            hdr = struct.pack(">BBH", 0x82, 126, size)
+
+        # Send header followed by data
         self.s.send(hdr)
         self.s.send(data)
 
-    def recvexactly(self, sz):
-        res = b""
-        while sz:
-            data = self.s.recv(sz)
+    def recvexactly(self, size):
+        response = b""
+        while size:
+            # Read requested number of bytes
+            data = self.s.recv(size)
+
+            # Reached end of data
             if not data:
                 break
-            res += data
-            sz -= len(data)
-        return res
+
+            # Add bytes to response, subtract from remaining length
+            response += data
+            size -= len(data)
+        return response
 
     def read(self, size, text_ok=False):
+        # Read until valid websocket frame found
         if not self.buf:
             while True:
+                # Get frame header
                 hdr = self.recvexactly(2)
                 assert len(hdr) == 2
+
+                # Get frame type, size
                 fl, sz = struct.unpack(">BB", hdr)
+
+                # Frame larger than 126 bytes: read second header, get actual size
                 if sz == 126:
                     hdr = self.recvexactly(2)
                     assert len(hdr) == 2
                     (sz,) = struct.unpack(">H", hdr)
+
+                # Binary frame found
                 if fl == 0x82:
                     break
+
+                # Text frame found
                 if text_ok and fl == 0x81:
                     break
+
+                # Invalid frame, skip
                 while sz:
                     skip = self.s.recv(sz)
                     sz -= len(skip)
+
+            # Read valid frame contents into self.buf
             data = self.recvexactly(sz)
             assert len(data) == sz
             self.buf = data
 
+        # Parse requested number of bytes, truncate self.buf
         d = self.buf[:size]
         self.buf = self.buf[size:]
         assert len(d) == size, len(d)
@@ -74,12 +92,15 @@ class websocket:
 
     # Minimal client handshake for MicroPython, may not comply with standards
     def client_handshake(self):
+        # Make unbuffered file-like obect
         cl = self.s.makefile("rwb", 0)
+        # Send websocket upgrade request to node
         cl.write(handshake_message)
-        l = cl.readline()
+
+        # Read node response until end of message
         while 1:
-            l = cl.readline()
-            if l == b"\r\n":
+            line = cl.readline()
+            if line == b"\r\n":
                 break
 
 
@@ -90,6 +111,7 @@ class Webrepl():
         self.password = password
         self.ws = None
 
+    # Open socket, upgrade to websocket, login with self.password
     def open_connection(self):
         try:
             s = socket.socket()
@@ -141,56 +163,77 @@ class Webrepl():
             if not self.open_connection():
                 raise OSError
 
-        src_fname = (SANDBOX + remote_file).encode("utf-8")
-        rec = struct.pack("<2sBBQLH64s", b"WA", 2, 0, 0, 0, len(src_fname), src_fname)
-        self.ws.write(rec)
+        # Create request: webrepl protocol, operation code 2 (read), request size, encoded filename
+        remote_file = (remote_file).encode("utf-8")
+        request = struct.pack("<2sBBQLH64s", b"WA", 2, 0, 0, 0, len(remote_file), remote_file)
+
+        # Send request (no response = success)
+        self.ws.write(request)
         assert self.read_resp() == 0
-        with open(local_file, "wb") as f:
-            cnt = 0
+
+        with open(local_file, "wb") as output_file:
+            count = 0
             while True:
+                # Request chunk, parse size
                 self.ws.write(b"\0")
                 (sz,) = struct.unpack("<H", self.ws.read(2))
+
+                # End of file reached
                 if sz == 0:
                     break
+
+                # Read bytes into buffer, write to output file
                 while sz:
                     buf = self.ws.read(sz)
                     if not buf:
                         raise OSError()
-                    cnt += len(buf)
-                    f.write(buf)
+                    count += len(buf)
+                    output_file.write(buf)
                     sz -= len(buf)
-                    sys.stdout.write("Received %d bytes\r" % cnt)
+
+                    # Overwrite previous progress report
+                    sys.stdout.write("Received %d bytes\r" % count)
                     sys.stdout.flush()
         print()
         assert self.read_resp() == 0
-
 
     def get_file_mem(self, remote_file):
         if self.ws is None:
             if not self.open_connection():
                 raise OSError
 
-        src_fname = (SANDBOX + remote_file).encode("utf-8")
-        rec = struct.pack("<2sBBQLH64s", b"WA", 2, 0, 0, 0, len(src_fname), src_fname)
-        self.ws.write(rec)
+        # Create request: webrepl protocol, operation code 2 (read), request size, encoded filename
+        remote_file = (remote_file).encode("utf-8")
+        request = struct.pack("<2sBBQLH64s", b"WA", 2, 0, 0, 0, len(remote_file), remote_file)
+
+        # Send request (no response = success)
+        self.ws.write(request)
         assert self.read_resp() == 0
 
+        # Create buffer for received file
         output = io.BytesIO()
 
-        cnt = 0
+        count = 0
         while True:
+            # Request chunk, parse size
             self.ws.write(b"\0")
             (sz,) = struct.unpack("<H", self.ws.read(2))
+
+            # End of file reached
             if sz == 0:
                 break
+
+            # Read bytes into buffer, write to output buffer
             while sz:
                 buf = self.ws.read(sz)
                 if not buf:
                     raise OSError()
-                cnt += len(buf)
+                count += len(buf)
                 output.write(buf)
                 sz -= len(buf)
-                sys.stdout.write("Received %d bytes\r" % cnt)
+
+                # Overwrite previous progress report
+                sys.stdout.write("Received %d bytes\r" % count)
                 sys.stdout.flush()
 
         print()
@@ -203,21 +246,32 @@ class Webrepl():
             if not self.open_connection():
                 raise OSError
 
+        # Create request: webrepl protocol, operation code 1 (write), payload size, encoded filename
         sz = os.stat(local_file)[6]
-        dest_fname = (SANDBOX + remote_file).encode("utf-8")
-        rec = struct.pack("<2sBBQLH64s", b"WA", 1, 0, 0, sz, len(dest_fname), dest_fname)
-        self.ws.write(rec[:10])
-        self.ws.write(rec[10:])
+        remote_file = (remote_file).encode("utf-8")
+        request = struct.pack("<2sBBQLH64s", b"WA", 1, 0, 0, sz, len(remote_file), remote_file)
+
+        # Send first 10 bytes of request, then all remaining bytes (no response = success)
+        self.ws.write(request[:10])
+        self.ws.write(request[10:])
         assert self.read_resp() == 0
-        cnt = 0
-        with open(local_file, "rb") as f:
+
+        count = 0
+        with open(local_file, "rb") as source_file:
             while True:
-                sys.stdout.write("Sent %d of %d bytes\r" % (cnt, sz))
+                # Overwrite previous progress report
+                sys.stdout.write("Sent %d of %d bytes\r" % (count, sz))
                 sys.stdout.flush()
-                buf = f.read(1024)
+
+                # Read next chunk
+                buf = source_file.read(1024)
+
+                # End of file reached
                 if not buf:
                     break
+
+                # Send chunk
                 self.ws.write(buf)
-                cnt += len(buf)
+                count += len(buf)
         print()
         assert self.read_resp() == 0
