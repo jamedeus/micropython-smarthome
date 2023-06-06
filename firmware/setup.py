fc089da0bc374aa37f721b61f4cb563d7ae19dea
Josh Medeiros <josh@jamedeus.com>
2023-06-05 18:12:31 -0700

    Regression fix: No longer requires timezone param (removed in 0f478add)

--- firmware/setup.py
+++ firmware/setup.py
@@ -39,7 +39,6 @@ def create_config_file(data):
                 "id": "",
                 "location": "",
                 "floor": "",
-                "timezone": data["timezone"],
                 "schedule_keywords": {}
             },
             "wifi": {
