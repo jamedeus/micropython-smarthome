5d20fab519912b7cb9e6b1a2b05e97873c13b5ea
Josh Medeiros <josh@jamedeus.com>
2023-07-06 02:04:12 -0700

    Bug fixes (increment_rule endpoint): No longer crashes when target instance not found. No longer crashes when increment amount is float. Add missing unit tests.

--- core/Api.py
+++ core/Api.py
@@ -251,15 +251,23 @@ def increment_rule(args):
 
     target = app.config.find(args[0])
 
-    if is_device_or_sensor(target.name):
-        if target._type not in ['dimmer', 'bulb', 'pwm', 'wled', 'si7021', 'pir']:
-            return {"ERROR": "Target must accept int rule"}
-    else:
-        return {"ERROR": "Target must be device or sensor with int rule"}
+    if not target:
+        return {"ERROR": "Instance not found, use status to see options"}
 
+    if target._type in ['dimmer', 'bulb', 'pwm', 'wled']:
+        return increment_int_rule(target, args)
+
+    elif target._type in ['si7021', 'pir']:
+        return increment_float_rule(target, args)
+
+    else:
+        return {"ERROR": "Unsupported target, must accept int or float rule"}
+
+
+def increment_int_rule(target, args):
     try:
         increment = int(args[1])
-    except TypeError:
+    except (TypeError, ValueError):
         return {"ERROR": "Argument must be integer"}
 
     try:
@@ -267,11 +275,27 @@ def increment_rule(args):
     except ValueError:
         return {"ERROR": f"Unable to increment current rule ({target.current_rule})"}
 
-    if target._type in ['dimmer', 'bulb', 'pwm', 'wled']:
-        if new > target.max_bright:
-            new = target.max_bright
-        if new < target.min_bright:
-            new = target.min_bright
+    if new > target.max_bright:
+        new = target.max_bright
+    if new < target.min_bright:
+        new = target.min_bright
+
+    if target.set_rule(new):
+        return {target.name: new}
+    else:
+        return {"ERROR": "Invalid rule"}
+
+
+def increment_float_rule(target, args):
+    try:
+        increment = float(args[1])
+    except TypeError:
+        return {"ERROR": "Argument must be integer or float"}
+
+    try:
+        new = float(target.current_rule) + increment
+    except ValueError:
+        return {"ERROR": f"Unable to increment current rule ({target.current_rule})"}
 
     if target.set_rule(new):
         return {target.name: new}
