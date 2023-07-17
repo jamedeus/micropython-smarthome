4c0235bd5b7b200bcdad60b08cd03579df0f2f80
Josh Medeiros <josh@jamedeus.com>
2023-07-17 11:02:00 -0700

    Bug fix: mock WLAN class now singleton, allows test to get same instance used by code under test (confirm isconnected etc). WLAN.ifconfig now returns localhost (prevent connection timeout when tests attempt to connect to fake IP).

--- mock_test_environment/mocks/network.py
+++ mock_test_environment/mocks/network.py
@@ -2,9 +2,15 @@ STA_IF = "STA_IF"
 
 
 class WLAN:
-    def __init__(self, interface=STA_IF):
-        self._active = False
-        self.connected = False
+    # Singleton, simulate class reading hardware interface
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._active = False
+            cls._instance.connected = False
+        return cls._instance
 
     def active(self, state=None):
         if state is None:
@@ -19,4 +25,4 @@ class WLAN:
         self.connected = True
 
     def ifconfig(*args):
-        return ('192.168.1.123', '255.255.255.0', '192.168.1.1', '192.168.1.100')
+        return ('127.0.0.1', '255.255.255.0', '192.168.1.1', '192.168.1.100')
