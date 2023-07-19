eeba2231fa93284c4e3cd353c7d04b1085dedf5d
Josh Medeiros <josh@jamedeus.com>
2023-07-18 23:49:30 -0700

    Add unit tests for DimmableLight base class

---
+++ tests/test_device_dimmablelight.py
@@ -0,0 +1,122 @@
+import unittest
+import SoftwareTimer
+from DimmableLight import DimmableLight
+
+
+class TestDimmableLight(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Instantiate with default_rule 50, min_bright 1, max_bright 100
+        cls.instance = DimmableLight("device1", "device1", "DimmableLight", True, 50, 50, "1", "100")
+
+    def test_01_initial_state(self):
+        # Confirm min/max cast to int
+        self.assertIsInstance(self.instance, DimmableLight)
+        self.assertIsInstance(self.instance.min_bright, int)
+        self.assertIsInstance(self.instance.max_bright, int)
+        self.assertFalse(self.instance.fading)
+
+    def test_02_instantiate_with_invalid_min_max(self):
+        # AttributeError should be raised if default_rule is outside limits
+        try:
+            DimmableLight("device1", "device1", "DimmableLight", True, 50, 500, "1", "100")
+            # Should not make it to this line, test failed
+            self.assertFalse(True)
+        except AttributeError:
+            # Should raise exception, test passed
+            self.assertTrue(True)
+
+    def test_03_increment_invalid_rule(self):
+        # Set non-integer rule
+        self.instance.current_rule = 'disabled'
+        # Attempt to increment, confirm error
+        self.assertEqual(
+            self.instance.increment_rule(1),
+            {"ERROR": "Unable to increment current rule (disabled)"}
+        )
+
+    def test_04_increment_rule_out_of_range(self):
+        # Set rule to max_bright
+        self.instance.set_rule(100)
+        # Increment should still return True
+        self.assertTrue(self.instance.increment_rule(1))
+        # Confirm rule did not change
+        self.assertEqual(self.instance.current_rule, 100)
+
+        # Repeat for min_bright
+        self.instance.set_rule(1)
+        self.assertTrue(self.instance.increment_rule(-1))
+        self.assertEqual(self.instance.current_rule, 1)
+
+    def test_05_fade_rule_on_boot(self):
+        # Set rule to None, simulate first rule on boot
+        self.instance.current_rule = None
+        # Set fade rule, should immediately set current_rule to target
+        self.assertTrue(self.instance.set_rule('fade/100/3600'))
+        self.assertEqual(self.instance.current_rule, 100)
+        # Confirm not fading, no timer created
+        self.assertFalse(self.instance.fading)
+        self.assertNotIn(f'{self.instance.name}_fade', str(SoftwareTimer.timer.schedule))
+
+    def test_06_start_fade_already_at_target(self):
+        # Attempt to fade to current_rule, should return immediately
+        self.instance.current_rule = 100
+        self.assertTrue(self.instance.set_rule('fade/100/3600'))
+        # Confirm not fading, no timer created
+        self.assertFalse(self.instance.fading)
+        self.assertNotIn(f'{self.instance.name}_fade', str(SoftwareTimer.timer.schedule))
+
+    def test_07_start_fade_while_disabled(self):
+        # Attempt to fade to 100 while disabled
+        self.instance.current_rule = 'disabled'
+        self.assertTrue(self.instance.set_rule('fade/100/3600'))
+        # Confirm timer created, starting brightness = 0
+        self.assertEqual(self.instance.fading['starting_brightness'], 0)
+        self.assertIn(f'{self.instance.name}_fade', str(SoftwareTimer.timer.schedule))
+        SoftwareTimer.timer.cancel(f'{self.instance.name}_fade')
+
+    def test_08_fade_complete(self):
+        # Simulate fade up in progress
+        self.instance.fading = {
+            "started": SoftwareTimer.timer.epoch_now(),
+            "starting_brightness": 1,
+            "target": 50,
+            "period": 1000,
+            "down": False
+        }
+        # Simulate target rule reached
+        self.instance.current_rule = 50
+        self.assertTrue(self.instance.fade_complete())
+        # Confirm fade dict removed
+        self.assertFalse(self.instance.fading)
+
+        # Simulate fade down in progress
+        self.instance.fading = {
+            "started": SoftwareTimer.timer.epoch_now(),
+            "starting_brightness": 100,
+            "target": 0,
+            "period": 1000,
+            "down": True
+        }
+        self.instance.state = True
+        # Simulate target rule reached
+        self.instance.current_rule = 0
+        self.assertTrue(self.instance.fade_complete())
+        # Confirm fade dict removed
+        self.assertFalse(self.instance.fading)
+        self.assertFalse(self.instance.state)
+
+    def test_09_disable_while_fading(self):
+        # Simulate fade in progress
+        self.instance.fading = {
+            "started": SoftwareTimer.timer.epoch_now(),
+            "starting_brightness": 1,
+            "target": 50,
+            "period": 1000,
+            "down": False
+        }
+        # Disable, confirm fade completes
+        self.instance.enabled = False
+        self.assertTrue(self.instance.fade_complete())
+        self.assertFalse(self.instance.fading)
