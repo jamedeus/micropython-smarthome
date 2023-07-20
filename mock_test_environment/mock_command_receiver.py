2ccd4ffa922b4ce7a04291195704b454f6391702
Josh Medeiros <josh@jamedeus.com>
2023-07-19 22:34:31 -0700

    Add endpoints to simulate desktop integration

--- mock_test_environment/mock_command_receiver.py
+++ mock_test_environment/mock_command_receiver.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 tasmota_relay_last = "ON"
 wled_brightness = 255
 wled_state = 1
+desktop_state = 'On'
 
 
 # Tasmota relay mock
@@ -52,6 +53,35 @@ def wled():
     # Turn off
     else:
         return b'<?xml version="1.0" ?><vs><ac>0</ac><cl>255</cl><cl>160</cl><cl>0</cl><cs>0</cs><cs>0</cs><cs>0</cs><ns>0</ns><nr>1</nr><nl>0</nl><nf>1</nf><nd>60</nd><nt>0</nt><fx>97</fx><sx>128</sx><ix>255</ix><fp>11</fp><wv>-1</wv><ws>0</ws><ps>1</ps><cy>0</cy><ds>WLED</ds><ss>0</ss></vs>'
+
+
+# Desktop integration - get monitor state
+@app.get("/state")
+def get_dpms_state():
+    global desktop_state
+    return {'state': desktop_state}, 200
+
+
+# Desktop integration - get idle time
+@app.get("/idle_time")
+def get_idle_time():
+    return {'idle_time': 523}, 200
+
+
+# Desktop integration - turn screen on
+@app.get("/on")
+def monitor_on():
+    global desktop_state
+    desktop_state = 'On'
+    return {'state': 'on'}, 200
+
+
+# Desktop integration - turn screen off
+@app.get("/off")
+def monitor_off():
+    global desktop_state
+    desktop_state = 'Off'
+    return {'state': 'off'}, 200
 
 
 if __name__ == '__main__':
