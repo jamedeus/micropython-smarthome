jamedeus/micropython-smarthome

commit 9438e3e82331917e9bc33a109a5805e0a0802491

Move all non-django tests into tests/ dir and organize into cli, client, firmware subdirs. Move mock_test_environment to tests/mock_environment, update test script + documentation.
Mock environment no longer requires symlinks to discover tests

CLI/provision.py
@@ -142,12 +142,12 @@ class Provisioner():
     # Upload unit tests to IP address
     def upload_tests(self, ip):
         # Load unit test config file
-        with open(os.path.join(repo, "tests", "unit_test_config.json"), 'r') as file:
+        with open(os.path.join(repo, "tests", 'firmware', "unit_test_config.json"), 'r') as file:
             config = json.load(file)
 
-        # Get list of relative paths for all unit tests (Example: 'tests/test_core_config.py')
-        tests = [i for i in os.listdir(os.path.join(repo, 'tests')) if i.startswith('test_')]
-        tests = [os.path.join('tests', i) for i in tests]
+        # Get list of relative paths for all unit tests (Example: 'tests/firmware/test_core_config.py')
+        tests = [i for i in os.listdir(os.path.join(repo, 'tests', 'firmware')) if i.startswith('test_')]
+        tests = [os.path.join('tests', 'firmware', i) for i in tests]
 
         # Build list of all device and sensor modules
         modules = []

tests/cli/test_provision.py
@@ -261,7 +261,7 @@ class TestInstantiation(TestCase):
 
 class TestGetModules(TestCase):
     def setUp(self):
-        with open('tests/unit-test-config.json', 'r') as file:
+        with open(os.path.join(repo, "tests", "cli", "unit-test-config.json"), 'r') as file:
             self.config = json.load(file)
 
     def test_get_modules_full_config(self):

.gitlab-ci.yml
@@ -19,9 +19,8 @@ test_firmware:
     - pip3 install pipenv
     - export PATH=$PATH:/home/gitlab-runner/.local/bin
     - pipenv install
-    - cd mock_test_environment/
-    - for i in ../tests/test_*; do ln -s $i .; done
-    - pipenv run coverage run --source='../core,../devices,../sensors' runtests.py
+    - cd tests/mock_environment/
+    - pipenv run coverage run --source='../../core,../../devices,../../sensors' runtests.py
     - pipenv run coverage report -m --precision=1
   coverage: '/TOTAL.*\s+(\d+\.\d+\%)$/'
   rules:
@@ -56,8 +55,8 @@ test_cli:
     - pip3 install pipenv
     - export PATH=$PATH:/home/gitlab-runner/.local/bin
     - pipenv install
-    - cd CLI/
-    - pipenv run coverage run --omit='tests/*' -m unittest discover tests/
+    - export PYTHONPATH=$PYTHONPATH:`pwd`/CLI
+    - pipenv run coverage run --omit='tests/*' -m unittest discover tests/cli
     - pipenv run coverage report -m --precision=1
   coverage: '/TOTAL.*\s+(\d+\.\d+\%)$/'
   rules:

tests/cli/test_webrepl.py
@@ -6,8 +6,15 @@ from unittest import TestCase
 from unittest.mock import patch, MagicMock
 from Webrepl import Webrepl, websocket, handshake_message
 
+# Get full paths to repository root directory, CLI tools directory
+cli = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+repo = os.path.split(cli)[0]
+
+# Get absolute path to test config
+test_config_path = os.path.join(repo, "tests", "cli", "unit-test-config.json")
+
 # Binary contents of test config file, used as payload in simulated Webrepl connections
-with open('tests/unit-test-config.json', 'r') as file:
+with open(test_config_path, 'r') as file:
     binary_unit_test_config = json.dumps(json.load(file)).encode()
 
 # Used to track current position in binary_unit_test_config (read function called multiple times)
@@ -277,7 +284,7 @@ class WebreplTests(TestCase):
              patch.object(node, 'read_resp', side_effect=[0, 0]) as mock_read_resp:
 
             # Call method, confirm correct methods called
-            node.put_file('tests/unit-test-config.json', 'config.json')
+            node.put_file(test_config_path, 'config.json')
             self.assertTrue(mock_websocket.write.called)
             self.assertTrue(mock_read_resp.called)
 
@@ -285,7 +292,7 @@ class WebreplTests(TestCase):
         node = Webrepl('123.45.67.89', 'password')
 
         # Read file into variable
-        with open('tests/unit-test-config.json', 'r') as file:
+        with open(test_config_path, 'r') as file:
             config = json.load(file)
 
         # Mock websocket and read_resp to allow send to complete

commit 60426dcc698b0b2b9fd74bd7af9b708ab259169d

Bug fix: upload_tests modules dict now contains correct filenames (broken since 9438e3e8)
Issue: Filename was parsed from path by taking the second item in .split("/") output. This was correct when tests were at tests/* but broke when moved to tests/firmware/*, resulting in a file `firmware` being written and overwritten as each test was uploaded. Fixed by parsing last field instead of second.

Issue: unit test used called_with incorrectly, resulting in test passing incorrectly and preventing the issue from being discovered. Test now fixed but issue likely exists elsewhere.

CLI/provision.py
@@ -164,7 +164,7 @@ class Provisioner():
 
         # Convert to dict containing pairs of local:remote filesystem paths
         # Local path is uploaded to remote path on target ESP32
-        modules = {os.path.join(repo, i): i.split("/")[1] for i in modules}
+        modules = {os.path.join(repo, i): i.split("/")[-1] for i in modules}
 
         # Add unit_test_main.py (must add after dict comprehension, remote name is different)
         modules[os.path.join(repo, 'tests', 'firmware', 'unit_test_main.py')] = 'main.py'

tests/cli/test_provision.py
@@ -166,30 +166,36 @@ class TestInstantiation(TestCase):
             os.path.join(repo, 'sensors', 'Dummy.py'): 'Dummy.py',
             os.path.join(repo, 'sensors', 'Switch.py'): 'Switch.py',
             os.path.join(repo, 'sensors', 'Desktop_trigger.py'): 'Desktop_trigger.py',
-            os.path.join(repo, 'tests', 'test_device_desktop_target.py'): 'test_device_desktop_target.py',
-            os.path.join(repo, 'tests', 'test_device_tplink.py'): 'test_device_tplink.py',
-            os.path.join(repo, 'tests', 'test_sensor_desktop_trigger.py'): 'test_sensor_desktop_trigger.py',
-            os.path.join(repo, 'tests', 'test_sensor_switch.py'): 'test_sensor_switch.py',
-            os.path.join(repo, 'tests', 'test_sensor_thermostat.py'): 'test_sensor_thermostat.py',
-            os.path.join(repo, 'tests', 'test_device_apitarget.py'): 'test_device_apitarget.py',
-            os.path.join(repo, 'tests', 'test_device_wled.py'): 'test_device_wled.py',
-            os.path.join(repo, 'tests', 'test_device_dumbrelay.py'): 'test_device_dumbrelay.py',
-            os.path.join(repo, 'tests', 'test_core_config.py'): 'test_core_config.py',
-            os.path.join(repo, 'tests', 'test_api_api.py'): 'test_api_api.py',
-            os.path.join(repo, 'tests', 'test_device_mosfet.py'): 'test_device_mosfet.py',
-            os.path.join(repo, 'tests', 'test_device_irblaster.py'): 'test_device_irblaster.py',
-            os.path.join(repo, 'tests', 'test_sensor_motionsensor.py'): 'test_sensor_motionsensor.py',
-            os.path.join(repo, 'tests', 'test_device_ledstrip.py'): 'test_device_ledstrip.py',
-            os.path.join(repo, 'tests', 'test_sensor_dummy.py'): 'test_sensor_dummy.py',
-            os.path.join(repo, 'tests', 'test_device_relay.py'): 'test_device_relay.py',
-            os.path.join(repo, 'tests', 'test_core_main_loop.py'): 'test_core_main_loop.py',
-            os.path.join(repo, 'tests', 'test_core_softwaretimer.py'): 'test_core_softwaretimer.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_desktop_target.py'): 'test_device_desktop_target.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_device.py'): 'test_device_device.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_dimmablelight.py'): 'test_device_dimmablelight.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_tplink.py'): 'test_device_tplink.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_desktop_trigger.py'): 'test_sensor_desktop_trigger.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_switch.py'): 'test_sensor_switch.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_sensor.py'): 'test_sensor_sensor.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_thermostat.py'): 'test_sensor_thermostat.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_apitarget.py'): 'test_device_apitarget.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_wled.py'): 'test_device_wled.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_dumbrelay.py'): 'test_device_dumbrelay.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_config.py'): 'test_core_config.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_api_api.py'): 'test_api_api.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_mosfet.py'): 'test_device_mosfet.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_irblaster.py'): 'test_device_irblaster.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_motionsensor.py'): 'test_sensor_motionsensor.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_ledstrip.py'): 'test_device_ledstrip.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_sensor_dummy.py'): 'test_sensor_dummy.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_device_relay.py'): 'test_device_relay.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_main_loop.py'): 'test_core_main_loop.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_softwaretimer.py'): 'test_core_softwaretimer.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_util.py'): 'test_core_util.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_group.py'): 'test_core_group.py',
+            os.path.join(repo, 'tests', 'firmware', 'test_core_wifi_setup.py'): 'test_core_wifi_setup.py',
             os.path.join(repo, 'core', 'Config.py'): 'Config.py',
             os.path.join(repo, 'core', 'Group.py'): 'Group.py',
             os.path.join(repo, 'core', 'SoftwareTimer.py'): 'SoftwareTimer.py',
             os.path.join(repo, 'core', 'Api.py'): 'Api.py',
             os.path.join(repo, 'core', 'util.py'): 'util.py',
-            os.path.join(repo, 'tests', 'unit_test_main.py'): 'main.py'
+            os.path.join(repo, 'tests', 'firmware', 'unit_test_main.py'): 'main.py'
         }
 
         # Mock args to upload unit tests to 192.168.1.123
@@ -204,8 +210,9 @@ class TestInstantiation(TestCase):
 
             # Instantiate, confirm called once with given IP + test modules
             Provisioner(args, '')
-            self.assertTrue(mock_provision.called_with('192.168.1.123'))
-            self.assertTrue(mock_provision.called_with(test_modules))
+            args = mock_provision.call_args[0]
+            self.assertEqual(args[0], '192.168.1.123')
+            self.assertEqual(args[3], test_modules)
             self.assertTrue(mock_provision.called_once)
 
     def test_provision_manual_args(self):