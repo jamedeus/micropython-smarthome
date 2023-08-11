jamedeus/micropython-smarthome

commit 187eb8d2271807cb32887d5edec1e5442f919ddd

ApiTarget send method now detects failed commands, returns False, logs error response

devices/ApiTarget.py
@@ -8,11 +8,14 @@ log = logging.getLogger("ApiTarget")
 
 
 class ApiTarget(Device):
-    def __init__(self, name, nickname, _type, default_rule, ip):
+    def __init__(self, name, nickname, _type, default_rule, ip, port=8123):
         super().__init__(name, nickname, _type, True, None, default_rule)
 
         # IP that API command is sent to
         self.ip = ip
+
+        # Port defaults to 8123 except in unit tests
+        self.port = port
 
         # Prevent instantiating with invalid default_rule
         if str(self.default_rule).lower() in ("enabled", "disabled"):
@@ -116,17 +119,39 @@ class ApiTarget(Device):
             print(f"{self.name}: Failed to change rule to {rule}")
             return False
 
+    # Takes payload and response, writes multiline log with indent for readability
+    def log_failed_request(self, msg, err):
+        log.error(f"""{self.name}: Send method failed
+        Payload: {msg}
+        Response: {err}""")
+        print(f"{self.name}: Send method failed with payload {msg}")
+        print(f"{self.name}: Response: {err}")
+
     async def request(self, msg):
-        reader, writer = await asyncio.open_connection(self.ip, 8123)
+        reader, writer = await asyncio.open_connection(self.ip, self.port)
         try:
             writer.write('{}\n'.format(json.dumps(msg)).encode())
             await writer.drain()
             # TODO change this limit?
-            await reader.read(1000)
+            res = await reader.read(1000)
+            res = json.loads(res)
         except OSError:
+            res = False
             pass
         writer.close()
         await writer.wait_closed()
+
+        # Return False if request failed
+        if not res:
+            return False
+
+        # Log payload + error and return False if response contains error
+        if "Error" in res.keys() or "ERROR" in res.keys():
+            self.log_failed_request(msg, res)
+            return False
+
+        # Return True if request successful
+        return True
 
     def send(self, state=1):
         # Refuse to turn disabled device on, but allow turning off
@@ -141,7 +166,9 @@ class ApiTarget(Device):
                 # If rule is ignore, do nothing
                 return True
 
-            asyncio.create_task(self.request(self.current_rule["on"]))
+            # Send request, return False if failed
+            if not asyncio.run(self.request(self.current_rule["on"])):
+                return False
 
             # Reset motion sensor to allow retriggering the remote motion sensor (restarts reset timer)
             # Retrigger when motion = True only restarts sensor's own resetTimer, but does not send another API command
@@ -155,7 +182,9 @@ class ApiTarget(Device):
                 # If rule is ignore, do nothing
                 return True
 
-            asyncio.create_task(self.request(self.current_rule["off"]))
+            # Send request, return False if failed
+            if not asyncio.run(self.request(self.current_rule["on"])):
+                return False
 
         # Tells main loop send succeeded
         return True

commit 293032c5bd662c81fdc26acafa61cd2695e1eaf4

Bug fix: ApiTarget send method no longer ignores argument, add regression test
Issue: Typo in 187eb8d2 caused "on" rule to be used regardless of argument, undetected for 2 weeks due to gap in test coverage

devices/ApiTarget.py
@@ -183,7 +183,7 @@ class ApiTarget(Device):
                 return True
 
             # Send request, return False if failed
-            if not asyncio.run(self.request(self.current_rule["on"])):
+            if not asyncio.run(self.request(self.current_rule["off"])):
                 return False
 
         # Tells main loop send succeeded

tests/firmware/test_device_apitarget.py
@@ -1,7 +1,13 @@
+import sys
 import json
 import unittest
 from ApiTarget import ApiTarget
 from MotionSensor import MotionSensor
+from cpython_only import cpython_only
+
+# Import dependencies for tests that only run in mocked environment
+if sys.implementation.name == 'cpython':
+    from unittest.mock import patch
 
 # Read mock API receiver address
 with open('config.json', 'r') as file:
@@ -214,3 +220,21 @@ class TestApiTarget(unittest.TestCase):
 
         # Should reject turn_on/turn_off for sensors
         self.assertFalse(self.instance.rule_validator({'on': ['turn_on', 'sensor1'], 'off': ['turn_off', 'sensor2']}))
+
+    # Original bug: A typo in 187eb8d2 caused the send method to use the "on" rule
+    # regardless of state argument. This was not detected for 2 weeks because no
+    # existing tests verified that on and off rules were used correctly.
+    @cpython_only
+    def test_16_regression_send_ignores_state(self):
+        # Set different on and off rules
+        self.instance.set_rule({'on': ['turn_on', 'device2'], 'off': ['turn_off', 'device2']})
+
+        # Call send with mocked request method to
+        with patch.object(ApiTarget, 'request', return_value=True) as mock_request:
+            # Turn on, confirm correct arg passed
+            self.instance.send(1)
+            self.assertEqual(mock_request.call_args_list[0][0][0], ['turn_on', 'device2'])
+
+            # Turn off, confirm correct arg passed
+            self.instance.send(0)
+            self.assertEqual(mock_request.call_args_list[1][0][0], ['turn_off', 'device2'])