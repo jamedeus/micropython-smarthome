jamedeus/micropython-smarthome

commit 573949dee3c410c672b2a13165fd0afb517eb5bc

Update frontend for compatibility with config file syntax changes in 5c0f4623, 9aa2a7f4, c9a8eae9
Add migration to update existing config files and database entries to new syntax

frontend/node_configuration/templates/node_configuration/edit-config.html
@@ -470,13 +470,13 @@
                                     </div>
 
                                     <div class="mb-2">
-                                        <label for="device{{ forloop.counter }}-min" class="device{{forloop.counter}}"><b>Min brightness:</b></label>
-                                        <input type="text" class="form-control device{{forloop.counter}} pwm-limits" id="device{{ forloop.counter }}-min" placeholder="0" value="{{params.min}}" required>
+                                        <label for="device{{ forloop.counter }}-min_bright" class="device{{forloop.counter}}"><b>Min brightness:</b></label>
+                                        <input type="text" class="form-control device{{forloop.counter}} pwm-limits" id="device{{ forloop.counter }}-min_bright" placeholder="0" value="{{params.min_bright}}" required>
                                     </div>
 
                                     <div class="mb-2">
-                                        <label for="device{{ forloop.counter }}-max" class="device{{forloop.counter}}"><b>Max brightness:</b></label>
-                                        <input type="text" class="form-control device{{forloop.counter}} pwm-limits" id="device{{ forloop.counter }}-max" placeholder="1023" value="{{params.max}}" required>
+                                        <label for="device{{ forloop.counter }}-max_bright" class="device{{forloop.counter}}"><b>Max brightness:</b></label>
+                                        <input type="text" class="form-control device{{forloop.counter}} pwm-limits" id="device{{ forloop.counter }}-max_bright" placeholder="1023" value="{{params.max_bright}}" required>
                                     </div>
 
                                     <div class="mb-2">

frontend/api/views.py
@@ -123,7 +123,7 @@ def get_api_target_instance_options(node, status):
 
     # Find all api-target instances, find same instance in options object, add options to output
     for i in config:
-        if is_device(i) and config[i]['type'] == 'api-target':
+        if is_device(i) and config[i]['_type'] == 'api-target':
             # Find section in options object with matching IP, add to context
             for node in options['addresses']:
                 if options['addresses'][node] == config[i]['ip']:

frontend/node_configuration/get_api_target_menu_options.py
@@ -21,7 +21,7 @@ def convert_config_to_api_target_options(config):
             # Instance string format: id-nickname (type)
             # Frontend splits, uses "nickname (type)" for dropdown option innerHTML, uses "id" for value
             # Backend only receives values (id) for config generation
-            instance_string = f'{i}-{config[i]["nickname"]} ({config[i]["type"]})'
+            instance_string = f'{i}-{config[i]["nickname"]} ({config[i]["_type"]})'
 
             # All devices have same options
             if i.startswith("device"):
@@ -37,7 +37,7 @@ def convert_config_to_api_target_options(config):
                 ]
 
             # All sensors have same options except thermostat and switch (trigger unsupported)
-            elif i.startswith("sensor") and config[i]["type"] not in ["si7021", "switch"]:
+            elif i.startswith("sensor") and config[i]["_type"] not in ["si7021", "switch"]:
                 result[instance_string] = [
                     'enable',
                     'disable',

frontend/node_configuration/migrations/0010_auto_20230526_0246.py
@@ -1,0 +1,50 @@
+# Generated by Django 4.2 on 2023-05-26 09:46
+
+import json
+from django.db import migrations
+from django.conf import settings
+
+
+def fix_config_type(apps, schema_editor):
+    Config = apps.get_model('node_configuration', 'Config')
+    for config in Config.objects.all():
+        # Replace all "type" attributes with "_type"
+        for i in config.config:
+            if not i.startswith('device') and not i.startswith('sensor'): continue
+            config.config[i]['_type'] = config.config[i]['type']
+            del config.config[i]['type']
+        config.save()
+
+
+def fix_config_pwm_limits(apps, schema_editor):
+    Config = apps.get_model('node_configuration', 'Config')
+    for config in Config.objects.all():
+        # Replace all "type" attributes with "_type"
+        for i in config.config:
+            if not i.startswith('device'): continue
+            if not config.config[i]['_type'] == 'pwm': continue
+
+            config.config[i]['min_bright'] = config.config[i]['min']
+            config.config[i]['max_bright'] = config.config[i]['max']
+            del config.config[i]['min']
+            del config.config[i]['max']
+        config.save()
+
+
+def update_config_on_disk(apps, scheda_editor):
+    Config = apps.get_model('node_configuration', 'Config')
+    for config in Config.objects.all():
+        with open(settings.CONFIG_DIR + config.filename, 'w') as file:
+            json.dump(config.config, file)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("node_configuration", "0009_auto_pre_populate_sunrise_sunset_keywords"),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_config_type),
+        migrations.RunPython(fix_config_pwm_limits),
+        migrations.RunPython(update_config_on_disk)
+    ]

frontend/node_configuration/static/node_configuration/add-instance.js
@@ -340,13 +340,13 @@ async function load_device_section(select) {
                     </div>
 
                     <div class="mb-2">
-                        <label for="device${index}-min" class="device${index}"><b>Min brightness:</b></label>
-                        <input type="min" class="form-control device${index} pwm-limits" id="device${index}-min" placeholder="0" required>
+                        <label for="device${index}-min_bright" class="device${index}"><b>Min brightness:</b></label>
+                        <input type="min" class="form-control device${index} pwm-limits" id="device${index}-min_bright" placeholder="0" required>
                     </div>
 
                     <div class="mb-2">
-                        <label for="device${index}-max" class="device${index}"><b>Max brightness:</b></label>
-                        <input type="text" class="form-control device${index} pwm-limits" id="device${index}-max" placeholder="1023" required>
+                        <label for="device${index}-max_bright" class="device${index}"><b>Max brightness:</b></label>
+                        <input type="text" class="form-control device${index} pwm-limits" id="device${index}-max_bright" placeholder="1023" required>
                     </div>
 
                     <div class="mb-2">
@@ -442,8 +442,8 @@ async function load_device_section(select) {
 
     // Add listener for PWM max/min fields
     if (selected == "pwm") {
-        document.getElementById(`device${index}-max`).addEventListener('input', pwmLimits);
-        document.getElementById(`device${index}-min`).addEventListener('input', pwmLimits);
+        document.getElementById(`device${index}-max_bright`).addEventListener('input', pwmLimits);
+        document.getElementById(`device${index}-min_bright`).addEventListener('input', pwmLimits);
     };
 
     // Check if IrBlaster selected in any device dropdown

frontend/node_configuration/static/node_configuration/classes.js
@@ -25,7 +25,7 @@ class Device {
 
     // Create property for each field in addDevice section
     getParams() {
-        this.type = document.getElementById(`${this.id.replace("device", "deviceType")}`).value;
+        this._type = document.getElementById(`${this.id.replace("device", "deviceType")}`).value;
 
         var params = document.getElementById(`add${this.id.replace("device", "DeviceOptions")}`).children;
 
@@ -37,12 +37,12 @@ class Device {
             } catch(err) {};
         };
 
-        if (this.type == "ir-blaster") {
+        if (this._type == "ir-blaster") {
             // Remove empty property (checkbox inputs don't have value)
             delete this.undefined;
             // Get checkbox inputs selections
             this.getIrTargets();
-        } else if (this.type == "dimmer" || this.type == "bulb" || this.type == "pwm" || this.type == "wled") {
+        } else if (this._type == "dimmer" || this._type == "bulb" || this._type == "pwm" || this._type == "wled") {
             this['default_rule'] = parseInt(document.getElementById(this.id + "-default_rule").value);
         };
     };
@@ -73,7 +73,7 @@ class Device {
 
     // Get array containing all selected virtual IR remotes (first page)
     getIrTargets() {
-        if (this.type != "ir-blaster") { return };
+        if (this._type != "ir-blaster") { return };
 
         var checks = document.getElementsByClassName('ir-target');
 
@@ -115,7 +115,7 @@ class Sensor {
 
     // Create property for each field in addDevice section
     getParams() {
-        this.type = document.getElementById(`${this.id.replace("sensor", "sensorType")}`).value;
+        this._type = document.getElementById(`${this.id.replace("sensor", "sensorType")}`).value;
 
         var params = document.getElementById(`add${this.id.replace("sensor", "SensorOptions")}`).children;
 
@@ -125,7 +125,7 @@ class Sensor {
             this[name] = input.children[1].value;
         };
 
-        if (this.type == "pir" || this.type == "si7021") {
+        if (this._type == "pir" || this._type == "si7021") {
             this['default_rule'] = parseFloat(document.getElementById(this.id + "-default_rule").value);
         };
     };

frontend/node_configuration/tests.py
@@ -572,7 +572,7 @@ class EditConfigTests(TestCase):
         # Confirm all devices and sensors present
         self.assertContains(response, '<input type="text" class="form-control sensor1 nickname" id="sensor1-nickname" placeholder="" value="Motion Sensor (Bath)"')
         self.assertContains(response, '<input type="text" class="form-control sensor2 nickname" id="sensor2-nickname" placeholder="" value="Motion Sensor (Entry)"')
-        self.assertContains(response, '<input type="text" class="form-control device1 pwm-limits" id="device1-max" placeholder="1023" value="1023" required>')
+        self.assertContains(response, '<input type="text" class="form-control device1 pwm-limits" id="device1-max_bright" placeholder="1023" value="1023" required>')
         self.assertContains(response, '<input type="text" class="form-control device2 ip-input" id="device2-ip" placeholder="" value="192.168.1.239"')
         self.assertContains(response, '<input type="text" class="form-control device3 nickname" id="device3-nickname" placeholder="" value="Entry Light" onchange="update_nickname(this)" oninput="prevent_duplicate_nickname(event)" required>')
 
@@ -2105,14 +2105,14 @@ class ValidateConfigTests(TestCase):
         self.assertEqual(result, 'Invalid pin (non-integer)')
 
     def test_invalid_device_type(self):
-        self.valid_config['device1']['type'] = 'nuclear'
+        self.valid_config['device1']['_type'] = 'nuclear'
         result = validate_full_config(self.valid_config)
-        self.assertEqual(result, f'Invalid device type {self.valid_config["device1"]["type"]} used')
+        self.assertEqual(result, f'Invalid device type {self.valid_config["device1"]["_type"]} used')
 
     def test_invalid_sensor_type(self):
-        self.valid_config['sensor1']['type'] = 'ozone-sensor'
+        self.valid_config['sensor1']['_type'] = 'ozone-sensor'
         result = validate_full_config(self.valid_config)
-        self.assertEqual(result, f'Invalid sensor type {self.valid_config["sensor1"]["type"]} used')
+        self.assertEqual(result, f'Invalid sensor type {self.valid_config["sensor1"]["_type"]} used')
 
     def test_invalid_ip(self):
         self.valid_config['device1']['ip'] = '192.168.1.500'
@@ -2130,40 +2130,40 @@ class ValidateConfigTests(TestCase):
         self.assertEqual(result, 'Thermostat tolerance must be int or float')
 
     def test_pwm_min_greater_than_max(self):
-        self.valid_config['device6']['min'] = 1023
-        self.valid_config['device6']['max'] = 500
+        self.valid_config['device6']['min_bright'] = 1023
+        self.valid_config['device6']['max_bright'] = 500
         self.valid_config['device6']['default_rule'] = 700
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'PWM min cannot be greater than max')
 
     def test_pwm_limits_negative(self):
-        self.valid_config['device6']['min'] = -50
-        self.valid_config['device6']['max'] = -5
+        self.valid_config['device6']['min_bright'] = -50
+        self.valid_config['device6']['max_bright'] = -5
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'PWM limits cannot be less than 0')
 
     def test_pwm_limits_over_max(self):
-        self.valid_config['device6']['min'] = 1023
-        self.valid_config['device6']['max'] = 4096
+        self.valid_config['device6']['min_bright'] = 1023
+        self.valid_config['device6']['max_bright'] = 4096
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'PWM limits cannot be greater than 1023')
 
     def test_pwm_invalid_default_rule(self):
-        self.valid_config['device6']['min'] = 500
-        self.valid_config['device6']['max'] = 1000
+        self.valid_config['device6']['min_bright'] = 500
+        self.valid_config['device6']['max_bright'] = 1000
         self.valid_config['device6']['default_rule'] = 1100
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'Cabinet Lights: Invalid default rule 1100')
 
     def test_pwm_invalid_schedule_rule(self):
-        self.valid_config['device6']['min'] = 500
-        self.valid_config['device6']['max'] = 1000
+        self.valid_config['device6']['min_bright'] = 500
+        self.valid_config['device6']['max_bright'] = 1000
         self.valid_config['device6']['schedule']['01:00'] = 1023
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'Cabinet Lights: Invalid schedule rule 1023')
 
     def test_pwm_noninteger_limit(self):
-        self.valid_config['device6']['min'] = 'off'
+        self.valid_config['device6']['min_bright'] = 'off'
         result = validate_full_config(self.valid_config)
         self.assertEqual(result, 'Invalid PWM limits, both must be int between 0 and 1023')
 
@@ -2280,7 +2280,7 @@ class ValidatorErrorTests(TestCase):
     def test_invalid_type(self):
         # Verify error when type is unsupported
         invalid = self.config['device1']
-        invalid['type'] = 'foobar'
+        invalid['_type'] = 'foobar'
         self.assertEqual(validate_rules(invalid), 'Invalid type foobar')
 
     def test_invalid_rule_no_special_validator(self):

commit f4bc5fad85b1cf61e55c1d6fde499c770b38791d

Bug fixes
Schedule rule edit buttons no longer fail to open modal (broken since 573949de, still looking for type after change to _type)

Newly-added devices and sensors no longer missing type on page 2, page 3

frontend/node_configuration/static/node_configuration/page-buttons.js
@@ -27,17 +27,17 @@ document.getElementById('page1-button').addEventListener("click", function(e) {
     for (device in instances['devices']) {
         // Get device nickname and type
         const nickname = instances['devices'][device]['output']['nickname'];
-        const type = instances['devices'][device]['output']['type'];
+        const type = instances['devices'][device]['output']['_type'];
 
         // If device is new, add target select options on page2, add schedule rules card on page3
         if (instances['devices'][device].new) {
             // Skip IrBlaster (can't be targeted, doesn't support schedule rules)
-            if (instances['devices'][device]['output']['type'] == 'ir-blaster') { continue };
+            if (instances['devices'][device]['output']['_type'] == 'ir-blaster') { continue };
             handle_new_device(device, nickname, type);
 
         // If device nickname changed, but type did not change (targets + rules don't need to be cleared)
         } else if (instances['devices'][device].name_changed && ! instances['devices'][device].modified) {
-            rename_device(device, nickname);
+            rename_device(device, nickname, type);
 
         // If device type changed, change type displayed on page2 and page3
         } else if (instances['devices'][device].modified) {
@@ -49,7 +49,7 @@ document.getElementById('page1-button').addEventListener("click", function(e) {
     for (sensor in instances['sensors']) {
         // Get sensor nickname and type
         const nickname = instances['sensors'][sensor]['output']['nickname'];
-        const type = instances['sensors'][sensor]['output']['type'];
+        const type = instances['sensors'][sensor]['output']['_type'];
 
         // If sensor is new, add target select card to page2
         if (instances['sensors'][sensor].new) {
@@ -57,7 +57,7 @@ document.getElementById('page1-button').addEventListener("click", function(e) {
 
         // If sensor nickname changed, but type did not change (targets + rules don't need to be cleared)
         } else if (instances['sensors'][sensor].name_changed && ! instances['sensors'][sensor].modified) {
-            rename_sensor(sensor, nickname);
+            rename_sensor(sensor, nickname, type);
 
         // If sensor type changed, change type displayed on page2 and page3
         } else if (instances['sensors'][sensor].modified) {
@@ -98,9 +98,9 @@ function handle_new_device(device, nickname, type) {
 
 // Takes device ID and nickname
 // Changes nickname on target select options, schedule rules card
-function rename_device(device, nickname) {
+function rename_device(device, nickname, type) {
     // Change name on schedule rules card
-    document.getElementById(`${device}-rules-label`).innerHTML = `<b>${nickname}</b>`;
+    document.getElementById(`${device}-rules-label`).innerHTML = `<b>${nickname} (${type})</b>`;
 
     // Change text on all target options
     target_labels = document.getElementsByClassName(`${device} target-label`);
@@ -138,7 +138,7 @@ function change_device_type(device, nickname, type) {
 
     // Change name and tooltip on schedule rules card
     document.getElementById(`${device}-rules-label`).title = `${device} - ${type}`;
-    document.getElementById(`${device}-rules-label`).innerHTML = `<b>${nickname}</b>`;
+    document.getElementById(`${device}-rules-label`).innerHTML = `<b>${nickname} (${type})</b>`;
 
     // Clear existing schedule rules (likely invalid after type change)
     template = `<tr>
@@ -159,13 +159,13 @@ function handle_new_sensor(sensor, nickname, type) {
     // Target select card opening div
     var template =  `<div class='card ${sensor}'>
                          <div class='card-body'>
-                             <label id='${sensor}-targets-label' for='${sensor}-targets' class='card-title sensor-targets-label ${sensor}'><b>${nickname}</b> targets:</label>
+                             <label id='${sensor}-targets-label' for='${sensor}-targets' class='card-title sensor-targets-label ${sensor}'><b>${nickname} (${type})</b> targets:</label>
                              <div id='${sensor}-targets' class='form-check sensor-targets ${sensor}'>`
 
     // Iterate devices, add checkbox for each to new sensor card
     for (device in instances['devices']) {
         // Do not add if device is IrBlaster (cannot be targeted)
-        if (instances['devices'][device]['output']['type'] == "ir-blaster") { continue };
+        if (instances['devices'][device]['output']['_type'] == "ir-blaster") { continue };
 
         template += `<input type='checkbox' class='form-check-input ${device} ${sensor} target' id='target-${sensor}-${device}' value='target-${sensor}-${device}'>
                      <label for='target-${sensor}-${device}' class='form-check-label ${device} ${sensor} target-label'>${instances['devices'][device]['output']['nickname']}</label><br>`;
@@ -186,12 +186,12 @@ function handle_new_sensor(sensor, nickname, type) {
 
 // Takes sensor ID and nickname
 // Changes nickname on target select card, schedule rules card
-function rename_sensor(sensor, nickname) {
+function rename_sensor(sensor, nickname, type) {
     // Change name on schedule rules card
-    document.getElementById(`${sensor}-rules-label`).innerHTML = `<b>${nickname}</b>`;
+    document.getElementById(`${sensor}-rules-label`).innerHTML = `<b>${nickname} (${type})</b>`;
 
     // Change name on targets card
-    document.getElementById(`${sensor}-targets-label`).innerHTML = `<b>${nickname}</b> targets:`;
+    document.getElementById(`${sensor}-targets-label`).innerHTML = `<b>${nickname} (${type})</b> targets:`;
 
     instances['sensors'][sensor].name_changed = false;
 };
@@ -209,11 +209,11 @@ function change_sensor_type(sensor, nickname, type) {
     };
 
     // Change nickname and type on target card
-    document.getElementById(`${sensor}-targets-label`).innerHTML = `<b>${nickname}</b> targets:`;
+    document.getElementById(`${sensor}-targets-label`).innerHTML = `<b>${nickname} (${type})</b> targets:`;
 
     // Change name and tooltip on schedule rules card
     document.getElementById(`${sensor}-rules-label`).title = `${sensor} - ${type}`;
-    document.getElementById(`${sensor}-rules-label`).innerHTML = `<b>${nickname}</b>`;
+    document.getElementById(`${sensor}-rules-label`).innerHTML = `<b>${nickname} (${type})</b>`;
 
     // Clear existing schedule rules (likely invalid after type change)
     template = `<tr>
@@ -234,7 +234,7 @@ function create_schedule_rule_section(id, nickname, type) {
     return `<div class='card mb-4 ${id}'>
                 <div class='card-body text-center'>
                     <label id='${id}-rules-label' class='card-title schedule-rule-card ${id}' title='${id} - ${type}'>
-                        <b>${nickname}</b>
+                        <b>${nickname} (${type})</b>
                     </label>
                     <table id='${id}-rules' class='table table-borderless ${id} d-none'>
                         <tr>

frontend/node_configuration/templates/node_configuration/edit-config.html
@@ -764,7 +764,7 @@
                                     {% endif %}
 
                                     <td class="min">
-                                        <button type="button" class="btn btn-sm btn-primary mt-1" id="{{device}}-edit{{forloop.counter}}" data-type="{{params.type}}" onclick="edit_existing_rule(this);"><i class="bi-pencil"></i></button>
+                                        <button type="button" class="btn btn-sm btn-primary mt-1" id="{{instance}}-edit{{forloop.counter}}" data-type="{{params.type}}" onclick="edit_existing_rule(this);"><i class="bi-pencil"></i></button>
                                     </td>
                                 </tr>
 

frontend/webapp/serviceworker.js
@@ -1,5 +1,5 @@
 
-var staticCacheName = "django-pwa-vbe49e0a";
+var staticCacheName = "django-pwa-v812516c";
 var filesToCache = [
     '/offline/',
     '/static/css/django-pwa-app.css',