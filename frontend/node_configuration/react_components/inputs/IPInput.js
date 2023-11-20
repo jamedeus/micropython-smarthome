625e9aaee538f57f9b6a2d52f8467a9a25ce02b5
Josh Medeiros <josh@jamedeus.com>
2023-11-19 22:33:02 -0800

    Format IP address as user types in field

--- frontend/node_configuration/react_components/inputs/IPInput.js
+++ frontend/node_configuration/react_components/inputs/IPInput.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Form from 'react-bootstrap/Form';
 import { ConfigContext } from './../ConfigContext';
 import InputWrapper from './InputWrapper';
 
@@ -9,16 +10,60 @@ function IPInput({ key, id }) {
     // Get instance section in config
     const instance = config[id];
 
+    // Format IP address as user types in field
+    const formatIp = (value) => {
+        // Backspace and delete bypass formatting
+        if (value.length < instance.ip.length) {
+            handleInputChange(id, "ip", value);
+            return;
+        };
+
+        // Remove everything except digits and period, 15 char max
+        const input = value.replace(/[^\d.]/g, '').substring(0, 15);
+        let output = '';
+        let block = '';
+
+        // Iterate input and format character by character
+        for (let i = 0; i < input.length; i++) {
+            const char = input[i];
+
+            // Delimiter character handling
+            if (char === '.') {
+                // Drop if first char is delim, otherwise add to end of current block + start new block
+                if (block.length > 0) {
+                    output += block + '.';
+                    block = '';
+                }
+
+            // Numeric character handling
+            } else {
+                // Add to current block
+                block += char;
+                // If current block reached limit, add to output + start new block
+                if (block.length === 3) {
+                    output += block + '.';
+                    block = '';
+                }
+            }
+        }
+
+        // Add final block
+        output += block;
+
+        // Prevent >4 blocks (char limit may not be reached if single-digit blocks present)
+        output = output.split('.').slice(0, 4).join('.');
+
+        // Update state object
+        handleInputChange(id, "ip", output);
+    };
+
     return (
         <InputWrapper label="IP">
-            <input
+            <Form.Control
                 type="text"
-                className="form-control ip-input validate"
-                placeholder=""
                 value={instance.ip}
                 pattern="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
-                onChange={(e) => handleInputChange(id, "ip", e.target.value)}
-                required
+                onChange={(e) => formatIp(e.target.value)}
             />
         </InputWrapper>
     );
