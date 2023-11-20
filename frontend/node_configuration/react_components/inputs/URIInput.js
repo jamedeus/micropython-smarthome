644930fff69aed134cc2df37cd815f540f29b2ea
Josh Medeiros <josh@jamedeus.com>
2023-11-20 13:17:46 -0800

    Add validator for URI input

--- frontend/node_configuration/react_components/inputs/URIInput.js
+++ frontend/node_configuration/react_components/inputs/URIInput.js
@@ -1,23 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import { ConfigContext } from './../ConfigContext';
 import InputWrapper from './InputWrapper';
 
 function URIInput({ key, id }) {
+    const input = useRef(null);
+
     // Get curent state + callback functions from context
     const { config, handleInputChange } = useContext(ConfigContext);
 
     // Get instance section in config
     const instance = config[id];
 
+    // Validate when focus leaves, add highlight if invalid
+    useEffect(() => {
+        input.current.addEventListener("blur", () => {
+            if (!input.current.validity.valid) {
+                input.current.classList.add('is-invalid');
+            };
+        });
+    }, []);
+
+    // Remove invalid highlight immediately when contents become valid
+    const validate = (event) => {
+        if (event.target.validity.valid) {
+            event.target.classList.remove('is-invalid');
+        }
+        handleInputChange(id, "uri", event.target.value);
+    }
+
     return (
         <InputWrapper label="URI">
             <Form.Control
+                ref={input}
                 type="text"
                 placeholder="IP address or URL"
                 value={instance.uri}
                 pattern="(?:(?:http|https):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3})(?::\d{1,5})?|(?:(?:http|https):\/\/)?[a-zA-Z0-9.]+(?:-[a-zA-Z0-9]+)*(?:\.[a-zA-Z]{2,6})+(?:\/\S*)?"
-                onChange={(e) => handleInputChange(id, "uri", e.target.value)}
+                onChange={(e) => validate(e)}
             />
         </InputWrapper>
     );
