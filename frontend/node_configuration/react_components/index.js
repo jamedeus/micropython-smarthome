d406a53dbaedb48b2729a2f856a8778ac2c12034
Josh Medeiros <josh@jamedeus.com>
2023-11-28 22:48:17 -0800

    Replace deprecated ReactDOM.render with createRoot
    Upgrades to react 18

--- frontend/node_configuration/react_components/index.js
+++ frontend/node_configuration/react_components/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ConfigProvider } from './ConfigContext';
 
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <ConfigProvider>
         <App />
-    </ConfigProvider>,
-    document.getElementById('root')
+    </ConfigProvider>
 );
