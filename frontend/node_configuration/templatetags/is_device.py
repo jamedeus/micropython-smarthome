76770b6f080f7e89714c2263e242a7e65590cab6
Josh Medeiros <josh@jamedeus.com>
2023-11-29 22:46:39 -0800

    Remove templatetags (no longer used)

--- frontend/node_configuration/templatetags/is_device.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='is_device')
-def is_device(arg):
-    '''Returns True if arg starts with "device", otherwise False'''
-    return arg.startswith('device')
--- frontend/node_configuration/templatetags/is_sensor.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='is_sensor')
-def is_sensor(arg):
-    '''Returns True if arg starts with "sensor", otherwise False'''
-    return arg.startswith('sensor')
