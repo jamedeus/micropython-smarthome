2d66713d683fd01ca43ad776f6ec86a953049b25
Josh Medeiros <josh@jamedeus.com>
2023-12-02 20:15:18 -0800

    Add API frontend button, only render tables if configs/nodes exist

--- frontend/node_configuration/react_components/pages/overview/Overview.js
+++ frontend/node_configuration/react_components/pages/overview/Overview.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
+import { OverviewContext } from 'root/OverviewContext';
 import Header from './Header';
 import KeywordsTable from './KeywordsTable';
 import NewConfigTable from './NewConfigTable';
@@ -9,9 +10,15 @@ import { UploadModalContextProvider, UploadModal } from 'modals/UploadModal';
 
 
 const App = () => {
+    // Get django context
+    const { context } = useContext(OverviewContext);
 
     const new_config = () => {
         window.location.href = "new_config";
+    };
+
+    const frontend = () => {
+        window.location.href = "/api";
     };
 
     return (
@@ -19,14 +26,39 @@ const App = () => {
             <ErrorModalContextProvider>
                 <div className="d-flex flex-column vh-100">
                     <Header />
-                    <NewConfigTable />
-                    <ExistingNodesTable />
+
+                    {/* Add new config table if un-uploaded configs exist */}
+                    {(() => {
+                        if (context.not_uploaded.length) {
+                            return <NewConfigTable />;
+                        }
+                    })()}
+
+                    {/* Add existing nodes table if existing nodes exist */}
+                    {(() => {
+                        if (context.uploaded.length) {
+                            return <ExistingNodesTable />;
+                        }
+                    })()}
+
+                    {/* Button to create new config file */}
                     <div className="mt-2 mb-5 text-center">
                         <Button variant="primary" onClick={new_config}>
                             Create new config
                         </Button>
                     </div>
+
+                    {/* Add schedule keywords table */}
                     <KeywordsTable />
+
+                    {/* Button redirects to API frontend */}
+                    <div className="d-flex align-items-center flex-column mt-auto py-4">
+                        <Button variant="secondary" onClick={frontend}>
+                            Frontend
+                        </Button>
+                    </div>
+
+                    {/* Modals (hidden) */}
                     <UploadModal />
                     <ErrorModal />
                 </div>
