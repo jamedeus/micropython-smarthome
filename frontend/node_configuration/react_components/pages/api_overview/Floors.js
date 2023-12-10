81ea5e27a4836f4cf8ab276495df18bced9f8a65
Josh Medeiros <josh@jamedeus.com>
2023-12-09 18:58:58 -0800

    Change labels to "Upstairs", "Downstairs" when exactly 2 floors

--- frontend/node_configuration/react_components/pages/api_overview/Floors.js
+++ frontend/node_configuration/react_components/pages/api_overview/Floors.js
@@ -21,38 +21,61 @@ const Floors = () => {
     const NodeButton = ({ friendlyName }) => {
         return (
             <Button variant="primary" className="m-1" onClick={() => open(friendlyName)}>
-            {friendlyName}
+                {friendlyName}
             </Button>
         );
     };
 
-    // Takes floor number and array of friendly names on floor
+    // Takes section label and array of friendly names on floor
     // Returns section with button for each friendly name
-    const FloorSection = ({ floor, nodes }) => {
+    const FloorSection = ({ label, nodes }) => {
         return (
             <div className="section mt-3 mb-4 p-3">
-            <h5 className="mb-3"><b>Floor {floor}</b></h5>
+                <h5 className="mb-3"><b>{label}</b></h5>
 
-            {nodes.map((node) => {
-                return <NodeButton key={node} friendlyName={node} />;
-            })}
+                {nodes.map((node) => {
+                    return <NodeButton key={node} friendlyName={node} />;
+                })}
             </div>
         );
     };
 
-    return (
-        <div id="container" className="text-center">
-            {Object.keys(context.nodes).map((floor) => {
-                return (
+    // Get array of floor numbers (used to determine layout)
+    const floorNumbers = Object.keys(context.nodes);
+
+    // Exactly 2 floors: Label sections "Upstairs" and "Downstairs", switch order
+    // Default: Sequential "Floor #" labels, lowest to highest
+    switch(floorNumbers.length) {
+        case(2):
+            return (
+                <>
                     <FloorSection
-                        key={context.nodes[floor]}
-                        floor={floor}
-                        nodes={context.nodes[floor]}
+                        key={floorNumbers[1]}
+                        label="Upstairs"
+                        nodes={context.nodes[floorNumbers[1]]}
                     />
-                );
-            })}
-        </div>
-    );
+                    <FloorSection
+                        key={floorNumbers[0]}
+                        label="Downstairs"
+                        nodes={context.nodes[floorNumbers[0]]}
+                    />
+                </>
+            )
+        default:
+            return (
+                <>
+                    {floorNumbers.map((floor) => {
+                        return (
+                            <FloorSection
+                                key={context.nodes[floor]}
+                                label={`Floor ${floor}`}
+                                nodes={context.nodes[floor]}
+                            />
+                        );
+                    })}
+                </>
+            )
+    };
 };
 
 
