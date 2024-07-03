ae95e9d1313531f8d2220c6a4dc3706e5b6e65d7
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-07-02 22:03:38 -0700

    Move to css
    ????????

--- frontend/src/pages/api_overview/ApiOverview.js
+++ frontend/src/pages/api_overview/ApiOverview.js
@@ -7,21 +7,7 @@ import Header from './Header';
 import Floors from './Floors';
 import Macros from './Macros';
 import { LoadingSpinner } from 'util/animations';
-
-
-// CSS for full-screen overlay (background behind loading spinner)
-// Shown while waiting for status object after user clicks node button
-const overlayStyle = `
-#loading_overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 999;
-}`;
-
+import 'css/loadingOverlay.css';
 
 const App = () => {
     // Get state bool for loading overlay
@@ -52,13 +38,11 @@ const App = () => {
             </div>
 
             {/* Loading overlay (hidden) */}
-            <style>{overlayStyle}</style>
             <div id="loading_overlay" className={loading ? "d-flex" : "d-none"}>
                 <LoadingSpinner size="large" />
             </div>
         </>
     );
 };
-
 
 export default App;
