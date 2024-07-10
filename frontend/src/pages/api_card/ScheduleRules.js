4090e6a323e90cc424975257c5103d5485f85764
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-07-09 19:04:34 -0700

    Fix schedule rules re-rendering on each status update
    Schedule rules now useable, no longer closes popup and resets selection every 5 seconds. Surprisingly didn't need to set editing bool.
    
    Not entirely sure why moving row component out of table component fixed it, but I think it doesn't need to re-render because params didn't change (user changes are in local state).

--- frontend/src/pages/api_card/ScheduleRules.js
+++ frontend/src/pages/api_card/ScheduleRules.js
@@ -8,156 +8,168 @@ import DeleteOrEditButton from 'inputs/DeleteOrEditButton';
 import ApiTargetRuleButton from 'inputs/ApiTargetRuleButton';
 import Button from 'react-bootstrap/Button';
 
-const ScheduleRulesTable = ({ id, instance }) => {
-    const [showNewRule, setShowNewRule] = useState(false);
+// Rendered for each existing rule, pass existingRule=false for new rule row
+const ScheduleRuleRow = ({
+    id,
+    instance,
+    originalTime='',
+    originalRule='',
+    existingRule=true,
+    showNewRule,
+    setShowNewRule
+}) => {
+    // Get API call hooks
+    const {
+        status,
+        add_schedule_rule,
+        edit_schedule_rule,
+        delete_schedule_rule,
+        apiTargetOptions
+    } = useContext(ApiCardContext);
 
-    // Rendered for each existing rule, pass existingRule=false for new rule row
-    const ScheduleRuleRow = ({ originalTime='', originalRule='', existingRule=true }) => {
-        // Get API call hooks
-        const {
-            status,
-            add_schedule_rule,
-            edit_schedule_rule,
-            delete_schedule_rule,
-            apiTargetOptions
-        } = useContext(ApiCardContext);
+    // Create states to store modified timestamp and rule
+    const [newTime, setNewTime] = useState(originalTime);
+    const [newRule, setNewRule] = useState(originalRule);
 
-        // Create states to store modified timestamp and rule
-        const [newTime, setNewTime] = useState(originalTime);
-        const [newRule, setNewRule] = useState(originalRule);
+    // Create state for DeleteOrEditButton (values: delete, edit, loading)
+    const [loadingButtonState, setLoadingButtonState] = useState('delete');
 
-        // Create state for DeleteOrEditButton (values: delete, edit, loading)
-        const [loadingButtonState, setLoadingButtonState] = useState('delete');
-
-        // Called after input changes, sets appropriate button state
-        const updateLoadingButton = (newTimestamp, newRuleValue) => {
-            // Existing rules: Show edit button when either input changed
-            if (existingRule) {
-                if (newTimestamp != originalTime || newRuleValue != originalRule) {
-                    setLoadingButtonState('edit');
-                } else {
-                    setLoadingButtonState('delete');
-                }
-            // New rule: Show edit button when both inputs have value
+    // Called after input changes, sets appropriate button state
+    const updateLoadingButton = (newTimestamp, newRuleValue) => {
+        // Existing rules: Show edit button when either input changed
+        if (existingRule) {
+            if (newTimestamp != originalTime || newRuleValue != originalRule) {
+                setLoadingButtonState('edit');
             } else {
-                if (newTimestamp && newRuleValue) {
-                    setLoadingButtonState('edit');
-                } else {
-                    setLoadingButtonState('delete');
-                }
-            }
-        };
-
-        // Called by TimeField when user changes value and closes
-        const handleNewTimestamp = (newTimestamp, _) => {
-            setNewTime(newTimestamp);
-            updateLoadingButton(newTimestamp, newRule);
-        };
-
-        // Called by RuleField when user changes value and closes
-        const handleNewRule = (newRuleValue, fade_rule, duration, range_rule) => {
-            // Fade rule: Combine params into single string
-            if (range_rule && fade_rule) {
-                newRuleValue = `fade/${newRuleValue}/${duration}`;
-            }
-            setNewRule(newRuleValue);
-            updateLoadingButton(newTime, newRuleValue);
-        };
-
-        // New rule field add button handler
-        const addRule = async () => {
-            // Start loading animation, post new rule to backend
-            setLoadingButtonState('loading');
-            const result = await add_schedule_rule(id, newTime, newRule);
-
-            // If successful hide new rule field and reset inputs
-            if (result) {
-                setShowNewRule(false);
                 setLoadingButtonState('delete');
-                setNewTime('');
-                setNewRule('');
             }
-        };
+        // New rule: Show edit button when both inputs have value
+        } else {
+            if (newTimestamp && newRuleValue) {
+                setLoadingButtonState('edit');
+            } else {
+                setLoadingButtonState('delete');
+            }
+        }
+    };
 
-        // New rule field delete button handler
-        const hideNewRule = async () => {
-            // Hide new rule field, reset inputs
+    // Called by TimeField when user changes value and closes
+    const handleNewTimestamp = (newTimestamp, _) => {
+        setNewTime(newTimestamp);
+        updateLoadingButton(newTimestamp, newRule);
+    };
+
+    // Called by RuleField when user changes value and closes
+    const handleNewRule = (newRuleValue, fade_rule, duration, range_rule) => {
+        // Fade rule: Combine params into single string
+        if (range_rule && fade_rule) {
+            newRuleValue = `fade/${newRuleValue}/${duration}`;
+        }
+        setNewRule(newRuleValue);
+        updateLoadingButton(newTime, newRuleValue);
+    };
+
+    // New rule field add button handler
+    const addRule = async () => {
+        // Start loading animation, post new rule to backend
+        setLoadingButtonState('loading');
+        const result = await add_schedule_rule(id, newTime, newRule);
+
+        // If successful hide new rule field and reset inputs
+        if (result) {
             setShowNewRule(false);
+            setLoadingButtonState('delete');
             setNewTime('');
             setNewRule('');
-        };
+        }
+    };
 
-        // Existing rule field add button handler
-        const editRule = () => {
-            setLoadingButtonState('loading');
-            edit_schedule_rule(id, originalTime, newTime, newRule);
-        };
+    // New rule field delete button handler
+    const hideNewRule = async () => {
+        // Hide new rule field, reset inputs
+        setShowNewRule(false);
+        setNewTime('');
+        setNewRule('');
+    };
 
-        // Existing rule field delete button handler
-        const deleteRule = () => {
-            setLoadingButtonState('loading');
-            delete_schedule_rule(id, originalTime);
-        };
+    // Existing rule field add button handler
+    const editRule = () => {
+        setLoadingButtonState('loading');
+        edit_schedule_rule(id, originalTime, newTime, newRule);
+    };
 
-        // Renders button that opens ApiTarget rule modal
-        const ApiTargetRuleField = () => {
-            // Receives stringified dropdown selection when modal submitted
-            const handleSubmit = (newRule) => {
-                handleNewRule(newRule, false, '', false);
-            };
+    // Existing rule field delete button handler
+    const deleteRule = () => {
+        setLoadingButtonState('loading');
+        delete_schedule_rule(id, originalTime);
+    };
 
-            return (
-                <ApiTargetRuleButton
-                    currentRule={newRule ? newRule : ''}
-                    targetNodeOptions={apiTargetOptions[id]}
-                    handleSubmit={handleSubmit}
-                />
-            );
+    // Renders button that opens ApiTarget rule modal
+    const ApiTargetRuleField = () => {
+        // Receives stringified dropdown selection when modal submitted
+        const handleSubmit = (newRule) => {
+            handleNewRule(newRule, false, '', false);
         };
 
         return (
-            <tr className={!existingRule && !showNewRule ? 'd-none' : ''}>
-                <td>
-                    <TimeField
-                        timestamp={newTime}
-                        handleChange={handleNewTimestamp}
-                        schedule_keywords={status.metadata.schedule_keywords}
-                        highlightInvalid={false}
-                    />
-                </td>
-                <td>
-                    {instance.type === 'api-target' ? (
-                        <ApiTargetRuleField />
-                    ) : (
-                        <RuleField
-                            instance={instance}
-                            category={id.replace(/[0-9]/g, '')}
-                            type={instance.type}
-                            rule={newRule}
-                            handleChange={handleNewRule}
-                        />
-                    )}
-                </td>
-                <td className="min">
-                    <DeleteOrEditButton
-                        status={loadingButtonState}
-                        handleDelete={existingRule ? deleteRule : hideNewRule}
-                        handleEdit={existingRule ? editRule : addRule}
-                    />
-                </td>
-            </tr>
+            <ApiTargetRuleButton
+                currentRule={newRule ? newRule : ''}
+                targetNodeOptions={apiTargetOptions[id]}
+                handleSubmit={handleSubmit}
+            />
         );
     };
 
-    ScheduleRuleRow.propTypes = {
-        originalTime: PropTypes.string,
-        originalRule: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-            PropTypes.object,
-        ]),
-        existingRule: PropTypes.bool
-    };
+    return (
+        <tr className={!existingRule && !showNewRule ? 'd-none' : ''}>
+            <td>
+                <TimeField
+                    timestamp={newTime}
+                    handleChange={handleNewTimestamp}
+                    schedule_keywords={status.metadata.schedule_keywords}
+                    highlightInvalid={false}
+                />
+            </td>
+            <td>
+                {instance.type === 'api-target' ? (
+                    <ApiTargetRuleField />
+                ) : (
+                    <RuleField
+                        instance={instance}
+                        category={id.replace(/[0-9]/g, '')}
+                        type={instance.type}
+                        rule={newRule}
+                        handleChange={handleNewRule}
+                    />
+                )}
+            </td>
+            <td className="min">
+                <DeleteOrEditButton
+                    status={loadingButtonState}
+                    handleDelete={existingRule ? deleteRule : hideNewRule}
+                    handleEdit={existingRule ? editRule : addRule}
+                />
+            </td>
+        </tr>
+    );
+};
+
+ScheduleRuleRow.propTypes = {
+    id: PropTypes.string.isRequired,
+    instance: PropTypes.object.isRequired,
+    originalTime: PropTypes.string,
+    originalRule: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.object,
+    ]),
+    existingRule: PropTypes.bool,
+    showNewRule: PropTypes.bool.isRequired,
+    setShowNewRule: PropTypes.func.isRequired
+};
+
+const ScheduleRulesTable = ({ id, instance }) => {
+    const [showNewRule, setShowNewRule] = useState(false);
 
     return (
         <div className="collapse text-center" id={`${id}-schedule-rules`}>
@@ -173,12 +185,23 @@ const ScheduleRulesTable = ({ id, instance }) => {
                         return (
                             <ScheduleRuleRow
                                 key={time}
+                                id={id}
+                                instance={instance}
                                 originalTime={time}
                                 originalRule={rule}
+                                showNewRule={showNewRule}
+                                setShowNewRule={setShowNewRule}
                             />
                         );
                     })}
-                    <ScheduleRuleRow key={'new'} existingRule={false} />
+                    <ScheduleRuleRow
+                        key={'new'}
+                        id={id}
+                        instance={instance}
+                        existingRule={false}
+                        showNewRule={showNewRule}
+                        setShowNewRule={setShowNewRule}
+                    />
                 </tbody>
             </Table>
 
