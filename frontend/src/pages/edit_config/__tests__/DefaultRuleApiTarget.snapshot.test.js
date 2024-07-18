e60edc02f5c13e4151aa364d3d638bf4faa6d201
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-07-18 16:41:48 -0700

    Self-target coverage

--- frontend/src/pages/edit_config/__tests__/DefaultRuleApiTarget.snapshot.test.js
+++ frontend/src/pages/edit_config/__tests__/DefaultRuleApiTarget.snapshot.test.js
@@ -7,24 +7,30 @@ import { existingConfigContext, apiTargetOptionsContext } from './mockContext';
 import { edit_config_metadata } from 'src/testUtils/mockMetadataContext';
 
 describe('App', () => {
-    it('matches snapshot when existing rule targets IR Blaster', async () => {
+    let app, user;
+
+    beforeAll(() => {
         // Create mock state objects
         createMockContext('config', existingConfigContext.config);
         createMockContext('api_target_options', apiTargetOptionsContext);
         createMockContext('instance_metadata', edit_config_metadata);
         createMockContext('edit_existing', existingConfigContext.edit_existing);
         createMockContext('target_node_ip', existingConfigContext.IP);
+    });
 
+    beforeEach(async () => {
         // Create user, render EditConfig
-        const user = userEvent.setup();
-        const app = render(
+        user = userEvent.setup();
+        app = render(
             <MetadataContextProvider>
                 <ConfigProvider>
                     <EditConfig />
                 </ConfigProvider>
             </MetadataContextProvider>
         );
+    });
 
+    it('matches snapshot when existing rule targets IR Blaster', async () => {
         // Click button to open ApiTargetRuleModal
         await user.click(app.getByRole('button', { name: 'Set rule' }));
 
@@ -34,22 +40,26 @@ describe('App', () => {
     });
 
     it('matches snapshot when existing rule targets device', async () => {
-        // Create mock state objects
-        createMockContext('config', existingConfigContext.config);
-        createMockContext('api_target_options', apiTargetOptionsContext);
-        createMockContext('instance_metadata', edit_config_metadata);
-        createMockContext('edit_existing', existingConfigContext.edit_existing);
-        createMockContext('target_node_ip', existingConfigContext.IP);
+        // Click button to open ApiTargetRuleModal
+        await user.click(app.getByRole('button', { name: 'Set rule' }));
 
-        // Create user, render EditConfig
-        const user = userEvent.setup();
-        const app = render(
-            <MetadataContextProvider>
-                <ConfigProvider>
-                    <EditConfig />
-                </ConfigProvider>
-            </MetadataContextProvider>
-        );
+        // Get modal, change target instance to device1, change action to turn_on
+        const modal = app.getByText('API Target Rule').parentElement.parentElement;
+        await user.selectOptions(within(modal).getAllByRole('combobox')[0], 'device1');
+        await user.selectOptions(within(modal).getAllByRole('combobox')[1], 'turn_on');
+
+        // Change Off action target instance to device1, change action to turn_off
+        await user.click(app.getByText('Off Action'));
+        await user.selectOptions(within(modal).getAllByRole('combobox')[0], 'device1');
+        await user.selectOptions(within(modal).getAllByRole('combobox')[1], 'turn_off');
+
+        // Confirm correct options were rendered
+        expect(modal).toMatchSnapshot();
+    });
+
+    it('matches snapshot when self targetting', async () => {
+        // Change target node to self
+        await user.selectOptions(app.getByLabelText('Target Node:'), '127.0.0.1')
 
         // Click button to open ApiTargetRuleModal
         await user.click(app.getByRole('button', { name: 'Set rule' }));
