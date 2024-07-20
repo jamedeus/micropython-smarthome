jamedeus/micropython-smarthome

commit b418091673f009e894a7d0c16ea744b70481563a

Only debounce set_rule for sliders
Function also used by ChangeApiTargetRule.js and FadeModal which only send 1 request, no point to debounce

frontend/src/ApiCardContext.js
@@ -199,15 +199,19 @@ export const ApiCardContextProvider = ({ children }) => {
         }
     };
 
-    // Handler for rule sliders and buttons, updates local state immediately, calls
-    // debounced function to send API call to node when user stops moving slider
-    const set_rule = (id, value) => {
-        update_instance(id, {current_rule: value});
-        debounced_set_rule(id, value);
+    const set_rule = async (id, rule) => {
+        update_instance(id, {current_rule: rule});
+        const payload = {
+            command: 'set_rule',
+            instance: id,
+            rule: rule
+        };
+        const result = await send_command(payload);
+        console.log(result);
     };
 
-    // Called by set_rule, sends API call to node once user stops moving slider for
-    // 150ms (prevents constant API calls saturating ESP32 bandwidth)
+    // Handler for rule sliders and buttons, updates local state immediately, makes
+    // API call after 150ms delay (prevents contant calls saturating ESP32 bandwidth)
     const debounced_set_rule = useCallback(debounce(async (id, rule) => {
         const payload = {
             command: 'set_rule',
@@ -382,6 +386,7 @@ export const ApiCardContextProvider = ({ children }) => {
             trigger_sensor,
             turn_on,
             set_rule,
+            debounced_set_rule,
             reset_rule,
             add_schedule_rule,
             delete_schedule_rule,

frontend/src/pages/api_card/InstanceCard.js
@@ -117,7 +117,7 @@ const InstanceCard = ({ id }) => {
     const {
         recording,
         get_instance_section,
-        set_rule,
+        debounced_set_rule,
         enable_instance,
         reset_rule,
         highlightCards
@@ -152,7 +152,7 @@ const InstanceCard = ({ id }) => {
         // Set local state (move slider)
         setlocalState({ ...localState, current_rule: newRule });
         // Set upstream state (sends debounced API call to node)
-        set_rule(id, newRule);
+        debounced_set_rule(id, newRule);
     };
 
     // Called when user releases click on slider, resumes status updates

frontend/src/pages/api_card/__tests__/App.test.js
@@ -610,24 +610,22 @@ describe('App', () => {
 
         // Click Submit button, confirm correct payload sent
         await user.click(app.getByRole('button', { name: 'Submit' }));
-        await waitFor(() => {
-            expect(global.fetch).toHaveBeenCalledWith('/send_command', {
-                method: 'POST',
-                body: JSON.stringify({
-                    "command": "set_rule",
-                    "instance": "device7",
-                    "rule": {
-                        "on": [
-                            "ignore"
-                        ],
-                        "off": [
-                            "ignore"
-                        ]
-                    },
-                    "target": "192.168.1.100"
-                }),
-                headers: postHeaders
-            });
+        expect(global.fetch).toHaveBeenCalledWith('/send_command', {
+            method: 'POST',
+            body: JSON.stringify({
+                "command": "set_rule",
+                "instance": "device7",
+                "rule": {
+                    "on": [
+                        "ignore"
+                    ],
+                    "off": [
+                        "ignore"
+                    ]
+                },
+                "target": "192.168.1.100"
+            }),
+            headers: postHeaders
         });
     });
 

commit ef9a144e8e5b45be78a61649354579b2c8f9eef9

Regression fix: Slider handles no longer jump back when released
Broken in b4180916, no longer called update_instance to update main state before debounced rule. Slider is in edit mode while moving (uses own local state) but switches back to main state immediately when released. Since main state was not updated slider position jumped back until next status update.

frontend/src/ApiCardContext.js
@@ -205,9 +205,15 @@ export const ApiCardContextProvider = ({ children }) => {
         console.log(result);
     };
 
-    // Handler for rule sliders and buttons, updates local state immediately, makes
-    // API call after 150ms delay (prevents contant calls saturating ESP32 bandwidth)
-    const debounced_set_rule = useCallback(debounce(async (id, rule) => {
+    // Handler for rule sliders and buttons, updates context state immediately,
+    // makes API call after 150ms delay (avoid ESP32 DOS from too many calls)
+    const debounced_set_rule = async (id, rule) => {
+        update_instance(id, {current_rule: rule});
+        debounced_set_rule_callback(id, rule);
+    };
+
+    // Resets timer if called again within 150ms
+    const debounced_set_rule_callback = useCallback(debounce(async (id, rule) => {
         const payload = {
             command: 'set_rule',
             instance: id,