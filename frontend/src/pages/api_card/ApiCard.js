565af45bdb67b600e6d6cc571dd27f1ea2588b70
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-07-21 20:37:17 -0700

    Disable status updates while recording macro
    Status updates undo changes made by the user a few seconds later, which is confusing and may make the user think they need to repeat the change (eg user moves a rule slider, then status update resets it to orignal position - the user may think set_rule macro action was not saved)

--- frontend/src/pages/api_card/ApiCard.js
+++ frontend/src/pages/api_card/ApiCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ErrorModal from 'modals/ErrorModal';
 import DebugModal from './DebugModal';
 import ApiTargetRuleModal from 'modals/ApiTargetRuleModal';
@@ -8,13 +8,18 @@ import Layout from './Layout';
 import FadeModal from './FadeModal';
 import SaveRulesToast from './SaveRulesToast';
 import { UpdateStatus } from './UpdateStatus';
+import { ApiCardContext } from 'root/ApiCardContext';
 
 const App = () => {
+    const { recording } = useContext(ApiCardContext);
+
     return (
         <div>
             <Header />
             <Layout />
-            <UpdateStatus />
+            {/* Mount UpdateStatus utility component unless recording macro */}
+            {/* Status updates undo user changes while recording, confusing */}
+            {recording ? null : <UpdateStatus /> }
 
             {/* Modals (hidden) */}
             <FadeModal />
