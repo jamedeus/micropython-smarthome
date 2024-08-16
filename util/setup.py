a8d0e7d4dae918f56b0027dac47ab4c3906d49d2
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-08-15 22:42:54 -0700

    Util module no longer has to be installed as editable package
    Previous setup.py didn't actually discover any of the modules inside util (no __init__.py), installing it as editable was basically a hack (just points to the directory, doesn't copy anything to site-packages when installed)

--- util/setup.py
+++ util/setup.py
@@ -2,7 +2,18 @@ import glob
 import shutil
 import setuptools
 
-setuptools.setup(name="util", packages=setuptools.find_packages())
+setuptools.setup(
+    name='util',
+    packages=['util'],
+    package_dir={'util': '.'},
+    package_data={
+        'util': [
+            'metadata/devices/*.json',
+            'metadata/sensors/*.json',
+        ],
+    },
+    include_package_data=True
+)
 
 # Remove metadata clutter
 for i in glob.glob('*.egg-info'):
