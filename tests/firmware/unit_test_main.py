72261cc64d2bafa4e3a2dda75fa3c3de93d9d7a6
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-08-26 23:51:21 -0700

    Firmware test runner: Show prompt before running tests
    Prevents getting stuck in infinite loop if test suite crashes (api tests currently don't work on firmware). Previously wrote name of next test suite to config file after tests completed, but if they failed to complete it would just run the same failing tests forever until manually corrected
    
    Slightly less memory efficient (1-500 more bytes used, max free size reduced by around 20) but no new tests failing

--- tests/firmware/unit_test_main.py
+++ tests/firmware/unit_test_main.py
@@ -20,25 +20,40 @@ log = logging.getLogger("Main")
 sys.path.insert(len(sys.path), '/tests')
 
 
-async def run_tests():
-    print("\n\n---BEGIN TESTING---\n\n")
+def reboot():
+    import machine
+    machine.reset()
 
-    # Load config, records results from last boot + which test to run on next boot
+
+def start_webrepl():
+    # Start webrepl to allow upload
+    import webrepl
+    connect_wifi()
+    webrepl.start()
+
+    print("\nPress enter to reboot when upload complete")
+    input()
+    print("Rebooting...")
+    reboot()
+
+
+def load_testing_config_file():
     try:
         with open('testing_config.json', 'r') as file:
             testing_config = json.load(file)
     except OSError:
-        # If config doesn't exist, create template
+        # Return template if config doesn't exist
         testing_config = {}
-        testing_config["next"] = "core"
         testing_config["results"] = {}
         testing_config["results"]["core"] = {}
         testing_config["results"]["api"] = {}
         testing_config["results"]["device"] = {}
         testing_config["results"]["sensor"] = {}
 
-    target = testing_config["next"]
+    return testing_config
 
+
+async def run_tests(testing_config, target):
     print(f"---RUNNING {target.upper()} TESTS---\n\n")
 
     runner = unittest.TestRunner()
@@ -46,15 +61,15 @@ async def run_tests():
     # Record total tests, failed, errored, and skipped for each test case
     detailed_results = {}
 
-    # Import all modules under /tests
+    # Iterate modules to find tests
     for test in os.listdir():
-        # Only run tests if they are in correct category (core, device, sensor)
+        # Only import tests if they are in correct category (core, device, sensor)
         if test.startswith("test_" + target):
             module = __import__(test.split(".")[0])
         else:
             continue
 
-        # Find and run test case classes in module
+        # Find and run unittest.TestCase in module
         for i in dir(module):
             c = getattr(module, i)
             try:
@@ -85,81 +100,53 @@ async def run_tests():
     micropython.mem_info()
     print()
 
-    # Set test to run on next boot, add results to report
-    if testing_config["next"] == "core":
-        testing_config["next"] = "api"
-        testing_config["results"]["core"] = detailed_results
-    if testing_config["next"] == "api":
-        testing_config["next"] = "device"
-        testing_config["results"]["api"] = detailed_results
-    elif testing_config["next"] == "device":
-        testing_config["next"] = "sensor"
-        testing_config["results"]["device"] = detailed_results
-    elif testing_config["next"] == "sensor":
-        testing_config["next"] = "core"
-        testing_config["results"]["sensor"] = detailed_results
-
-    # Write to disk
+    # Add results to report, write to disk
+    testing_config["results"][target] = detailed_results
     with open('testing_config.json', 'w') as file:
         json.dump(testing_config, file)
 
-    # Start webrepl to allow upload
-    import webrepl
-    connect_wifi()
-    webrepl.start()
+
+def prompt():
+    # Load config, records results from last boot
+    testing_config = load_testing_config_file()
 
     while True:
-        print("\nWhat would you like to do next?")
+        print("\nWhat would you like to do?")
         print(" [1] Run core tests")
         print(" [2] Run api tests")
         print(" [3] Run device tests")
         print(" [4] Run sensor tests")
-        print(f" [5] View results from current test ({target})")
-        print(" [6] View results from all tests")
+        print(" [5] View results from all tests")
+        print(" [6] Start webrepl (wait for upload)")
         print(" [7] Reboot")
         choice = input()
         print()
 
         if choice == "1":
-            testing_config["next"] = "core"
-            with open('testing_config.json', 'w') as file:
-                json.dump(testing_config, file)
-            import machine
-            machine.reset()
+            asyncio.run(run_tests(testing_config, "core"))
 
         if choice == "2":
-            testing_config["next"] = "api"
-            with open('testing_config.json', 'w') as file:
-                json.dump(testing_config, file)
-            import machine
-            machine.reset()
+            asyncio.run(run_tests(testing_config, "api"))
+            reboot()
 
         elif choice == "3":
-            testing_config["next"] = "device"
-            with open('testing_config.json', 'w') as file:
-                json.dump(testing_config, file)
-            import machine
-            machine.reset()
+            asyncio.run(run_tests(testing_config, "device"))
+            reboot()
 
         elif choice == "4":
-            testing_config["next"] = "sensor"
-            with open('testing_config.json', 'w') as file:
-                json.dump(testing_config, file)
-            import machine
-            machine.reset()
+            asyncio.run(run_tests(testing_config, "sensor"))
+            reboot()
 
         elif choice == "5":
-            print(f"---{target.upper()} TESTS---\n")
-            print_report(testing_config["results"][target])
-
-        elif choice == "6":
             for category in testing_config["results"]:
                 print(f"---{category.upper()} TESTS---\n")
                 print_report(testing_config["results"][category])
 
+        elif choice == "6":
+            start_webrepl()
+
         elif choice == "7":
-            import machine
-            machine.reset()
+            reboot()
 
         else:
             print("\nERROR: Please enter a number and press enter.\n")
@@ -172,9 +159,9 @@ def print_report(results):
     # Print results summary
     for i in results:
         print(i.split(".")[0].split("_")[2])
-        print(f" - Tests:         {results[i]["tests_run"]}")
-        print(f"   - Failed:      {results[i]["failed"]}")
-        print(f"   - Errored:     {results[i]["errors"]}\n")
+        print(f" - Tests:         {results[i]['tests_run']}")
+        print(f"   - Failed:      {results[i]['failed']}")
+        print(f"   - Errored:     {results[i]['errors']}\n")
         total_tests += results[i]["tests_run"]
         total_failed += results[i]["failed"]
 
@@ -208,4 +195,4 @@ def start():
 
     gc.collect()
 
-    asyncio.run(run_tests())
+    prompt()
