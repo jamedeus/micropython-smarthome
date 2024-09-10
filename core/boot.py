jamedeus/micropython-smarthome

commit 164fe9ab6dcae1a25926d3d2529e0e926f62d6bb

Add debug logging statements, change log level of existing statements
Change all failed send commands and API calls to error (default log level)

Debug now intended for short-term use to identify known issue (very verbose, logs will rotate after a few hours), info intended for medium-term (logs send commands, use to identify motion sensor activating when nobody home etc)

core/main.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     style='%'
 )
 log = logging.getLogger("Main")
-log.info("Booted")
+log.critical("Booted")
 
 
 # Log uncaught exceptions to disk before calling default handler
@@ -36,7 +36,7 @@ def start():
         config = Config(read_config_from_disk())
     # Load blank config template if config.json does not exist (initial setup)
     except OSError:
-        log.info("config.json not found, loading blank template")
+        log.critical("config.json not found, loading blank template")
         from default_config import default_config
         config = Config(default_config)
     gc.collect()

core/Api.py
@@ -510,7 +510,7 @@ def get_climate_data(args):
 def clear_log_file(args):
     try:
         clear_log()
-        log.info("Deleted old log (API request)")
+        log.critical("Deleted old log (API request)")
         return {"clear_log": "success"}
     except OSError:
         return {"ERROR": "no log file found"}
@@ -526,6 +526,7 @@ def set_log_level(args):
         }
     with open("log_level.py", "w") as file:
         file.write(f"LOG_LEVEL = '{args[0]}'")
+    log.critical("Log level changed to %s", args[0])
     return {"Success": "Log level set (takes effect after reboot)"}
 
 

core/Config.py
@@ -125,8 +125,7 @@ class Config():
 
         # Get HH:MM timestamp of next reload, write to log
         self.reload_time = f"{time.localtime(next_reload + adjust)[3]}:{time.localtime(next_reload + adjust)[4]}"
-        log.debug("Reload_schedule_rules callback scheduled for %s am", self.reload_time)
-        print_with_timestamp(f"Reload_schedule_rules callback scheduled for {self.reload_time} am")
+        log.info("Reload_schedule_rules callback scheduled for %s am", self.reload_time)
 
         # Add timer to queue
         SoftwareTimer.timer.create(ms_until_reload, self.reload_schedule_rules, "reload_schedule_rules")
@@ -168,6 +167,7 @@ class Config():
 
     # Takes config dict (devices only), instantiates each with appropriate class, adds to self.devices
     def instantiate_devices(self, conf):
+        log.debug("Instantiating devices")
         for device in sorted(conf):
             # Add device's schedule rules to dict
             self.schedule[device] = conf[device]["schedule"]
@@ -192,10 +192,11 @@ class Config():
                 )
                 print_with_timestamp(f"ERROR: Failed to instantiate {device}, unsupported device type {conf[device]['_type']}")
 
-        log.debug("Finished creating device instances")
+        log.debug("Finished instantiating device instances")
 
     # Takes config dict (sensors only), instantiates each with appropriate class, adds to self.sensors
     def instantiate_sensors(self, conf):
+        log.debug("Instantiating sensors")
         for sensor in sorted(conf):
             # Add sensor's schedule rules to dict
             self.schedule[sensor] = conf[sensor]["schedule"]
@@ -230,12 +231,14 @@ class Config():
                 )
                 print_with_timestamp(f"ERROR: Failed to instantiate {sensor}, unsupported sensor type {conf[sensor]['_type']}")
 
-        log.debug("Finished creating sensor instances")
+        log.debug("Finished instantiating sensor instances")
 
     # Maps relationships between sensors ("triggers") and devices ("targets")
     # Multiple sensors with identical targets are merged into groups (or 1 sensor per group if unique targets)
     # Main loop iterates Groups, calls Group methods to check sensor conditions and apply device actions
     def build_groups(self):
+        log.debug("Building groups")
+
         # Stores completed Group instances
         self.groups = []
 
@@ -270,6 +273,8 @@ class Config():
 
             for device in instance.targets:
                 device.group = instance
+
+        log.debug("Finished building %s groups", len(self.groups))
 
     # Called by status API endpoint, frontend polls every 5 seconds while viewing node
     # Returns object with metadata + current status info for all devices and sensors
@@ -325,19 +330,23 @@ class Config():
                 continue
             else:
                 print_with_timestamp(f"Successfully connected to {credentials['ssid']}")
-                log.info("Successfully connected to %s", credentials['ssid'])
+                log.debug("Successfully connected to %s", credentials['ssid'])
 
         failed_attempts = 0
 
         # Ensure enough free ram for API response
         gc.collect()
 
-        # Set time from internet in correct timezone, retry until successful (ntptime easier but no timezone support)
+        # Set system clock and sunrise/sunset times with timestamps from ipgeo API
+        # Determines timezone by IP address unless config file contains GPS coords
+        # Retry up to 5 times if API call fails, reboot after 5th failure
         while True:
             try:
+                log.debug("Getting system time from ipgeolocation.io API...")
                 # Use GPS coordinates if present, otherwise rely on IP lookup
                 url = f"https://api.ipgeolocation.io/astronomy?apiKey={ipgeo_key}"
                 if self.gps:
+                    log.debug("Using GPS coordinates from config file")
                     url += f"&lat={self.gps['lat']}&long={self.gps['lon']}"
 
                 response = requests.get(url, timeout=5)
@@ -351,10 +360,16 @@ class Config():
 
                 # Set RTC (uses different parameter order than time.localtime)
                 RTC().datetime((year, month, day, 0, int(hour), int(minute), int(second), int(millisecond)))
+                log.debug("System clock set")
 
                 # Set sunrise/sunset times
                 self.schedule_keywords["sunrise"] = response.json()["sunrise"]
                 self.schedule_keywords["sunset"] = response.json()["sunset"]
+                log.debug(
+                    "Received sunrise time = %s, sunset time = %s",
+                    self.schedule_keywords["sunrise"],
+                    self.schedule_keywords["sunset"]
+                )
                 response.close()
                 break
 
@@ -367,10 +382,10 @@ class Config():
             # Network issue
             except OSError:
                 print_with_timestamp("Failed to set system time, retrying...")
-                log.debug("Failed to set system time, retrying...")
+                log.error("Failed to set system time, retrying...")
                 failed_attempts += 1
                 if failed_attempts > 5:
-                    log.info("Failed to set system time 5 times, reboot triggered")
+                    log.critical("Failed to set system time 5 times, reboot triggered")
                     reboot()
                 time.sleep_ms(1500)  # If failed, wait 1.5 seconds before retrying
                 gc.collect()  # Free up memory before retrying
@@ -449,6 +464,8 @@ class Config():
 
     # Add callbacks for all schedule rules to SoftwareTimer queue
     def build_queue(self):
+        log.debug("Building schedule rule queue for all devices and sensors")
+
         # Delete all existing schedule rule timers to avoid conflicts
         SoftwareTimer.timer.cancel("scheduler")
 
@@ -518,7 +535,7 @@ class Config():
                 SoftwareTimer.timer.create(milliseconds, instance.next_rule, "scheduler")
                 gc.collect()
 
-        print_with_timestamp(f"Finished building queue, total timers = {len(SoftwareTimer.timer.queue)}")
+        print_with_timestamp("Finished building schedule rule queue")
         log.debug("Finished building queue, total timers = %s", len(SoftwareTimer.timer.queue))
 
     # Takes ID (device1, sensor2, etc), returns instance or False

core/Group.py
@@ -1,5 +1,4 @@
 import logging
-from util import print_with_timestamp
 
 # Set name for module's log lines
 log = logging.getLogger("Group")
@@ -26,9 +25,10 @@ class Group():
         # https://docs.micropython.org/en/latest/reference/isr_rules.html#creation-of-python-objects
         self._refresh = self.refresh
 
-        log.info(f"Instantiated Group named {self.name}")
+        log.info("Instantiated Group named %s", self.name)
 
     def reset_state(self):
+        log.debug("%s: reset state to None", self.name)
         self.state = None
 
     # Called by decorators in some sensor's add_routines method, appends functions to self.post_action_routines
@@ -46,6 +46,7 @@ class Group():
             if sensor.enabled:
                 conditions.append(sensor.condition_met())
 
+        log.debug("%s: Sensor conditions: %s", self.name, conditions)
         return conditions
 
     def determine_correct_action(self, conditions):
@@ -68,6 +69,7 @@ class Group():
     def apply_action(self, action):
         # No action needed if group state already matches desired state
         if self.state == action:
+            log.debug("%s: current state already matches action", self.name)
             return
 
         failed = False
@@ -75,6 +77,7 @@ class Group():
         for device in self.targets:
             # Do not turn device on/off if already on/off
             if not action == device.state:
+                log.debug("%s: applying action to %s", self.name, device.name)
                 # int converts True to 1, False to 0
                 success = device.send(int(action))
 
@@ -87,17 +90,23 @@ class Group():
 
         # If all succeeded, change group state to prevent re-sending
         if not failed:
+            log.debug("%s: finished applying action, no errors", self.name)
             self.state = action
 
             # Run post-action routines (if any) for all sensors in group
             for function in self.post_action_routines:
                 function()
 
+        else:
+            log.debug("%s: encountered errors while applying action", self.name)
+
     # Check condition of all sensors in group, turn devices on/off if needed
     # Arg is required for micropython.schedule but unused
     # Called by all sensors when condition changes
     def refresh(self, arg=None):
+        log.debug("%s: refresh group", self.name)
         action = self.determine_correct_action(self.check_sensor_conditions())
+        log.debug("%s: correct action: %s", self.name, action)
         if action is not None:
-            print_with_timestamp(f"{self.name}: Applying action: {action}")
+            log.info("%s: applying action: %s", self.name, action)
             self.apply_action(action)

core/Instance.py
@@ -68,6 +68,7 @@ class Instance():
         '''Sets enabled bool to True (allows sensors to be checked, devices to
         be turned on/off), and ensures current_rule contains a usable value.
         '''
+        log.debug("%s: enabled", self.name)
         self.enabled = True
 
         # Replace "disabled" with usable rule
@@ -78,6 +79,7 @@ class Instance():
         '''Sets enabled bool to False (prevents sensor from being checked,
         prevents devices from being turned on).
         '''
+        log.debug("%s: disabled", self.name)
         self.enabled = False
 
     def get_usable_rule(self):
@@ -107,6 +109,10 @@ class Instance():
           rule:      The new rule, will be set as current_rule if valid
           scheduled: Optional, if True also sets scheduled_rule if rule valid
         '''
+        log.debug(
+            "%s: set_rule called with %s (scheduled=%s)",
+            self.name, rule, scheduled
+        )
 
         # Check if rule is valid (may return modified rule, eg cast str to int)
         valid_rule = self.rule_validator(rule)
@@ -171,7 +177,7 @@ class Instance():
         '''Called by SoftwareTimer interrupt at each scheduled rule change.
         Calls set_rule with first item in rule_queue (see Config.build_queue).
         '''
-        log.debug("%s: Scheduled rule change", self.name)
+        log.info("%s: Scheduled rule change", self.name)
         self.print("Scheduled rule change")
         self.set_rule(self.rule_queue.pop(0), True)
 

core/util.py
@@ -81,7 +81,7 @@ def write_ir_macros_to_disk(conf):
 # Must accept arg (hardware Timer passes self as arg)
 def reboot(arg=None):
     print_with_timestamp("Reboot function called, rebooting...")
-    log.info("Reboot function called, rebooting...\n")
+    log.critical("Reboot function called, rebooting...\n")
     from machine import reset
     reset()
 
@@ -106,7 +106,7 @@ def check_log_size():
     if os.stat('app.log')[6] > 100000:
         print_with_timestamp("\nLog exceeded 100 KB, clearing...\n")
         clear_log()
-        log.info("Deleted old log (exceeded 100 KB size limit)")
+        log.critical("Deleted old log (exceeded 100 KB size limit)")
 
     # Add back to queue
     SoftwareTimer.timer.create(60000, check_log_size, "check_log_size")

devices/ApiTarget.py
@@ -153,6 +153,10 @@ class ApiTarget(Device):
         If new rule is "enabled" replaces with default_rule and calls enable.
         If device is already on calls send method so new rule takes effect.
         '''
+        log.debug(
+            "%s: set_rule called with %s (scheduled=%s)",
+            self.name, rule, scheduled
+        )
 
         # Check if rule is valid - may return a modified rule (ie cast str to int)
         valid_rule = self.rule_validator(rule)
@@ -182,7 +186,7 @@ class ApiTarget(Device):
         '''Called when an API call receives an error response. Takes full
         payload and response, writes multiline log with indent for readability.
         '''
-        log.error("""%s: Send method failed
+        log.error("""%s: Request failed
         Payload: %s
         Response: %s""", self.name, msg, err)
         self.print(f"Send method failed with payload {msg}")
@@ -200,6 +204,7 @@ class ApiTarget(Device):
             res = s.recv(1000).decode()
             res = json.loads(res)
         except (OSError, ValueError):
+            log.error("%s: exception during request", self.name)
             res = False
         s.close()
 
@@ -219,6 +224,10 @@ class ApiTarget(Device):
         '''Sends API call in current_rule "on" key if argument is True.
         Sends API call in current_rule "off" key if argument is False.
         '''
+        log.debug(
+            "%s: send method called, rule=%s, state=%s",
+            self.name, self.current_rule, state
+        )
 
         # Refuse to turn disabled device on, but allow turning off
         if not self.enabled and state:
@@ -257,6 +266,8 @@ class ApiTarget(Device):
         if state:
             for sensor in self.triggered_by:
                 if sensor._type in ["pir", "ld2410"]:
+                    # BUG this is only appropriate when
+                    log.debug("%s: Reset %s motion attribute", self.name, sensor.name)
                     sensor.motion = False
 
         # Tells group send succeeded
@@ -267,6 +278,7 @@ class ApiTarget(Device):
         Passes current_rule directly to API backend without opening connection
         (request method is synchronous, blocks Api.run_client method).
         '''
+        log.debug("%s: send_to_self method called, command=%s", self.name, command)
         path = command[0]
         args = command[1:]
 

devices/DesktopTarget.py
@@ -30,7 +30,10 @@ class DesktopTarget(HttpGet):
         '''Makes API call to turn screen ON if argument is True.
         Makes API call to turn screen OFF if argument is False.
         '''
-        log.info("%s: send method called, state = %s", self.name, state)
+        log.debug(
+            "%s: send method called, rule=%s, state=%s",
+            self.name, self.current_rule, state
+        )
 
         # Refuse to turn disabled device on, but allow turning off
         if not self.enabled and state:
@@ -40,6 +43,7 @@ class DesktopTarget(HttpGet):
 
         try:
             response = self.request(self.get_url(state))
+            log.debug("%s: response status: %s", self.name, response.status_code)
             if response.status_code == 200:
                 if state:
                     self.print("Turned on")
@@ -54,12 +58,13 @@ class DesktopTarget(HttpGet):
             raise ValueError
         except OSError:
             # Wifi interruption, send failed
+            log.error("%s: send failed (wifi error)", self.name)
             return False
         except ValueError:
             # Unexpected response (different service running on port 5000), disable
             if self.enabled:
                 self.print("Fatal error (unexpected response from desktop), disabling")
-                log.info(
+                log.critical(
                     "%s: Fatal error (unexpected response from desktop), disabling",
                     self.name
                 )

commit cacde759e991225feab881650bb229ffdec1a29f

Bug fix: Wifi setup no longer fails to reboot after receiving credentials
Issue: util.reboot contains a log line, but log file and syntax were never set in wifi_setup. This resulted in a TypeError when reboot was called.

Issue started in 164fe9ab when log level was changed to critical in reboot function, previously line was skipped. Bug present in firmware 0.171 through 0.179 (still works but have to reboot manually after wifi setup).

core/boot.py
@@ -1,6 +1,13 @@
 import os
 import gc
+import logging
 from flashbdev import bdev
+try:
+    from log_level import LOG_LEVEL
+except ImportError:
+    LOG_LEVEL = 'ERROR'
+
+print("--------Booted--------")
 
 # Mount filesystem (or format if first boot)
 try:
@@ -10,6 +17,16 @@ except OSError:
     import inisetup
     vfs = inisetup.setup()
 gc.collect()
+
+# Set log file and syntax
+logging.basicConfig(
+    level=logging._nameToLevel[LOG_LEVEL],
+    filename='app.log',
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    style='%'
+)
+log = logging.getLogger("Boot")
+log.critical("Booted, log level: %s", LOG_LEVEL)
 
 
 # Start main loop if wifi_credentials file exists

core/main.py
@@ -6,22 +6,8 @@ from Api import app
 from Config import Config
 from SoftwareTimer import timer
 from util import read_config_from_disk, check_log_size
-try:
-    from log_level import LOG_LEVEL
-except ImportError:
-    LOG_LEVEL = 'ERROR'
 
-print("--------Booted--------")
-
-# Set log file and syntax
-logging.basicConfig(
-    level=logging._nameToLevel[LOG_LEVEL],
-    filename='app.log',
-    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-    style='%'
-)
 log = logging.getLogger("Main")
-log.critical("Booted")
 
 
 # Log uncaught exceptions to disk before calling default handler

core/wifi_setup.py
@@ -4,11 +4,15 @@ import json
 import socket
 import asyncio
 import network
+import logging
 import binascii
 from machine import Timer
 import setup_ssl_certs
 from util import reboot
 from setup_page import setup_page
+
+log = logging.getLogger("Setup")
+log.critical("Wifi credentials not found, starting captive portal")
 
 reboot_timer = Timer(1)
 