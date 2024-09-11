9b757077c186ff7114c63196f5586c0569116cd7
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-09-10 23:21:30 -0700

    Rename desktop-integration -> desktop_integration (snake case)

--- desktop_integration/setup.sh
+++ desktop_integration/setup.sh
@@ -5,7 +5,7 @@ pip3 install -r requirements.txt > /dev/null 2>&1 || { printf "Failed to install
 printf "Dependencies installed.\n"
 
 # Copy executable to local bin
-cp desktop-integration.py /home/$USER/.local/bin/desktop-integration.py || { printf "Failed to install application.\n"; exit 1; }
+cp desktop_integration.py /home/$USER/.local/bin/desktop_integration.py || { printf "Failed to install application.\n"; exit 1; }
 printf "Application installed.\n"
 
 # Create service file for current user
@@ -21,7 +21,7 @@ Restart=on-failure
 User=$USER
 Environment="DISPLAY=$DISPLAY"
 WorkingDirectory=/home/$USER/.local/bin
-ExecStart=/home/$USER/.local/bin/gunicorn --workers 2 --timeout 60 --bind 0.0.0.0:5000 desktop-integration:app
+ExecStart=/home/$USER/.local/bin/gunicorn --workers 2 --timeout 60 --bind 0.0.0.0:5000 desktop_integration:app
 
 [Install]
 WantedBy=network.target
