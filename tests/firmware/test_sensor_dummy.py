jamedeus/micropython-smarthome

commit 66a6e6c4d00ea081687d9b56c46b42b10ed4644d

Fix crash if Dummy sensor not in group
Broken by 7b254dc1, all devices and sensors now have group attribute (contains None if not in group)

sensors/Dummy.py
@@ -59,8 +59,7 @@ class Dummy(Sensor):
 
         result = super().set_rule(rule, scheduled)
         # Refresh group if rule changed successfully
-        # TODO
-        if result and hasattr(self, "group"):
+        if self.group:
             self.refresh_group()
         return result
 

commit 20ac857edc8b53f00a679c73a6e17ffcb3e8bb6e

Bug fix: Only refresh group if new rule valid
Broken in 66a6e6c4, should have been  instead of  (Sensor.refresh_group already checks self.group, redundant)

sensors/Dummy.py
@@ -50,11 +50,11 @@ class Dummy(Sensor):
           scheduled: Optional, if True also sets scheduled_rule if rule valid
         '''
 
-        result = super().set_rule(rule, scheduled)
         # Refresh group if rule changed successfully
-        if self.group:
+        if super().set_rule(rule, scheduled):
             self.refresh_group()
-        return result
+            return True
+        return False
 
     def condition_met(self):
         '''Returns True if current_rule is "on" (turn target devices on).

tests/firmware/test_sensor_dummy.py
@@ -75,11 +75,16 @@ class TestDummySensor(unittest.TestCase):
         self.assertTrue(self.group.refresh_called)
         self.group.refresh_called = False
 
-        # Change rule to 'on', should accept, should call Group.refresh
+        # Change rule to 'off', should accept, should call Group.refresh
         self.assertTrue(self.instance.set_rule("off"))
         self.assertEqual(self.instance.current_rule, 'off')
         self.assertTrue(self.group.refresh_called)
         self.group.refresh_called = False
+
+        # Set invalid rule, should reject, should NOT call Group.refresh
+        self.assertFalse(self.instance.set_rule("invalid"))
+        self.assertEqual(self.instance.current_rule, 'off')
+        self.assertFalse(self.group.refresh_called)
 
     def test_06_condition_met(self):
         # Should always return True when rule is on